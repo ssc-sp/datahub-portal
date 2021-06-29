06a672eee53825f34c7cd52fc9b2f51a185e72c5
Iroel Perez <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2021-06-29 14:57:05 -0400

    Adding other Metadata component options

--- Datahub.Metadata/FieldChoice.cs
+++ Datahub.Metadata/FieldChoice.cs
@@ -1,9 +1,16 @@
-﻿namespace NRCan.Datahub.Metadata
+﻿using System;
+using System.Globalization;
+
+namespace NRCan.Datahub.Metadata
 {
     public class FieldChoice
     {
         public string ChoiceId { get; set; }
         public string LabelEnglish { get; set; }
         public string LabelFrench { get; set; }
+        
+        public string Label => IsFrenchCulture() ? LabelFrench : LabelEnglish;
+
+        static bool IsFrenchCulture() => CultureInfo.CurrentCulture.Name.StartsWith("fr", StringComparison.InvariantCulture);
     }
 }
