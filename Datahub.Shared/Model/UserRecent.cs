b8fcf0aab43c9dfcf0db388d6e8fedde16b43301
gcnabeel <nabeel.bader@canada.ca>
2021-07-10 00:10:54 -0400

    Location Managment
    Tracked user changes... needs mad code review

---
+++ Datahub.Shared/Model/UserRecent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NRCan.Datahub.Shared.EFCore
+{
+    public class UserRecent
+    {
+        [Key]
+        public string UserId { get; set; }
+        public ICollection<UserRecentActions> UserRecentActions{ get; set; } = new List<UserRecentActions>();
+        
+    }
+    [Owned]
+    public class UserRecentActions
+    { 
+        [Key]
+        public Guid UserRecentActionId { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public string icon { get; set; }
+        public DateTimeOffset accessedTime{ get; set; }
+    }
+}
