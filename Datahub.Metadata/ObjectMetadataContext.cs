06b355ec14b2d83ccc61d4ccf7f1ea31068c76a1
Iroel Perez <iroel.perez@gmail.com>
2021-07-28 09:13:13 -0400

    Metadata field values storage (progress)

---
+++ Datahub.Metadata/ObjectMetadataContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NRCan.Datahub.Metadata.Model;
+
+namespace NRCan.Datahub.Metadata
+{
+    public class ObjectMetadataContext
+    {
+        readonly string _objectId;
+        readonly MetadataDefinition _definition;
+        readonly List<ObjectFieldValue> _fieldValues;
+
+        public ObjectMetadataContext(string objectId, MetadataDefinition definition, IEnumerable<ObjectFieldValue> fieldValues)
+        {
+            _objectId = objectId;
+            _definition = definition;
+            _fieldValues = fieldValues.ToList();
+        }
+
+        public MetadataDefinition MetadataDefinition => _definition;
+        public List<ObjectFieldValue> FieldValues => _fieldValues;
+    }
+}
