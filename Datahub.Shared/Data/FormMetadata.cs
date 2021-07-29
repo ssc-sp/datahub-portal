d43323e65241b7abec0c044ec7192c065c829ce4
gcnabeel <nabeel.bader@canada.ca>
2021-07-29 12:53:04 -0400

    parital work
    doing this to pull latest dev changes into this branch as it has the paginated report

---
+++ Datahub.Shared/Data/FormMetadata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NRCan.Datahub.Shared.Data
+{
+    public class FormMetadata
+    {
+        public string Header { get; set; }
+        public string SubHeader { get; set; }
+
+        //public List<Func<object, string>> AccessorFunctions { get; set; }
+
+        public List<Func<T, string>> GetAccessorFunctions<T>()
+        {
+            return null;
+        }
+    }
+}
