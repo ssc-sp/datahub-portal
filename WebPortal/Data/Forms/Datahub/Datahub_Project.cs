33cfee80633119c73582843eecc22617680d022f
gcnabeel <nabeel.bader@canada.ca>
2021-08-09 14:22:27 -0400

    added approval logic for project users

--- WebPortal/Data/Forms/Datahub/Datahub_Project.cs
+++ WebPortal/Data/Forms/Datahub/Datahub_Project.cs
@@ -68,11 +68,12 @@ namespace NRCan.Datahub.Data.Projects
     {
 
         [Key]
-
         public int ProjectUserRequest_ID { get; set; }
         
         [StringLength(200)]
         public string User_ID { get; set; }
+
+        public DateTime Requested_DT { get; set; }
 
         public DateTime? Approved_DT { get; set;  }
         
