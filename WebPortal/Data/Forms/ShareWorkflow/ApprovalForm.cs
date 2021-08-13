88c8d6665980c48480231e40552b27abc6857160
Iroel Perez <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2021-08-13 10:05:43 -0400

    Misc anotation changes in Approval Form.

--- WebPortal/Data/Forms/ShareWorkflow/ApprovalForm.cs
+++ WebPortal/Data/Forms/ShareWorkflow/ApprovalForm.cs
@@ -1,9 +1,5 @@
 ﻿using Elemental.Components;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NRCan.Datahub.Portal.Data.Forms.ShareWorkflow
 {
@@ -20,60 +16,48 @@ namespace NRCan.Datahub.Portal.Data.Forms.ShareWorkflow
         public string Dataset_Title_TXT { get; set; }
 
         /** Section: Source Information **/
+        [Required]
+        [MaxLength(16)]
         [AeLabel(isDropDown: true)]
         public string Type_Of_Data_TXT { get; set; }
 
         /** Section: Legal / Licensing / Copyright **/
-        //[AeLabel(isDropDown: true)]
         public bool Copyright_Restrictions_FLAG { get; set; }
 
         /** Section: Authority to Release **/
-        //[AeLabel(isDropDown: true)]
         public bool Authority_To_Release_FLAG { get; set; }
 
         /** Section: Privacy **/
-        //[AeLabel(isDropDown: true)]
         public bool Private_Personal_Information_FLAG { get; set; }
 
         /** Section: Access to Information **/
-        //[AeLabel(isDropDown: true)]
         public bool Subject_To_Exceptions_Or_Eclusions_FLAG { get; set; }
 
         /** Section: Security **/
-        //[AeLabel(isDropDown: true)]
         public bool Not_Clasified_Or_Protected_FLAG { get; set; }
 
         /** Section: Cost **/
-        //[AeLabel(isDropDown: true)]
         public bool Can_Be_Released_For_Free_FLAG { get; set; }
 
         /** Section: Format **/
-        //[AeLabel(isDropDown: true)]
         public bool Machine_Readable_FLAG { get; set; }
 
         /** Section: Format **/
-        //[AeLabel(isDropDown: true)]
         public bool Non_Propietary_Format_FLAG { get; set; }
 
         /** Section: Format **/
-        //[AeLabel(isDropDown: true)]
         public bool Localized_Metadata_FLAG { get; set; }
 
         /** Section: Blanket Approval **/
-        //[AeLabel(isDropDown: true)]
         public bool Updated_On_Going_Basis_FLAG { get; set; }
 
         /** Section: Blanket Approval **/
-        //[AeLabel(isDropDown: true)]
         public bool Collection_Of_Datasets_FLAG { get; set; }
 
         /** Section: Blanket Approval **/
-        //[AeLabel(isDropDown: true)]
         public bool Approval_InSitu_FLAG { get; set; }
 
         /** Section: Blanket Approval **/
-        [Required]
-        [MaxLength(256)]
         public string Approval_Other_TXT { get; set; }
     }
 }
