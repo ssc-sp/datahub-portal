b317938490c0b303157059d6af8544327ba12ee4
Erik Putrycz <erik.putrycz@nrcan-rncan.gc.ca>
2021-08-13 18:12:14 -0400

    - new mode to run online with sqlite - initial PBI admin page

---
+++ Datahub.Shared/Model/Datahub/Project_PBI_DataSet.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace NRCan.Datahub.Shared.EFCore
+{
+    public class Project_PBI_DataSet
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid Workspace { get; set; }
+        public Datahub_Project Project { get; set; }
+    }
+}
---
+++ Datahub.Shared/Model/Datahub/Project_Storage.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace NRCan.Datahub.Shared.EFCore
+{
+
+    public enum Storage_Type
+    {
+        AzureGen1, AzureGen2
+    }
+    public class Project_Storage
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [MaxLength(200)]
+        public string AccountName { get; set; }
+
+        public Storage_Type Storage_Type { get; set; }
+    }
+}
