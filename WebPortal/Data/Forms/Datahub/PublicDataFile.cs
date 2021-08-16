aa63a4f9e3fd31d9fc476a273c734ecb0d77e0e2
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2021-08-16 14:16:28 -0400

    PublicDataFIle db model update

--- WebPortal/Data/Forms/Datahub/PublicDataFile.cs
+++ WebPortal/Data/Forms/Datahub/PublicDataFile.cs
@@ -21,18 +21,23 @@ namespace NRCan.Datahub.Data.Projects
         public string ProjectCode_CD { get; set; }
 
         public bool IsProjectBased => ProjectCode_CD != null;
-
-        [Required]
-        [StringLength(200)]
-        public string RequestingUserEmail_TXT { get; set; }
         
         [Required]
         [StringLength(200)]
         public string RequestingUser_ID { get; set; }
 
+        // Initial request timestamp
         public DateTime RequestedDate_DT { get; set; }
 
+        // Submission timestamp - after filling in required metadata
+        public DateTime? SubmittedDate_DT { get; set; }
+
         //TODO add field(s) for approver (user or person) information
+
+        // Approval timestamp
         public DateTime? ApprovedDate_DT { get; set; }
+
+        // Publication may be immediately after approval, or specified for a future date
+        public DateTime? PublicationDate_DT { get; set; } 
     }
 }
