d70a19d2a26cb51b593033ce62d27992486d7a67
gcnabeel <nabeel.bader@canada.ca>
2021-08-31 11:47:52 -0400

    language drop downs updated

---
+++ WebPortal/Data/Forms/LanguageTraining/Language_Constants.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NRCan.Datahub.Portal.Data.LanguageTraining
+{
+    public class Language_Constants
+    {
+        public static readonly string[] YESNO = { "Yes", "No" };
+
+        public static readonly string[] PROVINCES = { "NL",
+                                                    "PEI",
+                                                    "NS",
+                                                    "NB",
+                                                    "QUE",
+                                                    "ONT",
+                                                    "MAN",
+                                                    "SASK",
+                                                    "ALTA",
+                                                    "BC",
+                                                    "YT",
+                                                    "NWT",
+                                                    "NVT"
+        };
+
+        public static readonly string[] LANGUAGE_TRAINING = { "French Language Training", "English Language Training"};
+
+        public static readonly string[] LANGUAGE_TRAINING_PROVIDERS = { "NRCan Language School", "External Provider" };
+
+        public static readonly string[] EMPLOYMENT_STATUS = { "Indeterminate Employee", "Term Employee" };
+
+        public static readonly string[] SLESTATUS = { "X", "A", "B", "C", "E", "Not Applicable" };
+
+        public static readonly string[] QUARTERS = { "Q1 - April to June", "Q2 - July to September", "Q3 - October - December", "Q4 - January to March" };
+
+        public static readonly string[] TRAININGTYPE = { "Group Training - Full-Time Training", "Group Training - Part-Time Training", "Individual - Coaching" };
+
+        public static readonly string[] CLASSES_E = { "Beginner 1",
+                                                    "Beginner 2",
+                                                    "Beginner 3",
+                                                    "Intermediate 1 (BBB)",
+                                                    "Intermediate 2 (BBB)",
+                                                    "Intermediate 3 (BBB)",
+                                                    "Intermediate 4 (BBB)",
+                                                    "Intermediate 5 (BBB)",
+                                                    "Intermediate 6 (BBB)",
+                                                    "Intermediate 7 – Test Oral Proficiency – B level",
+                                                    "Advanced 1 (CBC)",
+                                                    "Advanced 2 (CBC)",
+                                                    "Advanced 3 (CBC)",
+                                                    "Advanced 4 (CBC)",
+                                                    "Advanced 5 (CBC)",
+                                                    "Advanced 6 – Test Oral Proficiency – C level"
+        };
+
+        public static readonly string[] CLASSES_F = {   "Intermediate 1 (BBB)",
+                                                        "Intermediate 2 (BBB)",
+                                                        "Intermediate 3 (BBB)",
+                                                        "Intermediate 4 (BBB)",
+                                                        "Intermediate 5 (BBB)",
+                                                        "Intermediate 6 (BBB) - Test Oral Proficiency – B level",
+                                                        "Advanced 1 (CBC)",
+                                                        "Advanced 2 (CBC)",
+                                                        "Advanced 3 (CBC)",
+                                                        "Advanced 4 (CBC)",
+                                                        "Advanced 5 (CBC)",
+                                                        "Advanced 6 (CBC) - Test Oral Proficiency – C level"                
+        };
+
+        public static readonly string[] LANGUAGE_SCHOOL_SELECTION = {
+                                                                    "Training accepted",
+                                                                    "Requires LETP assessment",
+                                                                    "Insufficient interest at level",
+                                                                    "Demand exceeds capacity",
+                                                                    "Late application"
+                                                                    };
+
+        public static readonly string[] SECTORSANDBRANCHES = { "AEB Audit Governance",
+                                                "AEB Audit Operations",
+                                                "AEB Chief Audit & Eval Exe Off",
+                                                "AEB Evaluation",
+                                                "CFS ADM's Office",
+                                                "CFS Atlantic Forestry Centre",
+                                                "CFS Cdn Wood Fibre Centre",
+                                                "CFS Great Lakes Forestry Centre",
+                                                "CFS Laurentian Forestry Centre",
+                                                "CFS Northern Forestry Centre",
+                                                "CFS Pacific Forestry Ctre",
+                                                "CFS Plan., Ops. & Info",
+                                                "CFS Science Pol & Integration",
+                                                "CFS Trade Economics & Industry",
+                                                "CMSS ADM's Office",
+                                                "CMSS Chief Info Off & Security",
+                                                "CMSS Finance & Procurement",
+                                                "CMSS Human Resources Branch",
+                                                "CMSS Planning & Operations",
+                                                "CMSS Real Prop & Workpl Serv",
+                                                "CPS ADM's Office",
+                                                "CPS Engagement & Digital Comms",
+                                                "CPS Management Services",
+                                                "CPS Portfol Mgmt & Corp Secr",
+                                                "CPS Public Affairs Branch",
+                                                "DMO",
+                                                "ETS ADM's Office",
+                                                "ETS CanmetENERGY Ottawa",
+                                                "ETS CanmetENERGY Varennes",
+                                                "ETS CanmetMATERIALS",
+                                                "ETS Office of Energy R&D",
+                                                "ETS Planning & Results",
+                                                "IARS",
+                                                "LCES International Energ",
+                                                "LCES ADM's Office",
+                                                "LCES Clean Fuels",
+                                                "LCES Electricity Resources",
+                                                "LCES Energy Policy & Intl Affa",
+                                                "LCES Office Energy Efficiency",
+                                                "LMS ADM's Office",
+                                                "LMS Bus Mgmt Serv & Data",
+                                                "LMS CanmetMINING",
+                                                "LMS Explos Safety & Secur",
+                                                "LMS Geological Survey Canada",
+                                                "LMS Hazards, Adaptation & Ops",
+                                                "LMS Policy & Economics",
+                                                "LMS Surveyor General Branch",
+                                                "LS Legal Services",
+                                                "MPMO ADM's Office",
+                                                "MPMO Ind Part Off West",
+                                                "MPMO Operations",
+                                                "MPMO Strategic Pol & Planning",
+                                                "OCS Environmental Assessment",
+                                                "OCS Impact Assess Sci Capacity",
+                                                "OCS Office of Chief Scientist",
+                                                "SPI ADM's Office",
+                                                "SPI Can Ctr Map & Earth Obs",
+                                                "SPI Econ Anal, Data & Res",
+                                                "SPI Ext Policy & Partner",
+                                                "SPI Innovation Branch",
+                                                "SPI PARDP",
+                                                "SPI Plan Delivery & Results",
+                                                "SPI Plan, Deliv & Results",
+                                                "SPI Strategic Policy",
+                                                "SPPIO ADM's Office",
+                                                "SPPIO CanmetENERGY-Devon",
+                                                "SPPIO Petroleum Resources",
+                                                "TMX - Special Projects"
+        };
+
+    }
+}
+
+
