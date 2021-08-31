1968e38095ee3d4379bffb6597db6985d0231930
Iroel Perez <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2021-08-31 11:38:02 -0400

    #2748 Audit Alerts first version and fix for the Recent Links issue

---
+++ Datahub.Shared/Services/IDatahubAuditingService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NRCan.Datahub.Shared.Services
+{
+    public interface IDatahubAuditingService
+    {
+        void TrackEvent(string eventName, params (string key, string value)[] properties);
+        void TrackDataEvent(string eventName, string eventData, params (string key, string value)[] properties);
+        void TrackException(Exception exception, params (string key, string value)[] properties);
+    }
+}
