ec697e2d86b1ed8e7a6fea384680e88273afb712
Erik Putrycz <erik.putrycz@nrcan-rncan.gc.ca>
2021-08-31 18:24:05 -0400

    added toggle for featured + reworked localization in project page

--- Datahub.Shared/Model/Datahub/Datahub_Project.cs
+++ Datahub.Shared/Model/Datahub/Datahub_Project.cs
@@ -59,6 +59,9 @@ namespace NRCan.Datahub.Shared.EFCore
         public bool IsDataApprover { get; set; }
         public Datahub_Project Project { get; set; }
 
+        //foreign key
+        public int ProjectId { get; set; }
+
         [Timestamp]
         public byte[] Timestamp { get; set; }
 
