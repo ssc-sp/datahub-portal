1e6c8337a80600bcb0b9de842cecfc85a5fa3951
gcnabeel <nabeel.bader@canada.ca>
2021-09-08 14:09:19 -0400

    fixed migration issue

--- Datahub.Shared/Migrations/Core/20210823161941_AddingProjectCostsTable.cs
+++ Datahub.Shared/Migrations/Core/20210823161941_AddingProjectCostsTable.cs
@@ -7,12 +7,12 @@ namespace NRCan.Datahub.Shared.Migrations.Core
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<DateTime>(
-                name: "Requested_DT",
-                table: "Project_Users_Requests",
-                type: "datetime2",
-                nullable: false,
-                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+            //migrationBuilder.AddColumn<DateTime>(
+            //    name: "Requested_DT",
+            //    table: "Project_Users_Requests",
+            //    type: "datetime2",
+            //    nullable: false,
+            //    defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
 
             migrationBuilder.CreateTable(
                 name: "Project_Costs",
