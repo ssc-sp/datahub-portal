9351cd542f4614a7fb1b4ebb975e6fe2f7b72cdf
Erik Putrycz <erik.putrycz@nrcan-rncan.gc.ca>
2021-09-09 16:33:30 -0400

    added deepl translation API integration for project title and description

--- WebPortal/Startup.cs
+++ WebPortal/Startup.cs
@@ -383,6 +383,7 @@ namespace NRCan.Datahub.Portal
 
             services.AddScoped<DataImportingService>();
             services.AddSingleton<DatahubTools>();
+            services.AddSingleton<TranslationService>();
 
             services.AddScoped<NotificationsService>();
             services.AddScoped<UIControlsService>();
