d9cd2f6b466b04a06e3ab92c0f7ca1126f3439f4
Erik Putrycz <erik.putrycz@nrcan-rncan.gc.ca>
2021-09-10 17:09:50 -0400

    refactored language training out of main project

---
+++ LanguageTraining/LanguageTrainingModule.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NRCan.Datahub.Portal.Data.LanguageTraining;
+using NRCan.Datahub.Shared;
+using NRCan.Datahub.Shared.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.LanguageTraining
+{
+    public class LanguageTrainingModule : IDatahubModule
+    {
+        public void ConfigureDatabases(IServiceCollection serviceProvider, IConfiguration configuration)
+        {
+            serviceProvider.ConfigureDbContext<LanguageTrainingDBContext>(configuration, "datahub-mssql-languagetraining", configuration.GetDriver());
+        }
+
+        public void InitializeDatabases(DatahubModuleContext ctx)
+        {
+            ctx.InitializeDatabase<LanguageTrainingDBContext>();
+        }
+    }
+}
