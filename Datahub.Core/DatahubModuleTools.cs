e89896059d2461219f2e8c4c0c9d6b20e89713c9
Erik Putrycz <erik.putrycz@nrcan-rncan.gc.ca>
2021-09-10 17:09:50 -0400

    refactored language training out of main project

---
+++ Datahub.Core/DatahubModuleTools.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NRCan.Datahub.Shared
+{
+    public static class DatahubModuleTools
+    {
+        public static void AddModule<T>(this IServiceCollection services, IConfiguration configuration) where T:IDatahubModule
+        {
+            var module = (T)Activator.CreateInstance(typeof(T));            
+            module.ConfigureDatabases(services, configuration);
+
+        }
+
+        public static void ConfigureModule<T>(this IApplicationBuilder builder) where T : IDatahubModule
+        {
+            var offline = false;            
+            var module = (T) Activator.CreateInstance(typeof(T));
+            var settings = builder.ApplicationServices.GetService(typeof(IConfiguration));
+            var context = new DatahubModuleContext(builder.ApplicationServices, offline);
+            module.InitializeDatabases(context);
+        }
+    }
+}
