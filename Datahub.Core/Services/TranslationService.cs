3bb4f72fa60bd96909a41115d40d70f4aebbfeba
Erik Putrycz <erik.putrycz@nrcan-rncan.gc.ca>
2021-09-09 22:28:40 -0400

    added missing translations

--- Datahub.Core/Services/TranslationService.cs
+++ Datahub.Core/Services/TranslationService.cs
@@ -1,4 +1,5 @@
 ﻿using DeepL;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,15 @@ namespace NRCan.Datahub.Shared.Services
 {
     public class TranslationService
     {
-        DeepLClient client = new DeepLClient("44a39371-3099-1bdf-f50a-2410747fbfd5", useFreeApi: true);
 
+        DeepLClient client;
+
+        public TranslationService(IConfiguration configuration)
+        {
+            var useFreeApi = configuration.GetSection("DeepL").GetValue<bool>("UseFreeApi");
+            var authKey = configuration.GetSection("DeepL").GetValue<string>("AuthKey");
+            client = new DeepLClient(authKey, useFreeApi);
+        }
         public async Task<string> GetFrenchTranslation(string englishText)
         {
             var translation = await client.TranslateAsync(
