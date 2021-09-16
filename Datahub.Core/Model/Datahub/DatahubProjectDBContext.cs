84dbf8e426b69b8a333e46ebae787ccd21ff8965
Erik Putrycz <erik.putrycz@nrcan-rncan.gc.ca>
2021-09-16 10:09:23 -0400

    minor cleanup

--- Datahub.Core/Model/Datahub/DatahubProjectDBContext.cs
+++ Datahub.Core/Model/Datahub/DatahubProjectDBContext.cs
@@ -18,32 +18,21 @@ namespace NRCan.Datahub.Shared.EFCore
         }
 
         public DbSet<Datahub_Project> Projects { get; set; }
-
         public DbSet<PBI_License_Request> PowerBI_License_Requests { get; set; }
-
         public DbSet<PBI_User_License_Request> PowerBI_License_User_Requests { get; set; }
-
         public DbSet<Datahub_ProjectComment> Project_Comments { get; set; }
-
         public DbSet<Datahub_Project_Access_Request> Access_Requests { get; set; }
-
         public DbSet<WebForm_Field> Fields { get; set; }
-
         public DbSet<WebForm> WebForms { get; set; }
-
         public DbSet<WebForm_DBCodes> DBCodes { get; set; }
-
         public DbSet<Datahub_Project_User> Project_Users { get; set; }
         public DbSet<Datahub_Project_User_Request> Project_Users_Requests { get; set; }
         public DbSet<Datahub_ProjectServiceRequests> Project_Requests { get; set; }
-
         public DbSet<Datahub_Project_Pipeline_Lnk> Project_Pipeline_Links { get; set; }
-
         public DbSet<Project_Database> Project_Databases { get; set; }
         public DbSet<Project_PBI_DataSet> Project_PBI_DataSets { get; set; }
         public DbSet<Project_PBI_Report> Project_PBI_Reports { get; set; }
         public DbSet<Project_PBI_Workspace> Project_PBI_Workspaces { get; set; }
-
         public DbSet<PublicDataFile> PublicDataFiles { get; set; }
 
         public DbSet<SharedDataFile> SharedDataFiles { get; set; }
