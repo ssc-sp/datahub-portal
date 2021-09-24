981a3fc6681834fdeb34dc577960b1d6df736569
gcnabeel <nabeel.bader@canada.ca>
2021-09-23 22:14:39 -0400

    added placeholders text

--- Datahub.Core/Model/Onboarding/OnboardingApp.cs
+++ Datahub.Core/Model/Onboarding/OnboardingApp.cs
@@ -15,45 +15,54 @@ namespace NRCan.Datahub.Shared.Model.Onboarding
         public int Application_ID { get; set; }
         [AeFormCategory("Client Information", 10)]
         [Required]
-        [AeLabel(isDropDown: true)]
+        [AeLabel(isDropDown: true, placeholder: "[Enter your Sector acronym and/or name]")]
         [StringLength(2000)]
         public string Client_Sector { get; set; }
         [StringLength(2000)]
         [AeFormCategory("Client Information", 10)]
         [Required]
-        [AeLabel(isDropDown: true)]
+        [AeLabel(isDropDown: true, placeholder: "[Enter your Branch acronym and/or name]")]
         public string Client_Branch { get; set; }
         [StringLength(2000)]
         [AeFormCategory("Client Information", 10)]
         [Required]
+        [AeLabel(placeholder: "[Enter your Division acronym and/or name]")]
         public string Client_Division { get; set; }
         [StringLength(200)]
         [AeFormCategory("Client Information", 10)]
         [Required]
+        [AeLabel(placeholder: "[Lastname, Firstname]")]
         public string Client_Contact_Name { get; set; }
         [StringLength(200)]
         [AeFormCategory("Client Information", 10)]
         [Required]
+        [AeLabel(placeholder: "[Firstname.Lastname@nrcan-rncan.gc.ca]")]
         public string Client_Email { get; set; }
         [StringLength(200)]
         [AeFormCategory("Client Information", 10)]
+        [AeLabel(placeholder: "[Lastname, Firstname]")]
         public string Additional_Contact_Name { get; set; }
         [StringLength(200)]
         [AeFormCategory("Client Information", 10)]
+        [AeLabel(placeholder: "[Firstname.Lastname@nrcan-rncan.gc.ca]")]
         public string Additional_Contact_Email_EMAIL { get; set; }
 
 
         [AeFormCategory("Project Information", 20)]
+        [AeLabel(placeholder: "[Enter the name of your project]")]
         public string Project_Name { get; set; }
 
 
         [AeFormCategory("Project Information", 20)]
+        [AeLabel(placeholder: "[Provide a brief summary/description of your project]")]
         public string Project_Summary_Description { get; set; }
 
         [AeFormCategory("Project Information", 20)]
+        [AeLabel(placeholder: "[Provide a brief description of the objective(s) you would like to accomplish with DataHub]")]
         public string Project_Goal { get; set; }
 
         [AeFormCategory("Project Information", 20)]
+        [AeLabel(placeholder: "[Provide any anticipated timelines or deadlines for onboarding to DataHub]")]
         public string Onboarding_Timeline { get; set; }
         
 
@@ -94,8 +103,10 @@ namespace NRCan.Datahub.Shared.Model.Onboarding
 
 
         [AeFormCategory("Additional Information", 30)]
+        [AeLabel(placeholder: "[Enter any additional information/comments regarding your project or questions for the DataHub team]")]
         public string Questions_for_the_DataHub_Team { get; set; }
 
+        [AeLabel(placeholder: "[Insert any attachments and/or links to any folders or documentation or sample data or artifacts related to your project]")]
         [AeFormCategory("Additional Information", 30)]
         public string Attachments { get; set; }
 
