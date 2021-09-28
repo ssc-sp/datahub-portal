d4dc45a5a127709f13db1c70d63dbd13cd722d97
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2021-09-28 15:02:27 -0400

    #2897 implementing the Opend Data package generator

---
+++ Datahub.Metadata.Tests/FieldValueContainerTests.cs
@@ -0,0 +1,84 @@
+﻿using NRCan.Datahub.Metadata.DTO;
+using NRCan.Datahub.Metadata.Model;
+using System.Linq;
+using Xunit;
+
+namespace NRCan.Datahub.Metadata.Tests
+{
+    public class FieldValueContainerTests
+    {
+        [Fact]
+        public void FieldValueContainer_AddOrUpdate_MustAddNewField()
+        {
+            var definitions = GetDefinitions(("name", true), ("new_field", false), ("expected", true));
+            var container = GetFieldValueContainer(definitions, ("name", "any name"), ("expected", "any value"));
+
+            var expected = "expected";
+            Assert.True(container.AddOrUpdate("new_field", expected));
+
+            var field = container["new_field"];
+            Assert.NotNull(field);
+            Assert.Equal(expected, field.Value_TXT);
+        }
+
+        [Fact]
+        public void FieldValueContainer_AddOrUpdate_MustUpdateNewField()
+        {
+            var definitions = GetDefinitions(("name", true), ("existing_field", false), ("expected", true));
+            var container = GetFieldValueContainer(definitions, ("name", "any name"), ("existing_field", "any value"));
+
+            var expected = "expected";
+            Assert.False(container.AddOrUpdate("existing_field", expected));
+
+            var field = container["existing_field"];
+            Assert.NotNull(field);
+            Assert.Equal(expected, field.Value_TXT);
+        }
+
+        [Fact]
+        public void FieldValueContainer_ValidateRequired_MustSucceedWhenAllRequiredFieldsEntered()
+        {
+            var definitions = GetDefinitions(("name", true), ("ignored", false), ("expected", true));
+            var container = GetFieldValueContainer(definitions, ("name", "any name"), ("expected", "any value"));
+
+            var actual = container.ValidateRequired();
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void FieldValueContainer_ValidateRequired_MustFailWhenARequiredFieldsIsNotEntered()
+        {
+            var definitions = GetDefinitions(("name", true), ("not_ignored", true), ("expected", true));
+            var container = GetFieldValueContainer(definitions, ("name", "any name"), ("expected", "any value"));
+
+            var actual = container.ValidateRequired();
+
+            Assert.False(actual);
+        }
+
+        static FieldDefinitions GetDefinitions(params (string name, bool required)[] fields)
+        {
+            FieldDefinitions definitions = new();
+            var id = 1;
+            definitions.Add(fields.Select(f => new FieldDefinition()
+            {
+                FieldDefinitionId = id++,
+                Field_Name_TXT = f.name,
+                Required_FLAG = f.required,
+                English_DESC = f.name,
+                French_DESC = f.name                
+            }));
+            return definitions;
+        }
+
+        static FieldValueContainer GetFieldValueContainer(FieldDefinitions definitions, params (string name, string value)[] fieldValues)
+        {
+            return new FieldValueContainer(definitions, fieldValues.Select(fv => new ObjectFieldValue() 
+            { 
+                FieldDefinitionId = definitions.Get(fv.name).FieldDefinitionId, 
+                Value_TXT = fv.value 
+            }));
+        }
+    }
+}
---
+++ Datahub.Metadata/CKAN/PackageGenerator.cs
@@ -0,0 +1,76 @@
+﻿using NRCan.Datahub.Metadata.DTO;
+using NRCan.Datahub.Metadata.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NRCan.Datahub.Metadata.CKAN
+{
+    public class PackageGenerator
+    {
+        readonly List<FieldAgent> _fieldAgents;
+
+        public PackageGenerator()
+        {
+            _fieldAgents = new()
+            {
+                new KeywordFieldAgent(),
+                new TranslatedFieldAgent(),
+                new CatchAllFieldAgent(true),
+                new CatchAllFieldAgent(false)
+            };
+        }
+
+        public Dictionary<string, object> GeneratePackage(FieldValueContainer fieldValues, bool @private = false)
+        {
+            Dictionary<string, object> dict = new();
+
+            // package id
+            dict["id"] = fieldValues["id"]?.Value_TXT;
+
+            // package name
+            dict["name"] = fieldValues["name"]?.Value_TXT;
+
+            // take title english for general title
+            dict["title"] = fieldValues["title_translated_en"]?.Value_TXT;
+
+            // private
+            dict["private"] = @private;
+
+            // version
+            //dict["version"] = null;
+
+            // state is active
+            dict["state"] = "active";
+
+            // type is dataset
+            dict["type"] = "dataset";
+
+            // no groups
+            //dict["groups"] = Array.Empty<object>();
+
+            var agents = InstanciateAgents(fieldValues).ToList();
+            foreach (var agent in agents)
+            {
+                agent.RenderField(dict);
+            }
+
+            return dict;
+        }
+
+        private IEnumerable<FieldAgent> InstanciateAgents(IEnumerable<ObjectFieldValue> fieldValues)
+        {
+            foreach (var fv in fieldValues)
+            {
+                var definition = fv.FieldDefinition;
+                var matchingAgent = _fieldAgents.FirstOrDefault(a => a.Matches(definition));
+
+                var tuple = matchingAgent.Instanciate(definition.Field_Name_TXT, fv.Value_TXT);
+                if (tuple.append)
+                {
+                    yield return tuple.agent;
+                }
+            }
+        }
+    }
+}
