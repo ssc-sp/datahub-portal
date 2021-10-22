ab5647f4cfd22eecb554d70d7a0813ed7cdeca41
Erik Putrycz <erik.putrycz@nrcan-rncan.gc.ca>
2021-10-22 10:49:00 -0400

    hotfix to prevent errors in claim transformers to break all pages

--- Datahub.Core/RoleManagement/RoleClaimTransformer.cs
+++ Datahub.Core/RoleManagement/RoleClaimTransformer.cs
@@ -1,47 +1,57 @@
 ﻿using Microsoft.AspNetCore.Authentication;
-using NRCan.Datahub.Portal.Services;
-using NRCan.Datahub.Shared.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NRCan.Datahub.Shared.Services;
 
-namespace NRCan.Datahub.Shared.RoleManagement
+namespace Datahub.Core.RoleManagement
 {
 
     //https://stackoverflow.com/questions/58483620/net-core-3-0-claimstransformation
     public class RoleClaimTransformer : IClaimsTransformation
     {
         private readonly ServiceAuthManager serviceAuthManager;
+        private readonly ILogger<RoleClaimTransformer> logger;
 
-        public RoleClaimTransformer(ServiceAuthManager serviceAuthManager)
+        public RoleClaimTransformer(ServiceAuthManager serviceAuthManager, ILogger<RoleClaimTransformer> logger)
         {
             this.serviceAuthManager = serviceAuthManager;
+            this.logger = logger;
         }
 
         public async Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
         {
-            var userName = principal.Identity.Name;
-            var userId = principal.Claims.ToList()[9].Value;
-            
-            var allProjects = serviceAuthManager.GetAllProjects();
-            
-            var authorizedProjects = await serviceAuthManager.GetUserAuthorizations(userId);
-
-            foreach (var project in allProjects)
+            try
             {
-                if (await serviceAuthManager.IsProjectAdmin(userId, project))
+                var userName = principal.Identity.Name;
+                var userId = principal.Claims.ToList()[9].Value;
+
+                var allProjects = serviceAuthManager.GetAllProjects();
+
+                var authorizedProjects = await serviceAuthManager.GetUserAuthorizations(userId);
+
+                foreach (var project in allProjects)
                 {
-                    ((ClaimsIdentity)principal.Identity).AddClaim(new Claim(ClaimTypes.Role, $"{project}-admin"));
+                    if (await serviceAuthManager.IsProjectAdmin(userId, project))
+                    {
+                        ((ClaimsIdentity)principal.Identity).AddClaim(new Claim(ClaimTypes.Role, $"{project}-admin"));
+                    }
                 }
-            }
-            foreach (var project in authorizedProjects)
-            {
-                ((ClaimsIdentity)principal.Identity).AddClaim(new Claim(ClaimTypes.Role, project.Project_Acronym_CD));
-            }
+                foreach (var project in authorizedProjects)
+                {
+                    ((ClaimsIdentity)principal.Identity).AddClaim(new Claim(ClaimTypes.Role, project.Project_Acronym_CD));
+                }
 
+            }
+            catch (Exception ex)
+            {
+                logger.LogCritical(ex, "Cannot load project permissions");
+            }
             return principal;
+
         }
 
     }
