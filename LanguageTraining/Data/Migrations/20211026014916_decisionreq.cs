86f11a6ab3a2798290ad80916dbfd76077d45777
gcnabeel <nabeel.bader@canada.ca>
2021-10-25 22:55:50 -0400

    language email fixes

---
+++ LanguageTraining/Data/Migrations/20211026014916_decisionreq.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Datahub.Portal.Migrations
+{
+    public partial class decisionreq : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Decision",
+                table: "LanguageTrainingApplications",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Decision",
+                table: "LanguageTrainingApplications",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
