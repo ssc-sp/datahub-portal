7cfb284f3f2714daf5383e57c80b455406ce0880
Erik Putrycz <erik.putrycz@nrcan-rncan.gc.ca>
2021-10-28 11:43:48 -0400

    added gulp and powerbi embed.js

---
+++ WebPortal/gulpfile.js
@@ -0,0 +1,18 @@
+﻿var gulp = require('gulp'),
+    cache = require('gulp-cached'); //If cached version identical to current file then it doesn't pass it downstream so this file won't be copied 
+
+gulp.task('default', 'copy-node_modules');
+
+gulp.task('copy-node_modules', function () {
+
+    try {
+
+        gulp.src('node_modules/**')
+            .pipe(cache('node_modules'))
+            .pipe(gulp.dest('wwwroot/node_modules'));
+    }
+    catch (e) {
+        return -1;
+    }
+    return 0;
+});
