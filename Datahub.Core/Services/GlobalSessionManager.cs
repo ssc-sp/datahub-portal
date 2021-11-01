ec28395ea34b0696bd6da6f68eba12babe7688ea
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2021-11-01 13:45:51 -0400

    #2888 limiting the number of sessions per user in Datahub

---
+++ Datahub.Core/Services/GlobalSessionManager.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Datahub.Core.Services
+{
+    public class GlobalSessionManager : IGlobalSessionManager
+    {
+        private readonly Dictionary<string, int> _sessions;
+        private readonly int _maxSessions;
+        
+        public GlobalSessionManager(IOptions<SessionsConfig> config)
+        {
+            _sessions = new();
+            _maxSessions = config.Value?.MaxSessionsPerUser ?? -1;
+        }
+
+        public bool TryAddSession(string userId)
+        {
+            lock (_sessions)
+            {
+                var count = 0;
+                if (_sessions.TryGetValue(userId, out count))
+                {
+                    if (_maxSessions > 0 && count >= _maxSessions)
+                        return false;
+                }
+                _sessions[userId] = count + 1;
+                return true;
+            }
+        }
+
+        public void RemoveSession(string userId)
+        {
+            lock (_sessions)
+            {
+                if (_sessions.TryGetValue(userId, out int count))
+                {
+                    _sessions[userId] = Math.Max(0, count - 1);
+                }
+            }
+        }
+
+        public int GetSessionCount(string userId)
+        {
+            lock (_sessions)
+            {
+                return _sessions.TryGetValue(userId, out int count) ? count : 0;
+            }
+        }
+    }
+
+    public class SessionsConfig
+    {
+        public int MaxSessionsPerUser { get; set; }
+    }
+}
