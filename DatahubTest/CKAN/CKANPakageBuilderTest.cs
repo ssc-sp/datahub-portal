fcf7cf7f11b62d61ea2ed6dbab07c8007f77da4b
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2021-11-05 13:56:57 -0400

    #2898 Refactoring the opendata json package

--- DatahubTest/CKAN/CKANPakageBuilderTest.cs
+++ DatahubTest/CKAN/CKANPakageBuilderTest.cs
@@ -24,7 +24,7 @@ namespace Datahub.Tests.CKAN
             var fieldValues = LoadFields(_fieldDefinitions);
             Assert.NotNull(fieldValues);
 
-            var dict = PackageGenerator.GeneratePackage(fieldValues);
+            var dict = (new PackageGenerator()).GeneratePackage(fieldValues, "url_goes_here");
 
             var expected = fieldValues.ObjectId;
 
@@ -47,6 +47,26 @@ namespace Datahub.Tests.CKAN
             var title_translated = (Dictionary<string, string>)dict["title_translated"];
             Assert.NotNull(title_translated["en"]);
             Assert.NotNull(title_translated["fr"]);
+
+            // notes translated exists and contains the en & fr entries
+            Assert.NotNull(dict["notes_translated"]);
+            var notes_translated = (Dictionary<string, string>)dict["notes_translated"];
+            Assert.NotNull(notes_translated["en"]);
+            Assert.NotNull(notes_translated["fr"]);
+
+            // resources exists
+            Assert.NotNull(dict["resources"]);
+            var resources = (object[])dict["resources"];
+            Assert.True(resources.Length == 1);
+            
+            var resource1 = resources[0] as Dictionary<string, object>;
+            Assert.NotNull(resource1);
+
+            // name_translated exists and contains the en & fr entries
+            Assert.NotNull(resource1["name_translated"]);
+            var name_translated = (Dictionary<string, string>)resource1["name_translated"];
+            Assert.NotNull(name_translated["en"]);
+            Assert.NotNull(name_translated["fr"]);
 
             var json = JsonConvert.SerializeObject(dict);
 
