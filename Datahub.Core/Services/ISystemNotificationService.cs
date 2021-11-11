9d94c5cb74c06f857e9958618296dc03ee7f2888
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2021-11-11 14:25:24 -0500

    System notifications - basic functionality

---
+++ Datahub.Core/Services/ISystemNotificationService.cs
@@ -0,0 +1,27 @@
+﻿using Datahub.Core.EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.Core.Services
+{
+    public interface ISystemNotificationService
+    {
+        Task<int> CreateSystemNotifications(List<string> userIds, string textKey, params string[] arguments);
+        Task<int> CreateSystemNotification(string userId, string textKey, params string[] arguments) =>
+            CreateSystemNotifications(new List<string>() { userId }, textKey, arguments);
+
+        Task<int> CreateSystemNotificationsWithLink(List<string> userIds, string actionLink, string textKey, params string[] arguments);
+        Task<int> CreateSystemNotificationWithLink(string userId, string actionLink, string textKey, params string[] arguments) =>
+            CreateSystemNotificationsWithLink(new List<string>() {  userId }, actionLink, textKey, arguments);
+
+        Task<int> GetNotificationCountForUser(string userId, bool unreadOnly = false);
+        Task<List<SystemNotification>> GetNotificationsForUser(string userId, bool unreadOnly = false);
+
+        Task SetReadStatus(long notificationId, bool readStatus);
+
+        event Func<string, Task> Notify;
+    }
+}
---
+++ Datahub.Core/Services/SystemNotificationService.cs
@@ -0,0 +1,133 @@
+﻿using Datahub.Core.EFCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.Core.Services
+{
+    public class SystemNotificationService : ISystemNotificationService
+    {
+        private readonly IDbContextFactory<DatahubProjectDBContext> _dbContextFactory;
+        private readonly IStringLocalizer<SystemNotificationService> _localizer;
+        private readonly ILogger<SystemNotificationService> _logger;
+
+        private readonly CultureInfo enCulture;
+        private readonly CultureInfo frCulture;
+
+        public event Func<string, Task> Notify;
+
+        public SystemNotificationService(
+            IDbContextFactory<DatahubProjectDBContext> dbContextFactory,
+            IStringLocalizer<SystemNotificationService> localizer,
+            ILogger<SystemNotificationService> logger
+            )
+        {
+            _dbContextFactory = dbContextFactory;
+            _localizer = localizer;
+            _logger = logger;
+
+            enCulture = new CultureInfo("en-ca");
+            frCulture = new CultureInfo("fr-ca");
+        }
+
+        private string GetLocalizedString(CultureInfo culture, string textKey, string[] arguments)
+        {
+            var currentThread = System.Threading.Thread.CurrentThread;
+            var oldCulture = currentThread.CurrentUICulture;
+            currentThread.CurrentUICulture = culture;
+            var localizedText = _localizer[textKey, arguments];
+            currentThread.CurrentUICulture = oldCulture;
+            return localizedText;
+        }
+
+        public async Task<int> CreateSystemNotifications(List<string> userIds, string textKey, params string[] arguments)
+        {
+            return await DoCreateSystemNotifications(userIds, textKey, arguments);
+        }
+        public async Task<int> CreateSystemNotificationsWithLink(List<string> userIds, string actionLink, string textKey, params string[] arguments)
+        {
+            return await DoCreateSystemNotifications(userIds, textKey, arguments, actionLink);
+        }
+
+        private async Task<int> DoCreateSystemNotifications(List<string> userIds, string textKey, string[] arguments, string actionLink = null)
+        {
+            var textEn = GetLocalizedString(enCulture, textKey, arguments);
+            var textFr = GetLocalizedString(frCulture, textKey, arguments);
+            var now = DateTime.UtcNow;
+
+            var notifications = userIds
+                .Select(userId => new SystemNotification()
+                {
+                    Generated_TS = now,
+                    NotificationTextEn_TXT = textEn,
+                    NotificationTextFr_TXT = textFr,
+                    ReceivingUser_ID = userId,
+                    Read_FLAG = false,
+                    ActionLink_URL = actionLink
+                });
+
+            using var ctx = _dbContextFactory.CreateDbContext();
+            await ctx.SystemNotifications.AddRangeAsync(notifications);
+            var result = await ctx.SaveChangesAsync();
+
+            await NotifyUsers(userIds);
+
+            return result;
+        }
+
+        private async Task NotifyUsers(List<string> userIds)
+        {
+            if (Notify != null)
+            {
+                var tasks = userIds.Select(u => Notify.Invoke(u));
+                await Task.WhenAll(tasks);
+            }
+        }
+
+        private static Expression<Func<SystemNotification, bool>> CreateUserNotificationCondition(string userId, bool unreadOnly)
+        {
+            var lowerUserId = userId.ToLower();
+            return (unreadOnly ?
+                n => n.ReceivingUser_ID.ToLower() == userId && !n.Read_FLAG :
+                n => n.ReceivingUser_ID.ToLower() == userId);
+        }
+
+        public async Task<int> GetNotificationCountForUser(string userId, bool unreadOnly = false)
+        {
+            var condition = CreateUserNotificationCondition(userId, unreadOnly);
+
+            using var ctx = _dbContextFactory.CreateDbContext();
+            return await ctx.SystemNotifications.CountAsync(condition);
+        }
+
+        public async Task<List<SystemNotification>> GetNotificationsForUser(string userId, bool unreadOnly = false)
+        {
+            var condition = CreateUserNotificationCondition(userId, unreadOnly);
+
+            using var ctx = _dbContextFactory.CreateDbContext();
+            return await ctx.SystemNotifications
+                .Where(condition)
+                .OrderByDescending(n => n.Generated_TS)
+                .ToListAsync();
+        }
+
+        public async Task SetReadStatus(long notificationId, bool readStatus)
+        {
+            using var ctx = _dbContextFactory.CreateDbContext();
+            var notification = await ctx.SystemNotifications.FirstOrDefaultAsync(n => n.Notification_ID == notificationId);
+            if (notification != null)
+            {
+                notification.Read_FLAG = readStatus;
+                await ctx.SaveChangesAsync();
+                await NotifyUsers(new List<string>() { notification.ReceivingUser_ID });
+            }
+        }
+    }
+}
