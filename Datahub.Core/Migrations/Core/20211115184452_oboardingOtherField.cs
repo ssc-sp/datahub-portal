26ac433f61b3eb3110389a9703c1df69a709796a
gcnabeel <nabeel.bader@canada.ca>
2021-11-15 14:17:43 -0500

    added other column to onboarding

---
+++ Datahub.Core/Migrations/Core/20211115184452_oboardingOtherField.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Datahub.Core.Migrations.Core
+{
+    public partial class oboardingOtherField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Project_Engagement_Category_Other",
+                table: "OnboardingApps",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Project_Engagement_Category_Other",
+                table: "OnboardingApps");
+        }
+    }
+}
