9270d114e8d1174a6387bccd667efe62a46d4b1b
gcnabeel <nabeel.bader@canada.ca>
2021-11-15 12:10:27 -0500

    onboarding updates

--- Datahub.Core/Migrations/Core/20211111195647_onboardingupdates.Designer.cs
+++ Datahub.Core/Migrations/Core/20211111195647_onboardingupdates.Designer.cs
@@ -4,14 +4,16 @@ using Datahub.Core.EFCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace Datahub.Portal.Migrations.Forms.DatahubProjectDB
+namespace Datahub.Core.Migrations.Core
 {
     [DbContext(typeof(DatahubProjectDBContext))]
-    partial class DatahubProjectDBContextModelSnapshot : ModelSnapshot
+    [Migration("20211111195647_onboardingupdates")]
+    partial class onboardingupdates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
