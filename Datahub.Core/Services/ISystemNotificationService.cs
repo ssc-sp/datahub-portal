12e215ee8e5810dda0899d3e65d098eeb8f80bf8
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2021-11-15 21:04:35 -0500

    Localizable arguments in notifications

--- Datahub.Core/Services/ISystemNotificationService.cs
+++ Datahub.Core/Services/ISystemNotificationService.cs
@@ -9,12 +9,12 @@ namespace Datahub.Core.Services
 {
     public interface ISystemNotificationService
     {
-        Task<int> CreateSystemNotifications(List<string> userIds, string textKey, params string[] arguments);
-        Task<int> CreateSystemNotification(string userId, string textKey, params string[] arguments) =>
+        Task<int> CreateSystemNotifications(List<string> userIds, string textKey, params object[] arguments);
+        Task<int> CreateSystemNotification(string userId, string textKey, params object[] arguments) =>
             CreateSystemNotifications(new List<string>() { userId }, textKey, arguments);
 
-        Task<int> CreateSystemNotificationsWithLink(List<string> userIds, string actionLink, string textKey, params string[] arguments);
-        Task<int> CreateSystemNotificationWithLink(string userId, string actionLink, string textKey, params string[] arguments) =>
+        Task<int> CreateSystemNotificationsWithLink(List<string> userIds, string actionLink, string textKey, params object[] arguments);
+        Task<int> CreateSystemNotificationWithLink(string userId, string actionLink, string textKey, params object[] arguments) =>
             CreateSystemNotificationsWithLink(new List<string>() {  userId }, actionLink, textKey, arguments);
 
         Task<int> GetNotificationCountForUser(string userId, bool unreadOnly = false);
@@ -26,4 +26,8 @@ namespace Datahub.Core.Services
 
         event Func<string, Task> Notify;
     }
+
+    // These classes (or similar) may be useful in a more general context
+    public record BilingualStringArgument(string English, string French);
+    public record LocalizedKeyStringArgument(string Key);
 }
