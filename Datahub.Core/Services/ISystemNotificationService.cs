3ee54719aad49eea1868ccd5c3e307572b886b3f
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2021-11-17 14:41:03 -0500

    Generate notifications with labelled action links

--- Datahub.Core/Services/ISystemNotificationService.cs
+++ Datahub.Core/Services/ISystemNotificationService.cs
@@ -13,9 +13,13 @@ namespace Datahub.Core.Services
         Task<int> CreateSystemNotification(string userId, string textKey, params object[] arguments) =>
             CreateSystemNotifications(new List<string>() { userId }, textKey, arguments);
 
-        Task<int> CreateSystemNotificationsWithLink(List<string> userIds, string actionLink, string textKey, params object[] arguments);
+        Task<int> CreateSystemNotificationsWithLink(List<string> userIds, string actionLink, string linkKey, string textKey, params object[] arguments);
+        Task<int> CreateSystemNotificationWithLink(string userId, string actionLink, string linkKey, string textKey, params object[] arguments) =>
+            CreateSystemNotificationsWithLink(new List<string>() {  userId }, actionLink, linkKey, textKey, arguments);
+        Task<int> CreateSystemNotificationsWithLink(List<string> userIds, string actionLink, string textKey, params object[] arguments) =>
+            CreateSystemNotificationsWithLink(userIds, actionLink, null, textKey, arguments);
         Task<int> CreateSystemNotificationWithLink(string userId, string actionLink, string textKey, params object[] arguments) =>
-            CreateSystemNotificationsWithLink(new List<string>() {  userId }, actionLink, textKey, arguments);
+            CreateSystemNotificationsWithLink(new List<string>() { userId }, actionLink, null, textKey, arguments);
 
         Task<int> GetNotificationCountForUser(string userId, bool unreadOnly = false);
         Task<List<SystemNotification>> GetNotificationsForUser(string userId, bool unreadOnly = false, int pageNumber = 0);
