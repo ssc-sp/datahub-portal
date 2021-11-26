d1cca438f514831d2aaf4ca734b1d3caacf441a3
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2021-11-26 09:29:37 -0500

    #3051 implementing the URL publishing option

---
+++ Datahub.Core/Migrations/Core/20211125155937_AddFileShareStorageField.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Datahub.Core.Migrations.Core
+{
+    public partial class AddFileShareStorageField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FileStorage_CD",
+                table: "OpenDataSharedFile",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileStorage_CD",
+                table: "OpenDataSharedFile");
+        }
+    }
+}
