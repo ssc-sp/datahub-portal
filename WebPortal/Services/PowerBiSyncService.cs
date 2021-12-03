a9b8a655416c2dadf1d776a33a7d4c42c4e5cd7c
Erik Putrycz <erik.putrycz@nrcan-rncan.gc.ca>
2021-12-03 00:44:19 -0500

    updated schema and methods

--- WebPortal/Services/PowerBiSyncService.cs
+++ WebPortal/Services/PowerBiSyncService.cs
@@ -15,21 +15,33 @@ namespace Datahub.Portal.Services
             dbFactoryProject = DbFactoryProject;
         }
         
-        public Project_PBI_Workspace? GetWorkspace(Group group)
-        {
-            using var ctx = dbFactoryProject.CreateDbContext();
-            var workspace = ctx.Find<Project_PBI_Workspace>(group.Id);
-            if (workspace == null)
-            {
-
-            }
-        }
-
-        public Project_PBI_Report? GetReport(Report report, Datahub_Project project, Group workspace, DatahubProjectDBContext current_ctx = null)
+        public Project_PBI_Workspace? GetWorkspace(Group group, Datahub_Project project, DatahubProjectDBContext current_ctx = null)
         {
             using var local_ctx = dbFactoryProject.CreateDbContext();
             var ctx = current_ctx ?? local_ctx;
+            var workspace = ctx.Find<Project_PBI_Workspace>(group.Id);
+            if (workspace == null)
+            {
+                workspace = new Project_PBI_Workspace() {  Id = group.Id, WorkspaceName = group.Name, Project = project};
+                ctx.Add(workspace);
+                if (current_ctx is null) ctx.SaveChanges();
+            }
+            return workspace;
+        }
 
+        public Project_PBI_Report GetReport(Report report, Datahub_Project project, Group workspace, DatahubProjectDBContext current_ctx = null)
+        {
+            using var local_ctx = dbFactoryProject.CreateDbContext();
+            var ctx = current_ctx ?? local_ctx;
+            var pbiReport = ctx.Find<Project_PBI_Report>(report.Id);
+            if (pbiReport == null)
+            {
+                var pbiWorkspace = GetWorkspace(workspace, project, ctx);
+                pbiReport = new Project_PBI_Report() { Id = report.Id, Workspace = pbiWorkspace, Project = project, ReportName = report.Name };
+                ctx.Add(pbiReport);
+                if (current_ctx is null) ctx.SaveChanges();
+            }
+            return pbiReport;
         }
 
         public Project_PBI_Report? GetReport(Report report, Datahub_Project project, Project_PBI_Workspace? workspace, DatahubProjectDBContext current_ctx = null)
