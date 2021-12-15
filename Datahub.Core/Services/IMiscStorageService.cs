1214df98fceea51961b9730ee1aa68831f951d72
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2021-12-15 11:48:00 -0500

    Misc storage

---
+++ Datahub.Core/Services/IMiscStorageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.Core.Services
+{
+    public interface IMiscStorageService
+    {
+        Task SaveObject<T>(T obj, string id);
+        Task SaveObjects<T>(IEnumerable<T> objects, Func<T, string> idGenerator);
+        Task<T> GetObject<T>(string id);
+        Task<IEnumerable<T>> GetAllObjects<T>();
+    }
+}
