a8a9eae82c6e0842ccc7e4b5ec8b52947bba74de
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2021-12-17 14:10:30 -0500

    Removed unnecessary usings

--- Datahub.Core/Data/StorageCostEstimatorClasses.cs
+++ Datahub.Core/Data/StorageCostEstimatorClasses.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Datahub.Core.Data.StorageCostEstimator
 {
