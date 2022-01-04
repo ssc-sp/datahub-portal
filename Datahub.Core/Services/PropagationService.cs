204e05494cd0c032c5a3f5ceece3f7747899b4b9
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2022-01-04 14:07:29 -0500

    propagation service with system notification functionality

---
+++ Datahub.Core/Services/PropagationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.Core.Services
+{
+    public class PropagationService : IPropagationService
+    {
+        public event Func<IEnumerable<string>, Task> UpdateSystemNotifications;
+
+        public async Task PropagateSystemNotificationUpdate(IEnumerable<string> userIds)
+        {
+            if (UpdateSystemNotifications != null)
+            {
+                await UpdateSystemNotifications.Invoke(userIds);
+            }
+        }
+    }
+}
