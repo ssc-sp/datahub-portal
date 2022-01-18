0913c8ce60dd9b40a53c5516b8114c115cb9c9ff
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-01-18 16:01:16 -0500

    #3164 modifying field definitions and choices to support cascading fields (sector, branch, etc)

---
+++ Datahub.Metadata/Migrations/20220117210644_AddingCascadingParentField.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datahub.Metadata.Migrations
+{
+    public partial class AddingCascadingParentField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CascadeParentId",
+                table: "FieldDefinitions",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CascadeParentId",
+                table: "FieldDefinitions");
+        }
+    }
+}
