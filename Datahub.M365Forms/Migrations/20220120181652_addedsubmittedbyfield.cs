c1ea2258104741ac7d59ff73a0523e157a8ee14c
gcnabeel <nabeel.bader@canada.ca>
2022-01-20 13:20:17 -0500

    added submitted by user

---
+++ Datahub.M365Forms/Migrations/20220120181652_addedsubmittedbyfield.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datahub.M365Forms.Migrations
+{
+    public partial class addedsubmittedbyfield : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SubmittedBy",
+                table: "M365FormsApplications",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubmittedBy",
+                table: "M365FormsApplications");
+        }
+    }
+}
