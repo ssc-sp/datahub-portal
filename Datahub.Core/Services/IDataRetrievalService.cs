9c1738ae4bd8b927ba296e821cae0d6344efe2b5
Erik <erik@apption.com>
2022-01-20 17:53:18 -0500

    minor refactoring and error handling updates

--- Datahub.Core/Services/IDataRetrievalService.cs
+++ Datahub.Core/Services/IDataRetrievalService.cs
@@ -10,7 +10,7 @@ namespace Datahub.Core.Services
         Task<System.Uri> DownloadFile(FileMetaData file);
         Task<List<string>> GetAllFolders(string rootFolderName, User user);
         Task<List<Version>> GetFileVersions(string fileId);
-        Task<Data.Folder> GetFolderContents(dynamic folder, string filterSearch, User user, string project);
+        Task<Data.Folder> GetFolderContents(Data.Folder folder, string filterSearch, User user, string project);
         Task<Data.Folder> GetFolderStructure(Data.Folder folder, User user, bool onlyFolders = true);
         Task<StorageMetadata> GetStorageMetadata(string project);
         
