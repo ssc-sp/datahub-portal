b30c4702211771ad07863f6cbf8e719ec6db994f
Erik <erik@apption.com>
2022-01-20 19:15:49 -0500

    added module management added SAS token generation and help

---
+++ DatahubTest/ModuleManagerTest.cs
@@ -0,0 +1,45 @@
+﻿using Datahub.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Datahub.Tests
+{
+    public class ModuleManagerTest
+    {
+        [Fact]
+        public async Task GivenModuleName_FindClass()
+        {
+            List<Type> allTypes = Datahub.Portal.Startup.LoadModules("*");
+            //var objectType = (from asm in AppDomain.CurrentDomain.GetAssemblies()
+            //                   from type in asm.GetTypes()
+            //                   where type.IsClass && type.Name == expectedClass
+            //                  select type).Single();
+            Assert.Contains(typeof(Datahub.Finance.FinanceModule), allTypes);
+            Assert.Contains(typeof(Datahub.M365Forms.M365FormsModule), allTypes);
+            Assert.Contains(typeof(Datahub.LanguageTraining.LanguageTrainingModule), allTypes);
+        }
+
+        [Fact]
+        public async Task GivenFilterStar_FindModule()
+        {
+            var allModules = Datahub.Portal.Startup.LoadModules("*");
+            Assert.Contains(typeof(Datahub.Finance.FinanceModule), allModules);
+            Assert.Contains(typeof(Datahub.M365Forms.M365FormsModule), allModules);
+            Assert.Contains(typeof(Datahub.LanguageTraining.LanguageTrainingModule), allModules);
+        }
+
+        [Fact]
+        public async Task GivenFilterSingle_FindModule()
+        {
+            var allModules = Datahub.Portal.Startup.LoadModules("financemodule");
+            Assert.Single(allModules);
+            Assert.Contains(typeof(Datahub.Finance.FinanceModule), allModules);
+        }
+
+    }
+}
