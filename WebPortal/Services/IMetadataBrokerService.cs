693c06adac84cc174b5abe190f9fc3a31c2b6ea3
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-01-26 13:03:11 -0500

    #3164 Creating metadata catalog data model and migrations

--- WebPortal/Services/IMetadataBrokerService.cs
+++ WebPortal/Services/IMetadataBrokerService.cs
@@ -10,14 +10,14 @@ namespace Datahub.Portal.Services
     {
         Task<Entities.MetadataProfile> GetProfile(string name);
         Task<FieldValueContainer> GetObjectMetadataValues(string objectId);
-        Task SaveMetadata(FieldValueContainer fieldValues);
+        Task<Entities.ObjectMetadata> SaveMetadata(FieldValueContainer fieldValues);
         Task<ApprovalForm> GetApprovalForm(int ApprovalFormId);
         Task DeleteApprovalForm(int approvalFormId);
         Task<int> SaveApprovalForm(ApprovalForm form);
         Task<List<string>> GetSuggestedEnglishKeywords(string text, int max);
         Task<List<string>> GetSuggestedFrenchKeywords(string text, int max);
         Task<List<SubjectKeyword>> GetSubjectKeywords(IEnumerable<string> subjectIds);
-    }
-
-    public record SubjectKeyword(string English, string French);
+        Task UpdateCatalog(long objectId, string objectName, string englishText, string frenchText);
+        Task<List<CatalogHit>> SearchCatalogEnglish(string searchText);
+    }    
 }
