8fb44e2e9c68fa915d5f7ac89a3ec22842c419e7
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2022-02-03 12:01:46 -0500

    PowerBI management WIP

---
+++ WebPortal/Data/PowerBiManagement.cs
@@ -0,0 +1,26 @@
+﻿using Datahub.Core.EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Datahub.Portal.Data
+{
+    public class PowerBiAdminWorkspaceItem
+    {
+        public Guid WorkspaceId { get; set; }
+        public string WorkspaceName { get; set; }
+        public bool SandboxFlag { get; set; }
+        public PowerBi_Workspace WorkspaceFromDb { get; set; }
+        public bool IsCatalogued => WorkspaceFromDb != null;
+        public string ProjectName => WorkspaceFromDb?.Project?.ProjectName;
+        public int? ProjectId { get; set; }
+        public int ProjectIdDropdown
+        {
+            get => ProjectId ?? -1;
+            set
+            {
+                ProjectId = (value < 0) ? null : value;
+            }
+        }
+    }
+}
