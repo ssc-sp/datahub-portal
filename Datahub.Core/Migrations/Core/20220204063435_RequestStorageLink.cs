e0a23544d31f338ab6f7a235164ad45c0ae8dcfe
gcerik <82101285+gcerik@users.noreply.github.com>
2022-02-04 01:36:32 -0500

    - reorg couple classes - added link from requests to storage

---
+++ Datahub.Core/Migrations/Core/20220204063435_RequestStorageLink.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datahub.Core.Migrations.Core
+{
+    public partial class RequestStorageLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Project_StorageId",
+                table: "Project_Requests",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_Requests_Project_StorageId",
+                table: "Project_Requests",
+                column: "Project_StorageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Requests_Project_Storage_Project_StorageId",
+                table: "Project_Requests",
+                column: "Project_StorageId",
+                principalTable: "Project_Storage",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Requests_Project_Storage_Project_StorageId",
+                table: "Project_Requests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Project_Requests_Project_StorageId",
+                table: "Project_Requests");
+
+            migrationBuilder.DropColumn(
+                name: "Project_StorageId",
+                table: "Project_Requests");
+        }
+    }
+}
