16d854db172708a998f9b2d38838275281a391ed
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2022-02-08 15:38:54 -0500

    PowerBI workspaces WIP

--- Datahub.Core/Services/IPowerBiDataService.cs
+++ Datahub.Core/Services/IPowerBiDataService.cs
@@ -10,7 +10,6 @@ namespace Datahub.Core.Services
     public interface IPowerBiDataService
     {
         public Task<IList<PowerBi_Workspace>> GetAllWorkspaces();
-        public Task AddCataloguedWorkspace(Guid workspaceId, string workspaceName, bool sandboxFlag, int? projectId);
-        public Task UpdateCataloguedWorkspace(Guid workspaceId, string workspaceName, bool sandboxFlag, int? projectId);
+        public Task<bool> AddOrUpdateCataloguedWorkspaces(IEnumerable<PowerBi_WorkspaceDefinition> workspaceDefinitions);
     }
 }
