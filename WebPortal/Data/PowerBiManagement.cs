6fd6eb5a76a490fdb5def49171ed98550c902ac1
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2022-02-15 11:40:35 -0500

    PowerBI management WIP - datasets and reports

--- WebPortal/Data/PowerBiManagement.cs
+++ WebPortal/Data/PowerBiManagement.cs
@@ -87,4 +87,144 @@ namespace Datahub.Portal.Data
         public string ProjectName => WorkspaceFromDb?.Project?.ProjectName;
         public PowerBi_WorkspaceDefinition Definition => new(_workspaceId, _workspaceName, _sandboxFlag, _projectId);
     }
+
+    public class PowerBiAdminDataSetItem
+    {
+        public PowerBiAdminDataSetItem(Dataset pbiDataset, Guid workspaceId, PowerBi_DataSet dbDataset)
+        {
+            _datasetId = Guid.Parse(pbiDataset.Id);
+            _workspaceId = workspaceId;
+            _datasetName = pbiDataset.Name;
+
+            DatasetFromDb = dbDataset;
+
+            _originalDefinition = Definition;
+        }
+
+        private Guid _datasetId;
+        private Guid _workspaceId;
+        private string _datasetName;
+
+        private bool _isChanged = false;
+        private readonly PowerBi_DataSetDefinition _originalDefinition;
+
+        public Guid DatasetId
+        {
+            get => _datasetId;
+            set
+            {
+                _datasetId = value;
+                _isChanged = true;
+            }
+        }
+
+        public Guid WorkspaceId
+        {
+            get => _workspaceId;
+            set
+            {
+                _workspaceId = value;
+                _isChanged = true;
+            }
+        }
+
+        public string DatasetName
+        {
+            get => _datasetName;
+            set
+            {
+                _datasetName = value;
+                _isChanged = true;
+            }
+        }
+
+        public bool IsChanged
+        {
+            get => _isChanged;
+            set
+            {
+                _isChanged = value;
+                if (!_isChanged)
+                {
+                    _datasetId = _originalDefinition.DataSetId;
+                    _workspaceId = _originalDefinition.WorkspaceId;
+                    _datasetName = _originalDefinition.DataSetName;
+                }
+            }
+        }
+
+        public PowerBi_DataSet DatasetFromDb { get; set; }
+        public bool IsAlreadyCatalogued => DatasetFromDb != null;
+        public PowerBi_DataSetDefinition Definition => new(_datasetId, _datasetName, _workspaceId);
+    }
+
+    public class PowerBiAdminReportItem
+    {
+        public PowerBiAdminReportItem(Report report, Guid workspaceId, PowerBi_Report dbReport)
+        {
+            _reportId = report.Id;
+            _workspaceId = workspaceId;
+            _reportName = report.Name;
+
+            ReportFromDb = dbReport;
+
+            _originalDefinition = Definition;
+        }
+
+        private Guid _reportId;
+        private string _reportName;
+        private Guid _workspaceId;
+
+        private bool _isChanged;
+        private readonly PowerBi_ReportDefinition _originalDefinition;
+
+        public Guid ReportId
+        {
+            get => _reportId;
+            set
+            {
+                _reportId = value;
+                _isChanged = true;
+            }
+        }
+
+        public string ReportName
+        {
+            get => _reportName;
+            set
+            {
+                _reportName = value;
+                _isChanged = true;
+            }
+        }
+
+        public Guid WorkspaceId
+        {
+            get => _workspaceId;
+            set
+            {
+                _workspaceId = value;
+                _isChanged = true;
+            }
+        }
+
+        public bool IsChanged
+        {
+            get => _isChanged;
+            set
+            {
+                _isChanged = value;
+                if (!_isChanged)
+                {
+                    _workspaceId = _originalDefinition.WorkspaceId;
+                    _reportId = _originalDefinition.ReportId;
+                    _reportName = _originalDefinition.ReportName;
+                }
+            }
+        }
+
+        public PowerBi_Report ReportFromDb { get; set; }
+        public bool IsAlreadyCatalogued => ReportFromDb != null;
+        public PowerBi_ReportDefinition Definition => new(_reportId, _reportName, _workspaceId);
+    }
 }
