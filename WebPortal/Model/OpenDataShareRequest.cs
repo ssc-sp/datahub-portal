cf5e8de8041b3ba297ced54ae0d5fae947520b2f
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-02-22 12:25:07 -0500

    Saving temp changes

---
+++ WebPortal/Model/OpenDataShareRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Datahub.Portal.Model
+{
+    public class OpenDataShareRequest
+    {
+        [Required]
+        public string FileURL { get; set; }
+        [Required]
+        public string CustomId { get; set; }
+        [Required]
+        public string title_translated_en { get; set; }
+        [Required]
+        public string title_translated_fr { get; set; }
+        [Required]
+        public string notes_translated_en { get; set; }
+        [Required]
+        public string notes_translated_fr { get; set; }
+        [Required]
+        public string keywords_en { get; set; }
+        [Required]
+        public string keywords_fr { get; set; }
+        [Required]
+        public string subject { get; set; }
+        [Required]
+        public string frequency { get; set; }
+        [Required]
+        public DateTime date_published { get; set; }
+        [Required]
+        public string jurisdiction { get; set; }
+    }
+}
