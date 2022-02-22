e98e4038d50052e156278409b38fad412edcb7ed
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2022-02-22 16:50:35 -0500

    WIP - bulk import/update

--- WebPortal/Data/PowerBiManagement.cs
+++ WebPortal/Data/PowerBiManagement.cs
@@ -227,4 +227,211 @@ namespace Datahub.Portal.Data
         public bool IsAlreadyCatalogued => ReportFromDb != null;
         public PowerBi_ReportDefinition Definition => new(_reportId, _reportName, _workspaceId);
     }
+
+    public class PowerBiAdminDatasetTreeItem
+    {
+        public PowerBiAdminDatasetTreeItem(Dataset pbiDataset, PowerBi_DataSet dbDataset, Guid? workspaceId)
+        {
+            _dbDataset = dbDataset;
+            _pbiDataset = pbiDataset;
+
+            if (_pbiDataset != null)
+            {
+                _datasetId = Guid.Parse(pbiDataset.Id);
+            }
+            else
+            {
+                _datasetId = dbDataset.DataSet_ID;
+            }
+
+            _pbiDatasetName = pbiDataset?.Name;
+            _dbDatasetName = dbDataset?.DataSet_Name;
+            _pbiWorkspaceId = workspaceId;
+            _dbWorkspaceId = dbDataset?.Workspace_Id;
+        }
+
+        private Guid _datasetId;
+        private string _pbiDatasetName;
+        private string _dbDatasetName;
+        private Guid? _pbiWorkspaceId;
+        private Guid? _dbWorkspaceId;
+
+        private readonly Dataset _pbiDataset;
+        private readonly PowerBi_DataSet _dbDataset;
+
+        public bool IsInPowerBi => _pbiDataset != null;
+        public bool IsInDb => _dbDataset != null;
+        public bool NeedsUpdate => !IsInDb; //TODO
+
+        public Guid DatasetId
+        {
+            get => _datasetId;
+            set => _datasetId = value;
+        }
+
+        public string PbiDatasetName
+        {
+            get => _pbiDatasetName;
+            set => _pbiDatasetName = value;
+        }
+
+        public string DbDatasetName
+        {
+            get => _dbDatasetName;
+            set => _dbDatasetName = value;
+        }
+
+        public Guid? DbWorkspaceId
+        {
+            get => _dbWorkspaceId;
+            set => _dbWorkspaceId = value;
+        }
+
+        public Guid? PbiWorkspaceId
+        {
+            get => _pbiWorkspaceId;
+            set => _pbiWorkspaceId = value;
+        }
+
+        public Guid AnyWorkspaceId => _pbiWorkspaceId ?? _dbWorkspaceId ?? Guid.Empty;
+        public PowerBi_DataSetDefinition Definition => new(_datasetId, _pbiDatasetName ?? _dbDatasetName, AnyWorkspaceId);
+    }
+
+    public class PowerBiAdminReportTreeItem
+    {
+        public PowerBiAdminReportTreeItem(Report pbiReport, PowerBi_Report dbReport, Guid? workspaceId)
+        {
+            _pbiReport = pbiReport;
+            _dbReport = dbReport;
+
+            _reportId = pbiReport?.Id ?? dbReport?.Report_ID ?? Guid.Empty;
+            _pbiReportName = pbiReport?.Name;
+            _dbReportName = dbReport?.Report_Name;
+            _pbiWorkspaceId = workspaceId;
+            _dbWorkspaceId = dbReport?.Workspace_Id;
+        }
+
+        private Guid _reportId;
+        private string _pbiReportName;
+        private string _dbReportName;
+        private Guid? _pbiWorkspaceId;
+        private Guid? _dbWorkspaceId;
+
+        private readonly Report _pbiReport;
+        private readonly PowerBi_Report _dbReport;
+
+        public bool IsInPowerBi => _pbiReport != null;
+        public bool IsInDb => _dbReport != null;
+        public bool NeedsUpdate => !IsInDb; //TODO
+
+        public Guid ReportId
+        {
+            get => _reportId;
+            set => _reportId = value;
+        }
+
+        public string PbiReportName
+        {
+            get => _pbiReportName;
+            set => _pbiReportName = value;
+        }
+
+        public string DbReportName
+        {
+            get => _dbReportName;
+            set => _dbReportName = value;
+        }
+
+        public Guid? PbiWorkspaceId
+        {
+            get => _pbiWorkspaceId;
+            set => _pbiWorkspaceId = value;
+        }
+
+        public Guid? DbWorkspaceId
+        {
+            get => _dbWorkspaceId;
+            set => _dbWorkspaceId = value;
+        }
+
+        public Guid AnyWorkspaceId => _pbiWorkspaceId ??_dbWorkspaceId ?? Guid.Empty;
+        public PowerBi_ReportDefinition Definition => new(_reportId, _pbiReportName ?? _dbReportName, AnyWorkspaceId);
+    }
+
+    public class PowerBiAdminWorkspaceTreeItem
+    {
+        public PowerBiAdminWorkspaceTreeItem(Group pbiWorkspace, PowerBi_Workspace dbWorkspace)
+        {
+            _dbWorkspace = dbWorkspace;
+            _pbiWorkspace = pbiWorkspace;
+
+            // should never be empty, since at least one workspace will be provided
+            _workspaceId = pbiWorkspace?.Id ?? dbWorkspace?.Workspace_ID ?? Guid.Empty;
+            _dbWorkspaceName = dbWorkspace?.Workspace_Name;
+            _pbiWorkspaceName = pbiWorkspace?.Name;
+            _sandboxFlag = dbWorkspace?.Sandbox_Flag ?? false;
+            _projectId = dbWorkspace?.Project_Id;
+        }
+
+        private Guid _workspaceId;
+        private string _dbWorkspaceName;
+        private string _pbiWorkspaceName;
+        private bool _sandboxFlag;
+        private int? _projectId;
+
+        private readonly Group _pbiWorkspace;
+        private readonly PowerBi_Workspace _dbWorkspace;
+
+        private List<PowerBiAdminDatasetTreeItem> _datasets = new();
+        private List<PowerBiAdminReportTreeItem> _reports = new();
+
+
+        public bool IsInPowerBi => _pbiWorkspace != null;
+        public bool IsInDb => _dbWorkspace != null;
+        public bool NeedsUpdate => !IsInDb || Datasets.Any(d => d.NeedsUpdate) || Reports.Any(r => r.NeedsUpdate); //TODO
+
+        public Guid WorkspaceId
+        {
+            get => _workspaceId;
+            set => _workspaceId = value;
+        }
+
+        public string DbWorkspaceName
+        {
+            get => _dbWorkspaceName;
+            set => _dbWorkspaceName = value;
+        }
+
+        public string PbiWorkspaceName
+        {
+            get => _pbiWorkspaceName; 
+            set => _pbiWorkspaceName = value;
+        }
+
+        public bool SandboxFlag
+        {
+            get => _sandboxFlag;
+            set => _sandboxFlag = value;
+        }
+
+        public int ProjectId
+        {
+            get => _projectId ?? -1;
+            set => _projectId = (value < 0) ? null : value;
+        }
+
+        public List<PowerBiAdminDatasetTreeItem> Datasets
+        {
+            get => _datasets;
+            private set => _datasets = value;
+        }
+
+        public List<PowerBiAdminReportTreeItem> Reports
+        {
+            get => _reports;
+            private set => _reports = value;
+        }
+
+        public PowerBi_WorkspaceDefinition Definition => new(_workspaceId, _pbiWorkspaceName ?? _dbWorkspaceName, _sandboxFlag, _projectId);
+    }
 }
