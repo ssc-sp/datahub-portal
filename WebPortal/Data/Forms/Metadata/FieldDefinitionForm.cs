f58005f7b426559e2d9c19316ca4460ea0243cfc
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-02-22 13:01:59 -0500

    Adding field order in the Definition Form.

--- WebPortal/Data/Forms/Metadata/FieldDefinitionForm.cs
+++ WebPortal/Data/Forms/Metadata/FieldDefinitionForm.cs
@@ -7,41 +7,41 @@ namespace Datahub.Portal.Data.Forms.Metadata
     {
         [Required]
         [StringLength(256)]
-        [AeFormCategory("Name")]
+        [AeFormCategory("Name", 10)]
         public string Field_Name_TXT { get; set; }
 
         [Required]
         [StringLength(256)]
-        [AeFormCategory("Name")]
+        [AeFormCategory("Name", 10)]
         public string Name_English_TXT { get; set; }
 
         [Required]
         [StringLength(256)]
-        [AeFormCategory("Name")]
+        [AeFormCategory("Name", 10)]
         public string Name_French_TXT { get; set; }
 
         [StringLength(256)]
-        [AeFormCategory("Description")]
+        [AeFormCategory("Description", 20)]
         public string English_DESC { get; set; }
 
         [StringLength(256)]
-        [AeFormCategory("Description")]
+        [AeFormCategory("Description", 20)]
         public string French_DESC { get; set; }
 
-        [AeFormCategory("Other")]
+        [AeFormCategory("Other", 30)]
         public bool Required_FLAG { get; set; }
 
-        [AeFormCategory("Other")]
+        [AeFormCategory("Other", 30)]
         public bool MultiSelect_FLAG { get; set; }
 
         [StringLength(256)]
-        [AeFormCategory("Other")]
+        [AeFormCategory("Other", 30)]
         public string Default_Value_TXT { get; set; }
 
-        [AeFormCategory("Other")]
+        [AeFormCategory("Other", 30)]
         public int Sort_Order_NUM { get; set; }
 
-        [AeFormCategory("Other")]
+        [AeFormCategory("Other", 30)]
         [AeLabel(placeholder: "Enter choice list separated by | characters. (new definitions)")]
         public string Choices_TXT { get; set; }
     }
