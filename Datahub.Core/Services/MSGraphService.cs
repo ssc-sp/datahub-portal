9bde7791260e5eb1f55b0ba506417dc01ef4fbd0
Erik <erik@apption.com>
2022-03-04 11:21:13 -0500

    tentative improvement for invalid user names

--- Datahub.Core/Services/MSGraphService.cs
+++ Datahub.Core/Services/MSGraphService.cs
@@ -37,6 +37,8 @@ namespace Datahub.Core.Services
         public async Task<GraphUser> GetUserAsync(string userId, CancellationToken tkn = default)
         {
             PrepareAuthenticatedClient();
+            if (!userId?.Contains("@") ?? true)
+                return new GraphUser();
             try
             {
                 var user = await graphServiceClient.Users.Request()
