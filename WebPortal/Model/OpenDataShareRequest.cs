6a0d5cdfbde67247a1cf6b35fc34f821728036bc
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-03-10 10:53:51 -0500

    #3269 Sending optional CFS fields to OpenData

--- WebPortal/Model/OpenDataShareRequest.cs
+++ WebPortal/Model/OpenDataShareRequest.cs
@@ -3,6 +3,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Datahub.Portal.Model
 {
+    public class MetadataField : Attribute
+    {
+    }
+
     public class OpenDataShareRequest
     {
         [Required]
@@ -14,31 +18,46 @@ namespace Datahub.Portal.Model
         [Required]
         public string email_contact { get; set; }
         [Required]
+        [MetadataField]
         public string collection { get; set; }
         [Required]
+        [MetadataField]
         public string title_translated_en { get; set; }
         [Required]
+        [MetadataField]
         public string title_translated_fr { get; set; }
         [Required]
+        [MetadataField]
         public string notes_translated_en { get; set; }
         [Required]
+        [MetadataField]
         public string notes_translated_fr { get; set; }
         [Required]
+        [MetadataField]
         public string keywords_en { get; set; }
         [Required]
+        [MetadataField]
         public string keywords_fr { get; set; }
         [Required]
+        [MetadataField]
         public string subject { get; set; }
         [Required]
+        [MetadataField]
         public string frequency { get; set; }
         [Required]
+        [MetadataField]
         public string date_published { get; set; }
         [Required]
+        [MetadataField]
         public string jurisdiction { get; set; }
-        // not required fields
+
+        [MetadataField]
         public string time_period_coverage_start { get; set; }
+        [MetadataField]
         public string time_period_coverage_end { get; set; }
+        [MetadataField]
         public string audience { get; set; }
+        [MetadataField]
         public string digital_object_identifier { get; set; }
     }
 }
