8f8af2743f08a1c97250c78f75f7446ba67f8287
gcnabeel <nabeel.bader@canada.ca>
2022-03-10 23:38:26 -0500

    added indicator status

--- WebPortal/Data/Forms/PIP/PIP_IndicatorAndResults.cs
+++ WebPortal/Data/Forms/PIP/PIP_IndicatorAndResults.cs
@@ -14,71 +14,100 @@ namespace Datahub.ProjectForms.Data.PIP
         [AeFormIgnore]
         public int IndicatorAndResult_ID { get; set; }
 
-       
         [MaxLength(20)]
         [AeFormIgnore]
         public string IndicatorCode { get; set; }
 
-        [AeLabel(row: "1", column: "1", isDropDown: true, placeholder: "Please Select")] 
-        [MaxLength(400)] 
+
+        [AeFormCategory("Indicator Status", 1)]
+        [MaxLength(100)]
+        [AeLabel(row: "1", column: "1", isDropDown: true, validValues: new[] { "Input Required" })]
+        public string Indicator_Status { get; set; }
+
+        [AeFormCategory("Indicator Details", 5)]
+        [AeLabel(row: "1", column: "2", isDropDown: true, placeholder: "Please Select")]
+        [MaxLength(400)]
         public string Outcome_Level_DESC { get; set; }
 
+        [AeFormCategory("Indicator Details", 5)]
         [AeLabel(row: "1", column: "2")]
-        [MaxLength(1000)] 
+        [MaxLength(1000)]
         public string Program_Output_Or_Outcome_DESC { get; set; }
 
+        [AeFormCategory("Indicator Details", 5)]
         [AeLabel(row: "3", column: "1")]
-        [MaxLength(4000)] 
+        [MaxLength(4000)]
         public string Indicator_DESC { get; set; }
 
+        [AeFormCategory("Indicator Details", 5)]
         [AeLabel(row: "4", column: "1", isDropDown: true, placeholder: "Please Select")]
         [MaxLength(1000)]
         public string Source_Of_Indicator_DESC { get; set; }
 
+        [AeFormCategory("Indicator Details", 5)]
         [AeLabel(row: "4", column: "2", isDropDown: true, placeholder: "Please Select")]
         [MaxLength(1000)]
         public string Source_Of_Indicator2_DESC { get; set; }
-        
+
+        [AeFormCategory("Indicator Details", 5)]
         [AeLabel(row: "4", column: "3", isDropDown: true, placeholder: "Please Select")]
         [MaxLength(1000)]
         public string Source_Of_Indicator3_DESC { get; set; }
 
+        [AeFormCategory("Indicator Details", 5)]
         [AeLabel(row: "7", column: "1", isDropDown: true, placeholder: "Please Select")]
         [MaxLength(50)]
         public string Can_Report_On_Indicator { get; set; }
+
+        [AeFormCategory("Indicator Details", 5)]
         [AeLabel(row: "7", column: "2", isDropDown: true, placeholder: "Please Select")]
         [MaxLength(100)]
         public string Cannot_Report_On_Indicator { get; set; }
 
+        [AeFormCategory("Indicator Details", 5)]
         [AeLabel(row: "8", column: "1")]
         [MaxLength(4000)]
         public string DRF_Indicator_No { get; set; }
+
+        [AeFormCategory("Indicator Details", 5)]
         [AeLabel(row: "8", column: "2")]
-        [MaxLength(1000)] 
+        [MaxLength(1000)]
         public string Tb_Sub_Indicator_Identification_Number_ID { get; set; }
 
+        [AeFormCategory("Indicator Details", 5)]
         [AeLabel(row: "9", column: "1")]
         [MaxLength(1000)]
         public string Branch_Optional_DESC { get; set; }
+
+        [AeFormCategory("Indicator Details", 5)]
         [AeLabel(row: "9", column: "2")]
         [MaxLength(1000)]
         public string Sub_Program { get; set; }
 
-        [AeLabel(row: "10", column: "1", isDropDown: true, placeholder: "Please Select")] 
-        [MaxLength(1000)] 
+        [AeFormCategory("Indicator Details", 5)]
+        [AeLabel(row: "10", column: "1", isDropDown: true, placeholder: "Please Select")]
+        [MaxLength(1000)]
         public string Indicator_Category_DESC { get; set; }
-        [AeLabel(row: "10", column: "2", isDropDown: true, placeholder: "Please Select")] [MaxLength(1000)] 
+
+        [AeFormCategory("Indicator Details", 5)]
+        [AeLabel(row: "10", column: "2", isDropDown: true, placeholder: "Please Select")]
+        [MaxLength(1000)]
         public string Indicator_Direction_DESC { get; set; }
-        [AeLabel(row: "10", column: "3", isDropDown: true, placeholder: "Please Select")] [MaxLength(1000)] 
+
+        [AeFormCategory("Indicator Details", 5)]
+        [AeLabel(row: "10", column: "3", isDropDown: true, placeholder: "Please Select")]
+        [MaxLength(1000)]
         public string Indicator__Progressive_Or_Aggregate_DESC { get; set; }
-        
-        [AeLabel(row: "11", column: "1", placeholder: "How it relates to the output or outcome; why is it relevant?")] [MaxLength(4000)] 
+
+
+        [AeLabel(row: "11", column: "1", placeholder: "How it relates to the output or outcome; why is it relevant?")]
+        [MaxLength(4000)]
         [AeFormCategory("Methodology", 10)]
         public string Indicator_Rationale_DESC { get; set; }
 
         [AeLabel(row: "12", column: "1")]
         [AeFormCategory("Methodology", 10)]
-        [MaxLength(2000)] 
+        [MaxLength(2000)]
         public string Indicator_Calculation_Formula_NUM { get; set; }
 
         [AeLabel(row: "13", column: "1")]
@@ -88,7 +117,7 @@ namespace Datahub.ProjectForms.Data.PIP
 
         [AeLabel(row: "14", column: "1")]
         [AeFormCategory("Methodology", 10)]
-        [MaxLength(1000)] 
+        [MaxLength(1000)]
         public string Baseline_DESC { get; set; }
         [AeLabel(row: "14", column: "2")]
         [AeFormCategory("Methodology", 10)]
@@ -102,18 +131,20 @@ namespace Datahub.ProjectForms.Data.PIP
 
         [AeLabel(row: "16", column: "1")]
         [AeFormCategory("Methodology", 10)]
-        [MaxLength(1000)]         
+        [MaxLength(1000)]
         public string Data_Source_DESC { get; set; }
         [AeLabel(row: "16", column: "2")]
         [AeFormCategory("Methodology", 10)]
-        [MaxLength(1000)] 
-        public string Data_Owner_NAME { get; set; }         
+        [MaxLength(1000)]
+        public string Data_Owner_NAME { get; set; }
 
 
-        [AeLabel(row: "17", column: "1", isDropDown: true, placeholder: "Please Select")] [MaxLength(1000)]
+        [AeLabel(row: "17", column: "1", isDropDown: true, placeholder: "Please Select")]
+        [MaxLength(1000)]
         [AeFormCategory("Methodology", 10)]
         public string Frequency_DESC { get; set; }
-        [AeLabel(row: "17", column: "2", isDropDown: true, placeholder: "Please Select")] [MaxLength(1000)]
+        [AeLabel(row: "17", column: "2", isDropDown: true, placeholder: "Please Select")]
+        [MaxLength(1000)]
         [AeFormCategory("Methodology", 10)]
         public string Data_Type_DESC { get; set; }
 
@@ -122,12 +153,13 @@ namespace Datahub.ProjectForms.Data.PIP
         [AeFormCategory("Methodology", 10)]
         public string Methodology_How_Will_The_Indicator_Be_Measured { get; set; }
 
-        [AeLabel(row: "19", column: "1", isDropDown: true, placeholder: "Please Select")] [MaxLength(1000)] 
+        [AeLabel(row: "19", column: "1", isDropDown: true, placeholder: "Please Select")]
+        [MaxLength(1000)]
         [AeFormCategory("Target", 20)]
         public string Target_Type_DESC { get; set; }
         [AeLabel(row: "19", column: "2")]
         [AeFormCategory("Target", 20)]
-        [MaxLength(4000)] 
+        [MaxLength(4000)]
         public string Target_DESC { get; set; }
         [AeLabel(row: "19", column: "3")]
         [AeFormCategory("Target", 20)]
@@ -141,19 +173,32 @@ namespace Datahub.ProjectForms.Data.PIP
         [AeLabel(row: "20", column: "2")]
         [AeFormCategory("Actual Results", 30)]
         public DateTime? Date_Result_Collected { get; set; }
-        [AeLabel(row: "20", column: "3", isDropDown: true, placeholder: "Please Select")] [MaxLength(1000)] 
+        [AeLabel(row: "20", column: "3", isDropDown: true, placeholder: "Please Select")]
+        [MaxLength(1000)]
         [AeFormCategory("Actual Results", 30)]
         public string Target_Met { get; set; }
 
-        
-        [AeLabel(row: "21", column: "1", placeholder: "Please provide additional context for the results achieved. This is required for targets not been met, to be achieved in the future, or if the result is significantly higher than the target. There is a character limit of 1000 for this cell in order to meet GC InfoBase character limit.")] [MaxLength(8000)]
+
+        [AeLabel(row: "21", column: "1", placeholder: "Please provide additional context for the results achieved. This is required for targets not been met, to be achieved in the future, or if the result is significantly higher than the target. There is a character limit of 1000 for this cell in order to meet GC InfoBase character limit.")]
+        [MaxLength(8000)]
         [AeFormCategory("Actual Results", 30)]
         public string Explanation { get; set; }
 
-       
+        [AeLabel(row: "22", column: "1")]
+        [AeFormCategory("Actual Results", 30)]
+        [MaxLength(1000)]
+        public string Result_201920_DESC { get; set; }
+        [AeLabel(row: "22", column: "2")]
+        [AeFormCategory("Actual Results", 30)]
+        [MaxLength(1000)]
+        public string Result_201819_DESC { get; set; }
+        [AeLabel(row: "22", column: "3")]
+        [AeFormCategory("Actual Results", 30)]
+        [MaxLength(1000)]
+        public string Result_201718_DESC { get; set; }
 
 
-        
+
         [AeFormIgnore]
         public bool IsDeleted { get; set; }
 
@@ -199,6 +244,11 @@ namespace Datahub.ProjectForms.Data.PIP
         public bool IsActualResultsLocked { get; set; }
         [AeFormIgnore]
         public bool IsLatestUpdateLocked { get; set; }
+
+        [AeFormIgnore]
+        public bool IsIndicatorStatusLocked { get; set; }
+        [AeFormIgnore]
+        public bool IsIndicatorDetailsLocked { get; set; }
         [AeFormIgnore]
         public string EditingUserId { get; set; }
 
