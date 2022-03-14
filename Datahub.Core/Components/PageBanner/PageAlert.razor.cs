a4c2acde1a8f2cfbe19b152535d60f86d487249a
Yannick Robert <yrobert@apption.com>
2022-03-14 13:29:20 -0400

    added page alert/tutorial to data project page

--- Datahub.Core/Components/PageBanner/PageAlert.razor.cs
+++ Datahub.Core/Components/PageBanner/PageAlert.razor.cs
@@ -6,6 +6,6 @@ public partial class PageAlert
     
     public const string Home = $"{Prefix}/home";
     public const string Projects = $"{Prefix}/projects";
-    public const string Features = $"{Prefix}/features";
+    public const string ProjectFeatures = $"{Prefix}/features";
     
 }
