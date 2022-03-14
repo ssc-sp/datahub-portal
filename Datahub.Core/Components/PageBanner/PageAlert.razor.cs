c3b20636125b25debc5c617120f2874f27e2aea6
Yannick Robert <yrobert@apption.com>
2022-03-14 13:14:28 -0400

    refactored page alert and added it to all projects page

--- Datahub.Core/Components/PageBanner/PageAlert.razor.cs
+++ Datahub.Core/Components/PageBanner/PageAlert.razor.cs
@@ -3,5 +3,9 @@ namespace Datahub.Core.Components.PageBanner;
 public partial class PageAlert
 {
     private const string Prefix = "tutorials";
+    
     public const string Home = $"{Prefix}/home";
+    public const string Projects = $"{Prefix}/projects";
+    public const string Features = $"{Prefix}/features";
+    
 }
