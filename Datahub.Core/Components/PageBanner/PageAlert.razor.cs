848d87794a02916faaa628ca84ac757e547a3add
Yannick Robert <yrobert@apption.com>
2022-03-14 11:15:24 -0400

    added local storage to home tutorial

---
+++ Datahub.Core/Components/PageBanner/PageAlert.razor.cs
@@ -0,0 +1,7 @@
+namespace Datahub.Core.Components.PageBanner;
+
+public partial class PageAlert
+{
+    private const string Prefix = "tutorials";
+    public const string Home = $"{Prefix}/home";
+}
