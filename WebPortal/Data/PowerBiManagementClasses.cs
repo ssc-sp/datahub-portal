b7aa02bd069873f59ae45018ea48c3267fb28cf3
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2022-03-16 11:24:55 -0400

    PowerBI management update WIP

--- WebPortal/Data/PowerBiManagementClasses.cs
+++ WebPortal/Data/PowerBiManagementClasses.cs
@@ -6,228 +6,6 @@ using System.Linq;
 
 namespace Datahub.Portal.Data
 {
-    public class PowerBiAdminWorkspaceItem
-    {
-        public PowerBiAdminWorkspaceItem(Group pbiWorkspace, PowerBi_Workspace dbWorkspace)
-        {
-            _workspaceId = pbiWorkspace.Id;
-            _workspaceName = pbiWorkspace.Name;
-            WorkspaceFromDb = dbWorkspace;
-            _sandboxFlag = dbWorkspace?.Sandbox_Flag ?? false;
-            _projectId = dbWorkspace?.Project_Id;
-            _originalDefinition = Definition;
-        }
-
-        private Guid _workspaceId;
-        private string _workspaceName;
-        private bool _sandboxFlag;
-        private int? _projectId;
-
-        private bool _isChanged = false;
-        private readonly PowerBi_WorkspaceDefinition _originalDefinition;
-
-        public Guid WorkspaceId
-        {
-            get { return _workspaceId; }
-            set
-            {
-                _workspaceId = value;
-                _isChanged = true;
-            }
-        }
-
-        public string WorkspaceName
-        {
-            get { return _workspaceName; }
-            set
-            {
-                _workspaceName = value;
-                _isChanged = true;
-            }
-        }
-
-        public bool SandboxFlag
-        {
-            get { return _sandboxFlag; }
-            set
-            {
-                _sandboxFlag = value;
-                _isChanged = true;
-            }
-        }
-
-        public int ProjectId
-        {
-            get { return _projectId ?? -1; }
-            set
-            {
-                _projectId = (value < 0) ? null : value;
-                _isChanged = true;
-            }
-        }
-
-        public bool IsChanged
-        {
-            get { return _isChanged; }
-            set
-            {
-                _isChanged = value;
-                if (!_isChanged)
-                {
-                    _workspaceId = _originalDefinition.WorkspaceId;
-                    _workspaceName = _originalDefinition.WorkspaceName;
-                    _projectId = _originalDefinition.ProjectId;
-                    _sandboxFlag = _originalDefinition.SandboxFlag;
-                }
-            }
-        }
-        
-        public PowerBi_Workspace WorkspaceFromDb { get; set; }
-        public bool IsAlreadyCatalogued => WorkspaceFromDb != null;
-        public string ProjectName => WorkspaceFromDb?.Project?.ProjectName;
-        public PowerBi_WorkspaceDefinition Definition => new(_workspaceId, _workspaceName, _sandboxFlag, _projectId);
-    }
-
-    public class PowerBiAdminDataSetItem
-    {
-        public PowerBiAdminDataSetItem(Dataset pbiDataset, Guid workspaceId, PowerBi_DataSet dbDataset)
-        {
-            _datasetId = Guid.Parse(pbiDataset.Id);
-            _workspaceId = workspaceId;
-            _datasetName = pbiDataset.Name;
-
-            DatasetFromDb = dbDataset;
-
-            _originalDefinition = Definition;
-        }
-
-        private Guid _datasetId;
-        private Guid _workspaceId;
-        private string _datasetName;
-
-        private bool _isChanged = false;
-        private readonly PowerBi_DataSetDefinition _originalDefinition;
-
-        public Guid DatasetId
-        {
-            get => _datasetId;
-            set
-            {
-                _datasetId = value;
-                _isChanged = true;
-            }
-        }
-
-        public Guid WorkspaceId
-        {
-            get => _workspaceId;
-            set
-            {
-                _workspaceId = value;
-                _isChanged = true;
-            }
-        }
-
-        public string DatasetName
-        {
-            get => _datasetName;
-            set
-            {
-                _datasetName = value;
-                _isChanged = true;
-            }
-        }
-
-        public bool IsChanged
-        {
-            get => _isChanged;
-            set
-            {
-                _isChanged = value;
-                if (!_isChanged)
-                {
-                    _datasetId = _originalDefinition.DataSetId;
-                    _workspaceId = _originalDefinition.WorkspaceId;
-                    _datasetName = _originalDefinition.DataSetName;
-                }
-            }
-        }
-
-        public PowerBi_DataSet DatasetFromDb { get; set; }
-        public bool IsAlreadyCatalogued => DatasetFromDb != null;
-        public PowerBi_DataSetDefinition Definition => new(_datasetId, _datasetName, _workspaceId);
-    }
-
-    public class PowerBiAdminReportItem
-    {
-        public PowerBiAdminReportItem(Report report, Guid workspaceId, PowerBi_Report dbReport)
-        {
-            _reportId = report.Id;
-            _workspaceId = workspaceId;
-            _reportName = report.Name;
-
-            ReportFromDb = dbReport;
-
-            _originalDefinition = Definition;
-        }
-
-        private Guid _reportId;
-        private string _reportName;
-        private Guid _workspaceId;
-
-        private bool _isChanged;
-        private readonly PowerBi_ReportDefinition _originalDefinition;
-
-        public Guid ReportId
-        {
-            get => _reportId;
-            set
-            {
-                _reportId = value;
-                _isChanged = true;
-            }
-        }
-
-        public string ReportName
-        {
-            get => _reportName;
-            set
-            {
-                _reportName = value;
-                _isChanged = true;
-            }
-        }
-
-        public Guid WorkspaceId
-        {
-            get => _workspaceId;
-            set
-            {
-                _workspaceId = value;
-                _isChanged = true;
-            }
-        }
-
-        public bool IsChanged
-        {
-            get => _isChanged;
-            set
-            {
-                _isChanged = value;
-                if (!_isChanged)
-                {
-                    _workspaceId = _originalDefinition.WorkspaceId;
-                    _reportId = _originalDefinition.ReportId;
-                    _reportName = _originalDefinition.ReportName;
-                }
-            }
-        }
-
-        public PowerBi_Report ReportFromDb { get; set; }
-        public bool IsAlreadyCatalogued => ReportFromDb != null;
-        public PowerBi_ReportDefinition Definition => new(_reportId, _reportName, _workspaceId);
-    }
-
     public class PowerBiAdminDatasetTreeItem
     {
         public PowerBiAdminDatasetTreeItem(Dataset pbiDataset, PowerBi_DataSet dbDataset, Guid? workspaceId)
@@ -295,6 +73,7 @@ namespace Datahub.Portal.Data
 
         public Guid AnyWorkspaceId => _pbiWorkspaceId ?? _dbWorkspaceId ?? Guid.Empty;
         public PowerBi_DataSetDefinition Definition => new(_datasetId, _pbiDatasetName ?? _dbDatasetName, AnyWorkspaceId);
+        public PowerBiAdminTreeItem ManagementTreeItem => new(DbDatasetName, PowerBiAdminTreeItemType.Dataset, DatasetId);
     }
 
     public class PowerBiAdminReportTreeItem
@@ -356,6 +135,7 @@ namespace Datahub.Portal.Data
 
         public Guid AnyWorkspaceId => _pbiWorkspaceId ??_dbWorkspaceId ?? Guid.Empty;
         public PowerBi_ReportDefinition Definition => new(_reportId, _pbiReportName ?? _dbReportName, AnyWorkspaceId);
+        public PowerBiAdminTreeItem ManagementTreeItem => new(DbReportName, PowerBiAdminTreeItemType.Report, ReportId);
     }
 
     public class PowerBiAdminWorkspaceTreeItem
@@ -433,5 +213,56 @@ namespace Datahub.Portal.Data
         }
 
         public PowerBi_WorkspaceDefinition Definition => new(_workspaceId, _pbiWorkspaceName ?? _dbWorkspaceName, _sandboxFlag, _projectId);
+        public PowerBiAdminTreeItem ManagementTreeItem => new(DbWorkspaceName, PowerBiAdminTreeItemType.Workspace, WorkspaceId);
+    }
+
+
+    public enum PowerBiAdminTreeItemType
+    {
+        Project,
+        Workspace,
+        Container,
+        Dataset,
+        Report
+    }
+
+    /*
+     *  root
+     *      project
+     *          workspace
+     *              container (datasets)
+     *                  dataset
+     *              container (reports)
+     *                  report
+     * */
+
+    public class PowerBiAdminTreeItem
+    {
+        public PowerBiAdminTreeItem(string label, PowerBiAdminTreeItemType itemType, Guid id = default)
+        {
+            Label = label;
+            ItemType = itemType;
+            Children = new();
+            Id = id;
+        }
+
+        public Guid Id { get; private set; }
+        public string Label { get; private set; }
+        public PowerBiAdminTreeItemType ItemType { get; private set; }
+        public List<PowerBiAdminTreeItem> Children { get; private set; }
+        public void AddChild(PowerBiAdminTreeItem item) => Children.Add(item);
+        public void AddChildren(IEnumerable<PowerBiAdminTreeItem> items) => Children.AddRange(items);
+        public bool HasChildren => Children.Count > 0;
+    }
+
+    public class PowerBiAdminGroupUser
+    {
+        public PowerBiAdminGroupUser(string userEmail, bool isAdmin)
+        {
+            UserEmail = userEmail;
+            IsAdmin = isAdmin;
+        }
+        public string UserEmail { get; private set; }
+        public bool IsAdmin { get; private set; }
     }
 }
