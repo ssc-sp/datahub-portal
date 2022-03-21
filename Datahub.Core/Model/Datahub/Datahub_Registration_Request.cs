f084b7f4f98296f090cb278546de654f0e9db2e2
Yannick Robert <yrobert@apption.com>
2022-03-21 08:50:56 -0400

    added registration request migration

--- Datahub.Core/Model/Datahub/Datahub_Registration_Request.cs
+++ Datahub.Core/Model/Datahub/Datahub_Registration_Request.cs
@@ -6,20 +6,28 @@ namespace Datahub.Core.EFCore;
 public class Datahub_Registration_Request
 {
     public int Id { get; set; }
+    
+    [StringLength(254)]
     public string Email { get; set; }
+    
+    [StringLength(36)]
     public Guid? LinkId { get; set; }
     
+    [StringLength(36)]
     public string RequestedProjectAcronym { get; set; }
     
     // create
     // invite
     // provisioned
     // logged in
+    [StringLength(200)]
     public string Status { get; set; }
 
     public DateTime? CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
+    [StringLength(200)]
     public string CreatedBy { get; set; }
+    [StringLength(200)]
     public string UpdatedBy { get; set; }
     
     [Timestamp]
