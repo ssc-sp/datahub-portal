bb0f6c2d8406d4657e83b51c0a36b2368e026925
gcerik <82101285+gcerik@users.noreply.github.com>
2022-03-21 14:25:43 -0400

    reworked page alerts with wiki links instead of hardcoded text

--- WebPortal/Startup.cs
+++ WebPortal/Startup.cs
@@ -275,40 +275,6 @@ namespace Datahub.Portal
                     .AddMicrosoftGraph(Configuration.GetSection("Graph"))
                     .AddInMemoryTokenCaches();
 
-                // var isCustomRedirectUriRequired = true;
-                // if (isCustomRedirectUriRequired)
-                // {
-                //     services
-                //         .Configure<OpenIdConnectOptions>(
-                //             AzureADDefaults.OpenIdScheme,
-                //             options =>
-                //             {
-                //                 options.Events =
-                //                     new OpenIdConnectEvents
-                //                     {
-                //                         OnRedirectToIdentityProvider = async ctx =>
-                //                         {
-                //                             ctx.ProtocolMessage.RedirectUri = "https://datahub-dev.nrcan-rncan.gc.ca/signin-oidc";
-                //                             await Task.Yield();
-                //                         }
-                //                     };
-                //             });
-                // }
-
-                //services
-                //    .AddAuthorization(
-                //        options =>
-                //        {
-                //            options.AddPolicy(
-                //                PolicyConstants.DashboardPolicy,
-                //                builder =>
-                //                {
-                //                    builder
-                //                        .AddAuthenticationSchemes(AzureADDefaults.AuthenticationScheme)
-                //                        .RequireAuthenticatedUser();
-                //                });
-                //        });
-
                 services.AddControllersWithViews()
                         .AddMicrosoftIdentityUI();
             }
