4ee7d92168b964caf310cb3081dd9f55a48bdf18
Yannick Robert <yrobert@apption.com>
2022-03-23 16:41:17 -0400

    datetime hotfix

--- Datahub.Core/Services/Storage/DataRetrievalService.cs
+++ Datahub.Core/Services/Storage/DataRetrievalService.cs
@@ -463,12 +463,17 @@ namespace Datahub.Portal.Services.Storage
                             fileId = newId;
                         }
 
+                        DateTime parsedModifiedDate;
                         string ownedBy = blobItem.Metadata.TryGetValue(FileMetaData.OwnedBy, out ownedBy) ? ownedBy : "Unknown";
                         string createdBy = blobItem.Metadata.TryGetValue(FileMetaData.CreatedBy, out createdBy) ? createdBy : "Unknown";
                         string lastModifiedBy = blobItem.Metadata.TryGetValue(FileMetaData.LastModifiedBy, out lastModifiedBy) ? lastModifiedBy : "lastmodifiedby";
                         string lastModified = blobItem.Metadata.TryGetValue(FileMetaData.LastModified, out lastModified) ? lastModified : DateTime.UtcNow.ToString();
                         string fileSize = blobItem.Metadata.TryGetValue(FileMetaData.FileSize, out fileSize) ? fileSize : "0";
 
+                        var isDateValid = DateTime.TryParse(lastModified, out parsedModifiedDate);
+                        if (!isDateValid)
+                            parsedModifiedDate = DateTime.UtcNow;
+                        
                         var file = new FileMetaData()
                         {
                             id = fileId,
@@ -476,7 +481,7 @@ namespace Datahub.Portal.Services.Storage
                             ownedby = ownedBy,
                             createdby = createdBy,
                             lastmodifiedby = lastModifiedBy,
-                            lastmodifiedts = DateTime.Parse(lastModified),
+                            lastmodifiedts = parsedModifiedDate,
                             filesize = fileSize
                         };
 
