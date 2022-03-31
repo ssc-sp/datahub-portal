2bf6ba5870f12eece3cac49bf92308f490ed9fbe
Yannick Robert <yrobert@apption.com>
2022-03-30 23:43:30 -0400

    added container name to data updating

--- WebPortal/Services/Offline/OfflineDataUpdatingService.cs
+++ WebPortal/Services/Offline/OfflineDataUpdatingService.cs
@@ -26,7 +26,7 @@ namespace Datahub.Portal.Services.Offline
             return Task.FromResult(true);
         }
 
-        public async Task RenameStorageBlob(string oldName, string newName, string projectAcronym)
+        public async Task RenameStorageBlob(string oldName, string newName, string projectAcronym, string containerName)
         {
             throw new NotImplementedException();
         }
