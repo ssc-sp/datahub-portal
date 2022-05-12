e2a267e2a641703fcf3c662f95017db964ceef26
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2022-05-12 13:55:10 -0400

    Database card/project resources WIP

--- Datahub.Core/Model/Datahub/Project_Database.cs
+++ Datahub.Core/Model/Datahub/Project_Database.cs
@@ -1,15 +1,43 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace Datahub.Core.EFCore
 {
-    public class Project_Database
-    {
-        [Key]
-        public Guid Id { get; set; }
+    //[Index(nameof(ResourceID), IsUnique = true)]
+    //public class Project_Database
+    //{
+    //    public const string DB_TYPE_POSTGRES = "psql";
+    //    public const string DB_TYPE_SQL_SERVER = "sql";
 
-        public Datahub_Project Project { get; set; }
-    }
+    //    [Key]
+    //    public Guid Id { get; set; }
+
+    //    [ForeignKey("Project")]
+    //    public int ProjectID { get; set; }
+
+    //    [ForeignKey("Resource")]
+    //    public int ResourceID { get; set; }
+
+    //    [Required]
+    //    [StringLength(128)]
+    //    public string Database_Name { get; set; }
+
+    //    [Required]
+    //    [StringLength(128)]
+    //    public string Database_Server { get; set; }
+
+    //    [Required]
+    //    [StringLength(32)]
+    //    public string Database_Type { get; set; }
+
+    //    public bool IsSqlServerDb => Database_Type == DB_TYPE_SQL_SERVER;
+    //    public bool IsPostgresDb => Database_Type == DB_TYPE_POSTGRES;
+
+    //    public Datahub_Project Project { get; set; }
+    //    public Project_Resources Resource { get; set; }
+    //}
 
     public class Project_Resources
     {
