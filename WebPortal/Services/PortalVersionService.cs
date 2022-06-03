452c828dfa5ef95f12f8c1bdf48723a6b58298ba
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-06-03 08:42:30 -0400

    Version display info using a static class generated by the pipeline

---
+++ WebPortal/Services/PortalVersionService.cs
@@ -0,0 +1,20 @@
+﻿using Datahub.Core.Services;
+using System.Reflection;
+
+namespace Datahub.Portal.Services
+{
+    public class PortalVersionService : IPortalVersionService
+    {
+        public PortalVersionService()
+        {
+        }
+
+        public string ReleaseTag => PortalVersion.ReleaseTag;
+        public string ReleaseUrl => PortalVersion.ReleaseUrl;
+        public string CommitSha => PortalVersion.CommitSha;
+        public string CommitUrl => PortalVersion.CommitUrl;
+        public string ReleaseVersion => Assembly.GetExecutingAssembly()
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+            .InformationalVersion;
+    }
+}
