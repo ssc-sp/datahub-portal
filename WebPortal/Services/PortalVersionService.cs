e93f5f6d0979f2ca52bebaf4c7bc2446e140a3f5
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-06-03 08:42:30 -0400

    Version display info using a static class generated by the pipeline

--- WebPortal/Services/PortalVersionService.cs
+++ WebPortal/Services/PortalVersionService.cs
@@ -5,16 +5,16 @@ namespace Datahub.Portal.Services
 {
     public class PortalVersionService : IPortalVersionService
     {
-        private readonly Dictionary<string, string> _attributes;
-        private readonly string _release;
-
         public PortalVersionService()
         {
-            _attributes = Assembly.GetExecutingAssembly().GetCustomAttributes<AssemblyMetadataAttribute>().ToDictionary(a => a.Key, a => a.Value);
-            _release = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
         }
 
-        public string ReleaseVersion => _release;
-        public string GetCustomValue(string key) => _attributes.ContainsKey(key) ? _attributes[key] : string.Empty;
+        public string ReleaseTag => PortalVersion.ReleaseTag;
+        public string ReleaseUrl => PortalVersion.ReleaseUrl;
+        public string CommitSha => PortalVersion.CommitSha;
+        public string CommitUrl => PortalVersion.CommitUrl;
+        public string ReleaseVersion => Assembly.GetExecutingAssembly()
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+            .InformationalVersion;
     }
 }
