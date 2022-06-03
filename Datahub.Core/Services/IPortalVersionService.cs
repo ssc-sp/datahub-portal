082f3203e353240a8471a771e4e84d0ef7222fac
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-06-03 08:42:30 -0400

    Version display info using a static class generated by the pipeline

---
+++ Datahub.Core/Services/IPortalVersionService.cs
@@ -0,0 +1,11 @@
+﻿namespace Datahub.Core.Services
+{
+    public interface IPortalVersionService
+    {
+        string CommitSha { get; }
+        string CommitUrl { get; }
+        string ReleaseTag { get; }
+        string ReleaseUrl { get; }
+        string ReleaseVersion { get; }
+    }
+}
