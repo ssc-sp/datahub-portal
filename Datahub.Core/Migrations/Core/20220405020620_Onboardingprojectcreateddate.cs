7d31c625fddf5e902622a6b9aa5faa01f9214455
Erik <erik@apption.com>
2022-06-13 17:37:47 -0400

    tried doing something with migrations

---
+++ Datahub.Core/Migrations/Core/20220405020620_Onboardingprojectcreateddate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datahub.Core.Migrations.Core
+{
+    public partial class Onboardingprojectcreateddate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ProjectCreatedDate",
+                table: "OnboardingApps",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProjectCreatedDate",
+                table: "OnboardingApps");
+        }
+    }
+}
