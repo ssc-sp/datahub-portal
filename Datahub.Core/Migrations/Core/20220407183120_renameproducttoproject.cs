4baad1d426600db6e7220b725223f87322c13278
Erik <erik@apption.com>
2022-06-13 17:37:47 -0400

    tried doing something with migrations

---
+++ Datahub.Core/Migrations/Core/20220407183120_renameproducttoproject.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datahub.Core.Migrations.Core
+{
+    public partial class renameproducttoproject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Project_Name",
+                table: "OnboardingApps",
+                newName: "Product_Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Product_Name",
+                table: "OnboardingApps",
+                newName: "Project_Name");
+        }
+    }
+}
