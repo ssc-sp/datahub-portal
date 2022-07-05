4402e4e1bdbc74450ab5d2e6edad49a442bcfefc
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-07-05 15:01:30 -0400

    #3477 Adding infinite scrolling to the Catalog Search

---
+++ WebPortal/Components/InfiniteScrollingItemsProviderRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace Datahub.Portal.Components;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+public sealed class InfiniteScrollingItemsProviderRequest
+{
+    public InfiniteScrollingItemsProviderRequest(long startIndex, CancellationToken cancellationToken)
+    {
+        StartIndex = startIndex;
+        CancellationToken = cancellationToken;
+    }
+
+    public long StartIndex { get; }
+    public CancellationToken CancellationToken { get; }
+}
+
+public delegate Task<IEnumerable<T>> ItemsProviderRequestDelegate<T>(InfiniteScrollingItemsProviderRequest request);
