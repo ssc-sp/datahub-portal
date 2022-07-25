f545f7aa9330d24a9059198b9f97c4e58fa592b9
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-07-25 10:41:58 -0400

    Fixing issue when filtering Catalog object by security classification

--- Datahub.Core/SecurityClassification.cs
+++ Datahub.Core/SecurityClassification.cs
@@ -1,7 +1,19 @@
+using Datahub.Metadata.Model;
+
 public static class SecurityClassification
 {
     public const string Unclassified = "Unclassified";
     public const string ProtectedA = "Protected A";
     public const string ProtectedB = "Protected B";
     public static readonly string[] SECURITY_CLASSES = { Unclassified, ProtectedA, ProtectedB };
+
+    public static string GetSecurityClassification(ClassificationType classificationType)
+    {
+        return classificationType switch
+        {
+            ClassificationType.ProtectedA => SecurityClassification.ProtectedA,
+            ClassificationType.ProtectedB => SecurityClassification.ProtectedB,
+            _ => SecurityClassification.Unclassified
+        };
+    }
 }
