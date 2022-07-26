59e1c1f01e8281195812142a051cdd84099a2fc4
gcnabeel <nabeel.bader@canada.ca>
2022-07-26 12:17:21 -0400

    added constant file

---
+++ Datahub.Core/Configuration/ConfigurationConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.Core.Configuration
+{
+    public static class ConfigurationConstants
+    {
+        public const string CONTACT_EMAIL = "Contact:Email";
+    }
+}
