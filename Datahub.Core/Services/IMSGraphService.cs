04c7dc306dc768c74086f4606092bdcbd9f4479d
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-07-28 11:50:19 -0400

    Catalog file upload for Admins

--- Datahub.Core/Services/IMSGraphService.cs
+++ Datahub.Core/Services/IMSGraphService.cs
@@ -2,19 +2,19 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Datahub.Core.Data;
-using Microsoft.Graph;
 
 namespace Datahub.Core.Services
 {
-    public interface IMSGraphService
+	public interface IMSGraphService
     {
         Dictionary<string, GraphUser> UsersDict { get; set; }
 
-        Task<GraphUser> GetUserAsync(string userId, CancellationToken tkn);
-        Task<GraphUser> GetUserFromEmailAsync(string email, CancellationToken tkn);
-        Task<Dictionary<string, GraphUser>> GetUsersListAsync(string filterText, CancellationToken tkn);
-        Task<string> GetUserName(string userId, CancellationToken tkn = default);
-        Task<string> GetUserEmail(string userId, CancellationToken tkn);
-        Task<string> GetUserIdFromEmailAsync(string email, CancellationToken tkn);
+        Task<GraphUser> GetUserAsync(string userId, CancellationToken token);
+        Task<GraphUser> GetUserFromEmailAsync(string email, CancellationToken token);
+        Task<Dictionary<string, GraphUser>> GetUsersListAsync(string filterText, CancellationToken token);
+        Task<string> GetUserName(string userId, CancellationToken token = default);
+        Task<string> GetUserEmail(string userId, CancellationToken token);
+        Task<string> GetUserIdFromEmailAsync(string email, CancellationToken token);
+        Task<GraphUser> GetUserFromSamAccountNameAsync(string userName, CancellationToken token);
     }
 }
