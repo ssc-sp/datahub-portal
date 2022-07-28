733bae76914724dd58b6cdcd6d7a1fc817f360c9
gcnabeel <nabeel.bader@canada.ca>
2022-07-28 11:19:41 -0400

    made default email a constant

--- Datahub.Core/Configuration/ConfigurationConstants.cs
+++ Datahub.Core/Configuration/ConfigurationConstants.cs
@@ -8,6 +8,7 @@ namespace Datahub.Core.Configuration
 {
     public static class ConfigurationConstants
     {
+        public const string DEFAULT_EMAIL = "datahub@nrcan-rncan.gc.ca";
         public const string CONTACT_EMAIL = "Contact:Email";
     }
 }
