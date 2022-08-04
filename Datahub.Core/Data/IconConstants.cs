30abf11456eda21a8d7c45a33762d7ee70bf13e7
Yan <yjmrobert@gmail.com>
2022-08-04 14:46:59 -0400

    finished side nav layout

--- Datahub.Core/Data/IconConstants.cs
+++ Datahub.Core/Data/IconConstants.cs
@@ -9,63 +9,73 @@ namespace Datahub.Core.Data
 {
     public class Icon
     {
-        public string Name { get; set; }
-        public string Color { get; set; }
+        public string Name { get; init; }
+        public string Color { get; init; }
+        public string HexColor { get; init; }
         
         public static readonly string DEFAULT_PROJECT_ICON = "swatchbook";
 
         public static readonly Icon HOME = new()
         {
             Name = "fad fa-home",
-            Color = Colors.Blue.Default,
+            Color = "blue",
+            HexColor = Colors.Blue.Default,
         };
 
         public static readonly Icon STORAGE = new()
         {
             Name = "fad fa-hdd",
-            Color = Colors.Indigo.Default,
+            Color = "indigo",
+            HexColor = Colors.Indigo.Default,
         };
 
         public static readonly Icon RESOURCES = new()
         {
             Name = "fad fa-books",
-            Color = Colors.Purple.Default,
+            Color = "purple",
+            HexColor = Colors.DeepPurple.Default,
         };
 
         public static readonly Icon TOOLS = new()
         {            
             Name = "fad fa-tools",
-            Color = Colors.Orange.Default,
+            Color = "orange",
+            HexColor = Colors.Orange.Default,
         };
 
         public static readonly Icon DATASETS = new()
         {
             Name = "fad fa-cabinet-filing",
-            Color = Colors.Pink.Default,
+            Color = "pink",
+            HexColor = Colors.Pink.Default,
         };
 
         public static readonly Icon POWERBI = new()
         {
             Name = "fad fa-chart-bar",
-            Color = Colors.Yellow.Default,
+            Color = "yellow",
+            HexColor = Colors.Yellow.Default,
         };
 
         public static readonly Icon ADMIN = new()
         {
             Name = "fad fa-user-cog",
-            Color = Colors.Green.Default,
+            Color = "green",
+            HexColor = Colors.Green.Default,
         };
 
         public static readonly Icon DATAENTRY = new()
         {
             Name = "fad fa-keyboard",
-            Color = Colors.Grey.Default,
+            Color = "grey",
+            HexColor = Colors.Grey.Default,
         };
 
         public static readonly Icon PROJECT = new()
         {
             Name = "fad fa-project-diagram",
-            Color = Colors.Yellow.Default,
+            Color = "yellow",
+            HexColor = Colors.Green.Darken1,
         };
     }
 }
