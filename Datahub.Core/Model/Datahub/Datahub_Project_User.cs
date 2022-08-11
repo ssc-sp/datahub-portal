34335e82d8541f9daf6cbe9d7b1d789e6152109a
Yan <yjmrobert@gmail.com>
2022-08-11 10:34:16 -0400

    split up classes into files

---
+++ Datahub.Core/Model/Datahub/Datahub_Project_User.cs
@@ -0,0 +1,31 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Elemental.Components;
+
+namespace Datahub.Core.EFCore;
+
+public class Datahub_Project_User {
+
+    [AeFormIgnore]
+    [Key]
+
+    public int ProjectUser_ID { get; set; }
+
+    [StringLength(200)]
+    public string User_ID { get; set; }
+    public DateTime? Approved_DT { get; set; }
+
+    public string ApprovedUser { get; set; }
+
+    public bool IsAdmin { get; set; }
+
+    public bool IsDataApprover { get; set; }
+    public Datahub_Project Project { get; set; }
+
+    [StringLength(200)]
+    public string User_Name {  get; set; }
+
+    [Timestamp]
+    public byte[] Timestamp { get; set; }
+
+}
