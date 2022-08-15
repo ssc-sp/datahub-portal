2a4c832fd44076d5295c8d1ef4f7fed58436ca34
gcnabeel <nabeel.bader@canada.ca>
2022-08-15 01:12:17 -0400

    initial files

---
+++ Datahub.Finance/Data/FundCenter.cs
@@ -0,0 +1,53 @@
+﻿using Elemental.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Datahub.Portal.Data.Finance
+{
+    public class FundCenter
+    {
+        [Key]
+        [AeFormIgnore]
+        public int FundCenter_ID { get; set; }
+
+        [Required]
+        public int FiscalYear { get; set; }
+
+        [AeLabel(isDropDown: true)]
+        [Required]
+        public string Sector { get; set; }
+
+        [AeLabel(isDropDown: true)]
+        [Required]
+        public string Branch { get; set; }
+
+        [AeLabel(isDropDown: true)]
+        [Required]
+        public string Division { get; set; }
+
+        public double? AttritionRate { get; set; }
+
+        [MaxLength(4000)]
+        public string Description { get; set; }
+
+        [AeFormIgnore]
+        public string Last_Updated_UserId { get; set; }
+
+        [AeFormIgnore]
+        public DateTime Last_Updated_DT { get; set; }
+
+        [AeFormIgnore]
+        public string Created_UserId { get; set; }
+
+        [AeFormIgnore]
+        public DateTime Created_DT { get; set; }
+
+        [AeFormIgnore]
+        [Timestamp]
+        public byte[] Timestamp { get; set; }
+    }
+}
