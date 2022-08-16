677231592c16fc7ffc386a3c90fbb2dc69b707e6
gcnabeel <nabeel.bader@canada.ca>
2022-08-15 22:43:30 -0400

    mudforms template

--- Datahub.Finance/Data/Migrations/20220815172030_addedexpirydatefield.Designer.cs
+++ Datahub.Finance/Data/Migrations/20220815172030_addedexpirydatefield.Designer.cs
@@ -4,6 +4,7 @@ using Datahub.Portal.Data.Finance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Datahub.Portal.Migrations.Forms.Finance
 {
     [DbContext(typeof(FinanceDBContext))]
-    partial class FinanceDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220815172030_addedexpirydatefield")]
+    partial class addedexpirydatefield
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
