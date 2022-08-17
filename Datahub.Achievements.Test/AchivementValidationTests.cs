f576fd71c50086fece14282b146618ad54f14775
Yan <yjmrobert@gmail.com>
2022-08-17 12:16:49 -0400

    added parameterized unit tests

---
+++ Datahub.Achievements.Test/AchivementValidationTests.cs
@@ -0,0 +1,91 @@
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Datahub.Achievements.Test;
+
+public class AchievementValidationTests
+{
+    private static readonly string AchievementDirectoryPath =
+        Path.Join(Directory.GetCurrentDirectory(), "../../../../Datahub.Achievements/Achievements");
+    private static readonly string UserId = Guid.NewGuid().ToString();
+
+    private static readonly Dictionary<string, (DatahubUserTelemetry, DatahubUserTelemetry)> EarnedUserTelemetryDictionary = new()
+    {
+        { "EXP-006", (new DatahubUserTelemetry() { UserId = UserId, ViewedTheirProfile = true}, new DatahubUserTelemetry() { UserId = UserId, ViewedTheirProfile = false}) }
+    };
+
+    private static IEnumerable<object[]> EarnedAchievementParams()
+    {
+        var achievementFactory =
+            AchievementFactory.CreateFromFilesAsync(AchievementDirectoryPath).GetAwaiter().GetResult();
+
+        return achievementFactory.Achievements!.Select(kvp => new object[]
+        {
+            kvp.Key,
+            kvp.Value,
+            EarnedUserTelemetryDictionary[kvp.Key].Item1,
+        });
+    }
+    
+    private static IEnumerable<object[]> NotEarnedAchievementParams()
+    {
+        var achievementFactory =
+            AchievementFactory.CreateFromFilesAsync(AchievementDirectoryPath).GetAwaiter().GetResult();
+
+        return achievementFactory.Achievements!.Select(kvp => new object[]
+        {
+            kvp.Key,
+            kvp.Value,
+            EarnedUserTelemetryDictionary[kvp.Key].Item2,
+        });
+    }
+
+
+    
+
+    [Test]
+    [TestCaseSource(nameof(EarnedAchievementParams))]
+    [Parallelizable(ParallelScope.None)]
+    public async Task EarnedIfConditionsMet(string code, Achievement achievement, DatahubUserTelemetry userTelemetry)
+    {
+        var mockLogger = new Mock<ILogger<AchievementService>>();
+        var achievementService = new AchievementService(mockLogger.Object);
+        achievementService!.AchievementEarned += (_, e) =>
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(e.Achievement, Is.TypeOf(typeof(Achievement)));
+                Assert.That(e.UserId, Is.EqualTo(UserId));
+            });
+
+            if (e.Achievement?.Code != code) return;
+            
+            Assert.Pass();
+        };
+        
+        await achievementService!.RunRulesEngine(userTelemetry, AchievementDirectoryPath);
+    }
+    
+    [Test]
+    [TestCaseSource(nameof(NotEarnedAchievementParams))]
+    [Parallelizable(ParallelScope.None)]
+    public async Task NotEarnedIfConditionsNotMet(string code, Achievement achievement, DatahubUserTelemetry userTelemetry)
+    {
+        var mockLogger = new Mock<ILogger<AchievementService>>();
+        var achievementService = new AchievementService(mockLogger.Object);
+        achievementService!.AchievementEarned += (_, e) =>
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(e.Achievement, Is.TypeOf(typeof(Achievement)));
+                Assert.That(e.UserId, Is.EqualTo(UserId));
+            });
+
+            if (e.Achievement?.Code != code) return;
+            
+            Assert.Fail();
+        };
+
+        await achievementService!.RunRulesEngine(userTelemetry, AchievementDirectoryPath);
+    }
+}
--- Datahub.Achievements/Achievement.cs
+++ Datahub.Achievements/Achievement.cs
@@ -9,7 +9,6 @@ public class Achievement
     public string? Name { get; set; }
     public string? Description { get; set; }
     public string? Icon { get; set; }
-    
     public List<string>? RuleExpressions { get; init; }
 
     public List<Rule> Rules
