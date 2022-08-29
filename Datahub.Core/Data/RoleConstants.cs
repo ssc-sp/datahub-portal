47c737cf3ecbc807c8cb3805bc4a614072abfbb7
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2022-08-29 16:28:42 -0400

    Hide admin actions in guest mode

--- Datahub.Core/Data/RoleConstants.cs
+++ Datahub.Core/Data/RoleConstants.cs
@@ -10,5 +10,11 @@ namespace Datahub.Core.Data
     {
         public const string DATAHUB_ADMIN_PROJECT = "DHPGLIST";
         public const string DATAHUB_ROLE_ADMIN = DATAHUB_ADMIN_PROJECT + "-admin";
+
+        /// <summary>
+        /// This role should never be assigned to a user, but is used to lock out elements,
+        /// e.g. for an admin using guest mode.
+        /// </summary>
+        public const string LOCKED_OUT_ROLE = "lockedout";
     }
 }
