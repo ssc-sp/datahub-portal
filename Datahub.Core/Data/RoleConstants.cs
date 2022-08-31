84d45a6146c70ee398130ea7fb2bd7d283a89ba8
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2022-08-31 11:37:08 -0400

    Guest mode using "admin-as-guest" role claim

--- Datahub.Core/Data/RoleConstants.cs
+++ Datahub.Core/Data/RoleConstants.cs
@@ -8,13 +8,9 @@ namespace Datahub.Core.Data
 {
     public static class RoleConstants
     {
+        public const string ADMIN_SUFFIX = "-admin";
         public const string DATAHUB_ADMIN_PROJECT = "DHPGLIST";
-        public const string DATAHUB_ROLE_ADMIN = DATAHUB_ADMIN_PROJECT + "-admin";
-
-        /// <summary>
-        /// This role should never be assigned to a user, but is used to lock out elements,
-        /// e.g. for an admin using guest mode.
-        /// </summary>
-        public const string LOCKED_OUT_ROLE = "lockedout";
+        public const string DATAHUB_ROLE_ADMIN = DATAHUB_ADMIN_PROJECT + ADMIN_SUFFIX;
+        public const string DATAHUB_ROLE_ADMIN_AS_GUEST = DATAHUB_ADMIN_PROJECT + "-admin-as-guest";
     }
 }
