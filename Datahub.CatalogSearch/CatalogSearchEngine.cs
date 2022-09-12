c99d8bb30212da08e506df1ac8052aceb9a89672
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-09-12 13:54:28 -0400

    Saving catalog search changes

---
+++ Datahub.CatalogSearch/CatalogSearchEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.CatalogSearch
+{
+    public class CatalogSearchEngine : ICatalogSearchEngine
+    {
+        public ILanguageCatalogSearch GetEnglishSearchEngine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILanguageCatalogSearch GetFrenchSearchEngine()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Datahub.CatalogSearch/Lucene/LanguageCatalogSearch.cs
@@ -0,0 +1,86 @@
+﻿using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Util;
+using System.Reflection;
+using Lucene.Net.Index;
+using Lucene.Net.Store;
+using Lucene.Net.Documents;
+using Lucene.Net.Search;
+using Lucene.Net.QueryParsers.Classic;
+
+namespace Datahub.CatalogSearch.Lucene
+{
+    public class LanguageCatalogSearch : ILanguageCatalogSearch
+    {
+        const LuceneVersion LUCENE_VERSION = LuceneVersion.LUCENE_48;
+        const string CONTENT_FIELD = "content";
+
+        private readonly IndexWriter _writer;
+        private readonly QueryParser _queryParser;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="language">Only supports: English (en) or French (fr).</param>
+        public LanguageCatalogSearch(string language)
+        {
+            // read stopwords
+            var stopWordReader = LoadStopWords(language);
+
+            // create the anylyzer
+            var standardAnalyzer = new StandardAnalyzer(LUCENE_VERSION, stopWordReader);
+
+            // create an index writer config
+            IndexWriterConfig indexConfig = new(LUCENE_VERSION, standardAnalyzer)
+            {
+                OpenMode = OpenMode.CREATE
+            };
+
+            // init index writer (in-memory)
+            _writer = new IndexWriter(new RAMDirectory(), indexConfig);
+
+            // init query parser
+            _queryParser = new QueryParser(LUCENE_VERSION, CONTENT_FIELD, standardAnalyzer);
+        }
+
+        public void AddDocument(string id, string content)
+        {
+            var doc = new Document();
+            doc.Add(new StringField("id", id, Field.Store.YES));
+            doc.Add(new TextField("content", content, Field.Store.YES));
+            _writer.AddDocument(doc);
+        }
+
+        public IEnumerable<string> GetAutocompleteSuggestions(string text, int maxResults)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> SearchDocuments(string text, int maxResults)
+        {
+            using DirectoryReader reader = _writer.GetReader(applyAllDeletes: true);
+            var searcher = new IndexSearcher(reader);
+
+            Query query = _queryParser.Parse(text);
+            TopDocs topDocs = searcher.Search(query, maxResults);
+
+            foreach (var d in topDocs.ScoreDocs)
+            {
+                Document resultDoc = searcher.Doc(d.Doc);
+                yield return resultDoc.Get("id");
+            }
+        }
+
+        public void FlushIndexes()
+        {
+            _writer.Commit();
+        }
+
+        private TextReader LoadStopWords(string language)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Datahub.CatalogSearch.Lucene.stopwords_{language.txt}";
+            var stream = assembly?.GetManifestResourceStream(resourceName);
+            return new StreamReader(stream ?? new MemoryStream());            
+        }
+    }
+}
