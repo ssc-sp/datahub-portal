80c4d88b4ca62e78568c5ed9212e4d05448a92ed
Yan <yjmrobert@gmail.com>
2022-09-13 08:33:43 -0400

    fixed the rules engine logic for the achievements #62 #61

--- Datahub.Achievements/Models/DatahubTelemetryEventMetric.cs
+++ Datahub.Achievements/Models/DatahubTelemetryEventMetric.cs
@@ -5,6 +5,6 @@ namespace Datahub.Achievements.Models;
 public class DatahubTelemetryEventMetric
 {
     [Key]
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public int Value { get; set; }
 }
