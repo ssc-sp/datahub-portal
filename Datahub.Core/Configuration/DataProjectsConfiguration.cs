ed96e5f179b64675aa01848860f37674a9af9481
Nick Salt <nsalt@apption.com>
2022-09-14 16:04:30 -0400

    updated project tool cards to use new button

--- Datahub.Core/Configuration/DataProjectsConfiguration.cs
+++ Datahub.Core/Configuration/DataProjectsConfiguration.cs
@@ -26,6 +26,8 @@ namespace Datahub.Core.Configuration
         public bool ProjectDatabases { get; set; } = true;
         public bool DataManagement { get; set; } = true;
         public bool IoT { get; set; } = true;
+        public bool StorageCost { get; set; } = true;
+        public bool ComputeCost { get; set; } = true;
 
     }
 }
