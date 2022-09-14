41fb12c269d4d1972696a12b0d0ed76c1daf4ccc
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-09-14 13:35:41 -0400

    Language Training session and year releated changes

--- LanguageTraining/Data/Migrations/20220912203850_AddingSeasonRegistrationPeriod.Designer.cs
+++ LanguageTraining/Data/Migrations/20220912203850_AddingSeasonRegistrationPeriod.Designer.cs
@@ -4,6 +4,7 @@ using Datahub.Portal.Data.LanguageTraining;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Datahub.Portal.Migrations
 {
     [DbContext(typeof(LanguageTrainingDBContext))]
-    partial class LanguageTrainingDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220912203850_AddingSeasonRegistrationPeriod")]
+    partial class AddingSeasonRegistrationPeriod
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -224,9 +226,6 @@ namespace Datahub.Portal.Migrations
                     b.Property<string>("Training_Type")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
-
-                    b.Property<int>("Year_For_Language_Training")
-                        .HasColumnType("int");
 
                     b.HasKey("Application_ID");
 
