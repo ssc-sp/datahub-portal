7931cb372fb4c88070e8f067658f48ac318bb854
Nick Salt <nsalt@apption.com>
2022-09-30 12:56:43 -0400

    update monthly cost function to get accurate values and added update button to ui

--- Datahub.Graph.Functions/CostManagement/CostManagementRow.cs
+++ Datahub.Graph.Functions/CostManagement/CostManagementRow.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Globalization;
 using System.Text.Json.Nodes;
 
 namespace Datahub.Graph.Functions.CostManagement;
@@ -9,25 +7,15 @@ public class CostManagementRow
     public decimal TotalCost { get; init; }
     // ReSharper disable once InconsistentNaming
     public decimal TotalCostUSD { get; init; }
-    public DateTime UsageDate { get; init; }
-    public string TagKey { get; init; }
+    
+    #nullable enable
     public string? TagName { get; init; }
-    public string ChargeType { get; init; }
-    public string ResourceType { get; init; }
-    public string ResourceGroup { get; init; }
-    public string Currency { get; init; }
+    #nullable disable
     
     public CostManagementRow(JsonArray row)
     {
         TotalCost = (decimal?)row[0] ?? 0;
         TotalCostUSD = (decimal?)row[1] ?? 0;
-        // ReSharper disable once StringLiteralTypo
-        UsageDate = DateTime.ParseExact(row[2].ToString(), "yyyyMMdd", CultureInfo.InvariantCulture );
-        TagKey = (string)row[3] ?? string.Empty;
-        TagName = (string)row[4];
-        ChargeType = (string)row[5] ?? string.Empty;
-        ResourceType = (string)row[6] ?? string.Empty;
-        ResourceGroup =(string)row[7] ?? string.Empty;
-        Currency = (string)row[8] ?? string.Empty;
+        TagName = (string)row[3];
     }
 }
