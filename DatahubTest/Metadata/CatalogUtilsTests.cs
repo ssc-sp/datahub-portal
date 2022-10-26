4c0dedbb4ac10fffc0363e272b10ea42c15117f2
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-10-26 13:31:31 -0400

    Grouping catalog objects and tests

---
+++ DatahubTest/Metadata/CatalogUtilsTests.cs
@@ -0,0 +1,50 @@
+﻿using Datahub.Metadata.DTO;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Datahub.Metadata.Model;
+using Datahub.Metadata.Utils;
+
+namespace Datahub.Tests.Meta_Data
+{
+	public class CatalogUtilsTests
+	{
+		[Fact]
+		public void GroupResults_CountAndPickedResults_AsExpected()
+		{
+			var targetGroup = Guid.NewGuid();
+			var results = new List<CatalogObjectResult>
+			{
+				new CatalogObjectResult() { CatalogObjectId = 1, Language = CatalogObjectLanguage.English, GroupId = targetGroup },
+                new CatalogObjectResult() { CatalogObjectId = 2, Language = CatalogObjectLanguage.French, GroupId = targetGroup },
+                new CatalogObjectResult() { CatalogObjectId = 3, Language = CatalogObjectLanguage.English },
+			};
+
+			var grouped = CatalogUtils.GroupResults(results, CatalogObjectLanguage.English);
+            
+			Assert.NotNull(grouped);
+            Assert.Equal(2, grouped.Count);
+			Assert.Equal(1, grouped[0].CatalogObjectId);
+            Assert.Equal(3, grouped[1].CatalogObjectId);
+        }
+
+        [Fact]
+        public void GroupResults_ResultsOrder_AsExpected()
+        {
+            var targetGroup = Guid.NewGuid();
+            var results = new List<CatalogObjectResult>
+            {
+                new CatalogObjectResult() { CatalogObjectId = 1, Language = CatalogObjectLanguage.French, GroupId = targetGroup },
+                new CatalogObjectResult() { CatalogObjectId = 2, Language = CatalogObjectLanguage.English, GroupId = targetGroup },
+                new CatalogObjectResult() { CatalogObjectId = 3, Language = CatalogObjectLanguage.English },
+            };
+
+            var grouped = CatalogUtils.GroupResults(results, CatalogObjectLanguage.English);
+
+            Assert.NotNull(grouped);
+            Assert.Equal(2, grouped.Count);
+            Assert.Equal(2, grouped[0].CatalogObjectId);
+            Assert.Equal(3, grouped[1].CatalogObjectId);
+        }
+    }
+}
