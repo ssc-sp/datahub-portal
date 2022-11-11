95fbfa02d9ccd0564dadf041fa334f2e7b639ffe
gcnabeel <83655937+gcnabeel@users.noreply.github.com>
2022-11-11 16:33:38 -0500

    fixed sector dropdowns

---
+++ Datahub.Core/Migrations/Core/20221111201626_migrationtest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datahub.Core.Migrations.Core
+{
+    public partial class migrationtest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
