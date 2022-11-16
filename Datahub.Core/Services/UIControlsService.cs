7baa388bb5d15911e76d6110729e70536ff020b1
Hipolito Perez-Garcia <hipolito.perez-garcia@nrcan-rncan.gc.ca>
2022-11-16 11:21:35 -0500

    Adding dialogs

--- Datahub.Core/Services/UIControlsService.cs
+++ Datahub.Core/Services/UIControlsService.cs
@@ -1,4 +1,6 @@
+using Datahub.Core.Components;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 using System;
 using System.Threading.Tasks;
 
@@ -13,7 +15,15 @@ namespace Datahub.Core.Services
         public event Action OnRightSidebarChange;
         public event Action OnModalChange;
         public event Action OnErrorModalShow;
-        
+
+        private IDialogService _dialogService;
+        private IDialogReference _dialogReference = null;
+
+        public UIControlsService(IDialogService dialogService)
+        {
+            _dialogService = dialogService;
+        }
+
         private void NotifyRightSidebarChange() => OnRightSidebarChange?.Invoke();
         private void NotifyModalChange() => OnModalChange?.Invoke();
         private void NotifyErrorModalShow() => OnErrorModalShow?.Invoke();
@@ -36,6 +46,29 @@ namespace Datahub.Core.Services
         public void ShowErrorModal()
         {
             NotifyErrorModalShow();
-        }     
+        }
+
+        public void ShowDialog(string dialogTitle, RenderFragment contentFragment)
+        {
+            var parameters = new DialogParameters();
+            parameters.Add("Content", contentFragment);
+
+            var options = new DialogOptions() 
+            { 
+                MaxWidth = MaxWidth.Medium, 
+                FullWidth = true, 
+                CloseButton = true,
+                CloseOnEscapeKey = true,
+                DisableBackdropClick = true
+            };
+
+            _dialogReference = _dialogService.Show<DialogModalFrame>(dialogTitle, parameters, options);
+        }
+
+        public void HideDialog()
+        {
+            _dialogReference?.Close();
+            _dialogReference = default;
+        }
     }
 }
