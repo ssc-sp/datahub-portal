9ada5618e84d505ee8ac8fda937d6158f5819a6b
gcerik <82101285+gcerik@users.noreply.github.com>
2022-11-18 11:57:06 -0500

    fix: merged develop updates fix: corrected class names for dotnet 7

--- Datahub.Core/Migrations/Core/20221113015837_newcommentfields.cs
+++ Datahub.Core/Migrations/Core/20221113015837_newcommentfields.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Datahub.Core.Migrations.Core
 {
-    public partial class newcommentfields : Migration
+    public partial class Newcommentfields : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
