8a515b328335fffe683c5aa3be5529a5add929fa
Alexander Khavich <alexander.khavich@nrcan-rncan.gc.ca>
2022-11-18 11:23:22 -0500

    Resources CSS WIP

---
+++ Datahub.Core/Components/Markdown/MudMDStyleExtension.cs
@@ -0,0 +1,30 @@
+﻿using Markdig;
+using Markdig.Renderers;
+using Markdig.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.Core.Components.Markdown
+{
+    public class MudMDStyleExtension : IMarkdownExtension
+    {
+        public void Setup(MarkdownPipelineBuilder pipeline)
+        {
+            pipeline.DocumentProcessed -= Pipeline_DocumentProcessed;
+            pipeline.DocumentProcessed += Pipeline_DocumentProcessed;
+        }
+
+        private static void Pipeline_DocumentProcessed(MarkdownDocument document)
+        {
+            
+        }
+
+        public void Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
+        {
+            
+        }
+    }
+}
