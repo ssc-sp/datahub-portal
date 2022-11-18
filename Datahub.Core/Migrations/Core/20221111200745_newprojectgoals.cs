dbe73987b5f22c632c899df23f13519d712c8004
gcerik <82101285+gcerik@users.noreply.github.com>
2022-11-18 11:57:06 -0500

    fix: merged develop updates fix: corrected class names for dotnet 7

--- Datahub.Core/Migrations/Core/20221111200745_newprojectgoals.cs
+++ Datahub.Core/Migrations/Core/20221111200745_newprojectgoals.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Datahub.Core.Migrations.Core
 {
-    public partial class newprojectgoals : Migration
+    public partial class Newprojectgoals : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
