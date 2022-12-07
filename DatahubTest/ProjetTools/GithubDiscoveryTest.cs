a22b0433519358bce3f1100f51d32d29d752be20
Erik <erik@apption.com>
2022-12-06 21:55:34 -0500

    feature: added regression on errors

--- DatahubTest/ProjetTools/GithubDiscoveryTest.cs
+++ DatahubTest/ProjetTools/GithubDiscoveryTest.cs
@@ -11,14 +11,21 @@ using Markdig.Syntax.Inlines;
 
 namespace Datahub.Tests.ProjetTools
 {
-    public record GitHubModuleDescriptor
+    public record GitHubModule
     {
         public string Name { get; init; }
         public string Path { get; init; }
+        public string? CalculatorPath { get; init; }
+
+        public List<GitHubModuleDescriptor> Descriptors { get; init; }
+    }
+
+    public record GitHubModuleDescriptor
+    {
+        public string Language { get; set; }
         public string Title { get; init; }
         public string? Description { get; init; }
         public string[]? Tags { get; init; }
-        public string? CalculatorPath { get; init; }
     }
 
     public record RepositoryDescriptorErrors
@@ -32,8 +39,10 @@ namespace Datahub.Tests.ProjetTools
         public const string GitHubOwner = "ssc-sp";
         public const string GitHubRepo = "datahub-resource-modules";
         public const string GitHubBranchName = "main";
+        public const string GitHubModuleFolder = "/modules";
 
         public const string GitHubRawURL = "https://raw.githubusercontent.com/ssc-sp/datahub-resource-modules/";
+        private const string GitHubModuleReadme = "datahub.readme.md";
         private RepositoryContentsClient contentClient;
         private List<RepositoryDescriptorErrors> errors;
 
@@ -46,7 +55,7 @@ namespace Datahub.Tests.ProjetTools
         }
 
         [Fact]
-        public async Task ListModulesFromGitHub()
+        public async Task TestListModulesFromGitHub()
         {
             //main/modules/azure-databricks/datahub.readme.md
             //main/modules/azure-databricks/datahub.parameters.json
@@ -54,32 +63,41 @@ namespace Datahub.Tests.ProjetTools
             //var github = new GitHubClient(new ProductHeaderValue("ssc-datahub"));
             //var repo = await github.Repository.Get(GitHubOwner,GitHubRepo);
             //Assert.NotNull(repo);
-            var data = await contentClient.GetAllContentsByRef(GitHubOwner, GitHubRepo, "/modules", GitHubBranchName);//, 
+            var data = await contentClient.GetAllContentsByRef(GitHubOwner, GitHubRepo, GitHubModuleFolder, GitHubBranchName);//, 
             Assert.NotNull(data);
             var modules = data.Where(d => d.Type == ContentType.Dir).ToList();
             Assert.True(modules.Count >= 4);
         }
 
         [Fact]
-        public async Task CheckIfValidModule()
+        public async Task TestCheckIfValidModule()
         {
             var c = new Connection(new ProductHeaderValue("ssc-datahub"));
             var connection = new ApiConnection(c);
             contentClient = new RepositoryContentsClient(connection);
-            var data = await contentClient.GetAllContentsByRef(GitHubOwner, GitHubRepo, "/modules", GitHubBranchName);//, 
+            var data = await contentClient.GetAllContentsByRef(GitHubOwner, GitHubRepo, GitHubModuleFolder, GitHubBranchName);//, 
             Assert.NotNull(data);
-            var modules = data.Where(d => d.Type == ContentType.Dir).ToList();
-            Assert.True(modules.Count >= 4);
-            var modulesDescriptors = await data.ToAsyncEnumerable().SelectAwait(async dir => await GetGitHubModule(dir)).Where(d => d != null).ToListAsync();
-            Assert.True(modulesDescriptors.Count >= 0);
+            var folders = data.Where(d => d.Type == ContentType.Dir).ToList();
+            Assert.True(folders.Count >= 4);
+            var modules = await folders.ToAsyncEnumerable().SelectAwait(async dir => await GetGitHubModule(dir)).Where(d => d != null).ToListAsync();
+            Assert.True(modules.Count >= 0);
+            Assert.Equal(10, errors.Count);
+            Assert.Contains(errors, e1 => e1.Path == "modules/azure-databricks-resource" && e1.Error == "Module azure-databricks-resource is missing 'datahub.readme.md' file");
         }
 
-        private async Task<GitHubModuleDescriptor?> GetGitHubModule(RepositoryContent dir)
+        private async Task<GitHubModule?> GetGitHubModule(RepositoryContent dir)
         {
             var content = await contentClient.GetAllContentsByRef(GitHubOwner, GitHubRepo, dir.Path, GitHubBranchName);//, 
-            var readme = content.Where(c => c.Name.ToLower() == "datahub.readme.md").FirstOrDefault();
+            var readme = content.Where(c => c.Name.ToLower() == GitHubModuleReadme).FirstOrDefault();
             if (readme is null)
+            {
+                errors.Add(new RepositoryDescriptorErrors()
+                {
+                    Path = dir.Path,
+                    Error = $"Module {dir.Name} is missing '{GitHubModuleReadme}' file"
+                });
                 return null;
+            }
             var readmeContent = await contentClient.GetRawContentByRef(GitHubOwner, GitHubRepo, readme.Path, GitHubBranchName);
             var readmeDoc = Markdown.Parse(Encoding.UTF8.GetString(readmeContent!));
             var readmeDocFlattened = readmeDoc.Descendants().ToList();
@@ -88,11 +106,28 @@ namespace Datahub.Tests.ProjetTools
             Assert.True(descriptors.ContainsKey("Title"));
             Assert.True(descriptors.ContainsKey("Why"));
             Assert.True(descriptors.ContainsKey("Tags"));
-            return new GitHubModuleDescriptor() { Title = ValidateKey("Title", dir, descriptors)?? "Missing Title", 
+            var french = GetSubSections(readmeDocFlattened, "Français", 1);
+            var descriptors_fr = ExtractSubSections(french, 2);
+            //Language            
+            
+            var en =  new GitHubModuleDescriptor() { Title = ValidateKey("Title", dir, descriptors)?? "Missing Title", 
                 Description = ValidateKey("Why", dir, descriptors), 
-                Tags = ValidateKey("Tags", dir, descriptors)?.Split(",").Select(t => t.Trim()).ToArray(), 
-                Name = dir.Name, 
-                Path = dir.Path };
+                Tags = ValidateKey("Tags", dir, descriptors)?.Split(",").Select(t => t.Trim()).ToArray(),
+                Language = "en"
+            };
+            var fr = new GitHubModuleDescriptor()
+            {
+                Title = ValidateKey("Titre", dir, descriptors_fr) ?? "Titre absent",
+                Description = ValidateKey("Pourquoi", dir, descriptors_fr),
+                Tags = ValidateKey("Mots Clefs", dir, descriptors_fr)?.Split(",").Select(t => t.Trim()).ToArray(),
+                Language = "en"
+            };
+            return new GitHubModule() {
+                Name = dir.Name,
+                Path = dir.Path,
+                CalculatorPath = ValidateKey("Calculator", dir, descriptors),
+                Descriptors = new List<GitHubModuleDescriptor>() { en,fr}
+            };
         }
 
         private string? ValidateKey(string key, RepositoryContent module, Dictionary<string, string> dic)
@@ -156,7 +191,7 @@ namespace Datahub.Tests.ProjetTools
                 }
                 if (item is ParagraphBlock paragraph && currentHeader != null)
                 {
-                    result[currentHeader] = paragraph.Inline?.FirstChild?.ToString();
+                    result[currentHeader] = paragraph!.Inline?.FirstChild?.ToString();
                     currentHeader = null;
                 }
             }
