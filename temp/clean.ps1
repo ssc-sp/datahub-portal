bf46fe98934575d6acd478073cc474325da5f460
Erik <erik@apption.com>
2022-12-09 14:36:04 -0500

    fix: removed cleanup script

--- temp/clean.ps1
+++
@@ -1,8 +0,0 @@
-gci . -Recurse | foreach {
-   if( $_.psiscontainer -eq $true){
-      if((gci $_.FullName) -eq $null){
-         Write-Output "Removing Empty folder $($_.FullName)"
-         $_.FullName | Remove-Item -Force
-      }
-}
-}
