320e3f66f3d60bdf9e3ec2a6ed5a51e045d97344
Yan <yjmrobert@gmail.com>
2022-12-12 12:55:58 -0500

    :construction: moved ui.tests project into .specs and started on accessibility feature

---
+++ Portal/test/Datahub.Specs/Hooks/Hook.cs
@@ -0,0 +1,35 @@
+using BoDi;
+using Datahub.Specs.PageObjects;
+using Microsoft.Playwright;
+
+namespace Datahub.Specs.Hooks;
+
+[Binding]
+public class Hooks
+{
+    [BeforeScenario("a11y")]
+    public async Task BeforeA11yScenario(IObjectContainer container)
+    {
+        var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+        var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false,
+            SlowMo = 1000
+        });
+        var pageObject = new HomePageObject(browser);
+
+        container.RegisterInstanceAs(playwright);
+        container.RegisterInstanceAs(browser);
+        container.RegisterInstanceAs(pageObject);
+    }
+
+    [AfterScenario("a11y")]
+    public async Task AfterA11yScenario(IObjectContainer container)
+    {
+        var browser = container.Resolve<Microsoft.Playwright.IBrowser>();
+        await browser.CloseAsync();
+
+        var playwright = container.Resolve<Microsoft.Playwright.IPlaywright>();
+        playwright.Dispose();
+    }
+}
