27864ece60fb51238338b7730d171bf5f2da8d31
Yan <yjmrobert@gmail.com>
2022-12-19 15:47:59 -0500

    :wrench: updated Program.cs

---
+++ Portal/src/Datahub.Functions/Program.cs
@@ -0,0 +1,27 @@
+using Datahub.Core.Model.Datahub;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        //
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("local.settings.json", optional: false, reloadOnChange: true)
+            .Build();
+        
+        var connectionString = config["datahub-mssql-project"];
+        if (connectionString is not null)
+        {
+            services.AddPooledDbContextFactory<DatahubProjectDBContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDbContextPool<DatahubProjectDBContext>(options => options.UseSqlServer(connectionString));
+        }
+
+    })
+    .Build();
+
+host.Run();
