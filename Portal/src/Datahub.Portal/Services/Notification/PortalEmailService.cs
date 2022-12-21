35694b2476c6bbbec1704de0adf232ead4b72f5a
gcnabeel <83655937+gcnabeel@users.noreply.github.com>
2022-12-21 12:39:26 -0500

    projects refactored

--- Portal/src/Datahub.Portal/Services/Notification/PortalEmailService.cs
+++ Portal/src/Datahub.Portal/Services/Notification/PortalEmailService.cs
@@ -13,9 +13,7 @@ using Datahub.Core.Services.Security;
 using Datahub.Core.Services.UserManagement;
 using Datahub.Portal.Templates;
 using Datahub.Portal.Templates.FileSharing;
-using Datahub.Portal.Templates.M365Forms;
 using Datahub.Portal.Templates.Onboarding;
-using Datahub.Portal.Templates.PowerBi;
 using Datahub.Core.Utils;
 using MailKit.Net.Smtp;
 using Microsoft.Extensions.Configuration;
@@ -42,21 +40,12 @@ public class EmailConfiguration
 public class PortalEmailService
 {
 
-    public static readonly string EMAIL_CONFIGURATION_ROOT_KEY = "EmailNotification";
-
-    public static readonly string USERNAME_TEMPLATE_KEY = "Username";
-    public static readonly string SERVICE_TEMPLATE_KEY = "Service";
-    public static readonly string DATA_PROJECT_TEMPLATE_KEY = "DataProject";
-
     private static readonly string DATAHUB_ADMIN_PROJECT_CODE = "DHPGLIST";
-
-    private EmailConfiguration _config;
 
     private IStringLocalizer _localizer;
 
     private ILogger<PortalEmailService> _logger;
 
-    private IMSGraphService _graphService;
 
     private ServiceAuthManager _serviceAuthManager;
     private IEmailNotificationService _emailNotificationService;
@@ -65,153 +54,15 @@ public class PortalEmailService
         IStringLocalizer localizer,
         IConfiguration config,
         ILogger<PortalEmailService> logger,
-        IMSGraphService graphService,
         IEmailNotificationService emailNotificationService,
         ServiceAuthManager serviceAuthManager
     )
     {
         _localizer = localizer;
-        _config = new EmailConfiguration();
-        config.Bind(EMAIL_CONFIGURATION_ROOT_KEY, _config);
-        if (_config.AppDomain is null)
-        {
-            logger.LogCritical("No Email Configuration available");
-            _config = null;
-        }
-        _logger = logger;
-        _graphService = graphService;
+    
+        _logger = logger;        
         _serviceAuthManager = serviceAuthManager;
         _emailNotificationService = emailNotificationService;
-    }
-
-        private Dictionary<string, object> BuildNotificationParameters(DatahubProjectInfo projectInfo, string serviceName, string username = null)
-    {
-        var parameters = new Dictionary<string, object>()
-        {
-            { SERVICE_TEMPLATE_KEY, serviceName },
-            { DATA_PROJECT_TEMPLATE_KEY, projectInfo }
-        };
-
-        if (username != null)
-        {
-            parameters.Add(USERNAME_TEMPLATE_KEY, username);
-        }
-
-        return parameters;
-    }
-
-    public async Task SendServiceCreationRequestNotification(string username, string serviceName, DatahubProjectInfo projectInfo, IList<string> recipients)
-    {
-        if (_config is null)
-        {
-            _logger.LogCritical("Cannot send email - no configuration available");
-            return;
-        }
-
-        var parameters = BuildNotificationParameters(projectInfo, serviceName, username);
-
-        var subject = $"[DataHub] New {serviceName} service request";
-
-        var adminLink = _emailNotificationService.BuildAppLink(ServiceCreationRequest.ADMIN_URL);
-        parameters.Add(nameof(ServiceCreationRequest.AdminPageUrl), adminLink);
-
-        var html = await _emailNotificationService.RenderTemplate<ServiceCreationRequest>(parameters);
-
-        await _emailNotificationService.SendEmailMessage(subject, html, recipients);
-    }
-
-    public async Task SendServiceAccessRequestNotification(string username, string serviceName, DatahubProjectInfo projectInfo, IList<string> recipients)
-    {
-        if (_config is null)
-        {
-            _logger.LogCritical("Cannot send email - no configuration available");
-            return;
-        }
-
-        var parameters = BuildNotificationParameters(projectInfo, serviceName, username);
-
-        var subject = $"[DataHub] {serviceName} access request for project {projectInfo.ProjectNameEn} / demande d’accès pour le projet {projectInfo.ProjectNameFr}";
-
-        var adminLink = _emailNotificationService.BuildAppLink(ServiceAccessRequest.ADMIN_URL);
-        parameters.Add(nameof(ServiceAccessRequest.AdminPageLink), adminLink);
-
-        var html = await _emailNotificationService.RenderTemplate<ServiceAccessRequest>(parameters);
-
-        await _emailNotificationService.SendEmailMessage(subject, html, recipients);
-    }
-
-    public async Task SendServiceAccessGrantedNotification(string serviceName, DatahubProjectInfo projectInfo, string recipientAddress, string recipientName = null)
-    {
-        if (_config is null)
-        {
-            _logger.LogCritical("Cannot send email - no configuration available");
-            return;
-        }
-
-        var parameters = BuildNotificationParameters(projectInfo, serviceName);
-
-        var subject = $"[DataHub] {serviceName} service access request approved / demande d’accès au service approuvée";
-
-        var projectPagePath = $"{UrlPathSegment.PROJECTS}/{projectInfo.ProjectCode}";
-        var projectPageLink = _emailNotificationService.BuildAppLink(projectPagePath);
-        parameters.Add(nameof(ServiceAccessRequestApproved.ProjectPageUrl), projectPageLink);
-
-        var html = await _emailNotificationService.RenderTemplate<ServiceAccessRequestApproved>(parameters);
-
-        await _emailNotificationService.SendEmailMessage(subject, html, recipientAddress, recipientName);
-    }
-
-    public async Task SendServiceCreationRequestApprovedIndividual(string serviceName, DatahubProjectInfo projectInfo, string recipientAddress, string recipientName = null)
-    {
-        if (_config is null)
-        {
-            _logger.LogCritical("Cannot send email - no configuration available");
-            return;
-        }
-
-        var parameters = BuildNotificationParameters(projectInfo, serviceName);
-
-        var subject = $"[DataHub] {serviceName} service request approved / demande de service approuvée";
-
-        var projectPagePath = $"{UrlPathSegment.PROJECTS}/{projectInfo.ProjectCode}";
-        var projectPageLink = _emailNotificationService.BuildAppLink(projectPagePath);
-        parameters.Add(nameof(ServiceRequestApproved.ProjectPageUrl), projectPageLink);
-
-        var html = await _emailNotificationService.RenderTemplate<ServiceRequestApproved>(parameters);
-
-        await _emailNotificationService.SendEmailMessage(subject, html, recipientAddress, recipientName);
-    }
-
-    public async Task SendServiceCreationGroupNotification(string serviceName, DatahubProjectInfo projectInfo, IList<string> recipients)
-    {
-        if (_config is null)
-        {
-            _logger.LogCritical("Cannot send email - no configuration available");
-            return;
-        }
-
-        var parameters = BuildNotificationParameters(projectInfo, serviceName);
-
-        var subject = $"[DataHub] {serviceName} service created / {serviceName} service créé ";
-
-        var projectPagePath = $"{UrlPathSegment.PROJECTS}/{projectInfo.ProjectCode}";
-        var projectPageLink = _emailNotificationService.BuildAppLink(projectPagePath);
-        parameters.Add(nameof(ServiceCreatedGroupNotification.ProjectPageUrl), projectPageLink);
-
-        var html = await _emailNotificationService.RenderTemplate<ServiceCreatedGroupNotification>(parameters);
-
-        await _emailNotificationService.SendEmailMessage(subject, html, recipients);
-    }
-
-    public async Task SendAccessRevokedNotification(string serviceName, DatahubProjectInfo projectInfo, string recipientAddress, string recipientName = null)
-    {
-        var parameters = BuildNotificationParameters(projectInfo, serviceName);
-
-        var subject = $"[DataHub] {serviceName} service access revoked / accès au service révoqué";
-
-        var html = await _emailNotificationService.RenderTemplate<ServiceAccessRevoked>(parameters);
-
-        await _emailNotificationService.SendEmailMessage(subject, html, recipientAddress, recipientName);
     }
 
 
@@ -241,16 +92,6 @@ public class PortalEmailService
         await _emailNotificationService.SendEmailMessage(subject, html, parameters.App.Client_Email, parameters.App.Client_Contact_Name);
     }
 
-    public async Task SendExternalPowerBiCreationRequested(PowerBiExternalReportParameters parameters)
-    {
-        var parametersDict = BuildPowerBiExternalReportParameters(parameters);
-
-        var subject = $"External Power Bi Report Requested";
-
-        var html = await _emailNotificationService.RenderTemplate<ExternalPowerBiCreation>(parametersDict);
-
-        await _emailNotificationService.SendEmailMessage(subject, html, parameters.AdminEmailAddresses);
-    }
 
    
     private Dictionary<string, object> BuildOnboardingParameters(OnboardingParameters parameters)
@@ -266,30 +107,6 @@ public class PortalEmailService
         return parametersDict;
     }
 
-    private Dictionary<string, object> BuildPowerBiExternalReportParameters(PowerBiExternalReportParameters parameters)
-    {
-
-        parameters.AppUrl = _emailNotificationService.BuildAppLink(parameters.AppUrl);
-        var parametersDict = new Dictionary<string, object>()
-        {
-            { "ApplicationParameters", parameters }
-
-        };
-
-        return parametersDict;
-    }
-
-    public async Task SendPowerBiExternalUrlEmail(PowerBiExternalReportParameters parameters)
-    {
-        var parametersDict = BuildPowerBiExternalReportParameters(parameters);
-
-        var subject = $"External Power Bi Report Request";
-
-        var html = await _emailNotificationService.RenderTemplate<ExternalPowerBiCreated>(parametersDict);
-
-        await _emailNotificationService.SendEmailMessage(subject, html, parameters.App.RequestingUser, parameters.App.RequestingUser);
-
-    }
 
     public async Task<IList<MailboxAddress>> TestUsernameEmailConversion(IList<(string address, string name)> recipients)
     {
@@ -413,12 +230,6 @@ public class PortalEmailService
 public class OnboardingParameters
 {
     public OnboardingApp App;
-    public string AppUrl;
-    public List<string> AdminEmailAddresses;
-}
-public class PowerBiExternalReportParameters
-{
-    public ExternalPowerBiReport App;
     public string AppUrl;
     public List<string> AdminEmailAddresses;
 }
