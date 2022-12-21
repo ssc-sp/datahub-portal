fb4f0450618a3f7aa70dd88d7e3cd7a54eec0e7d
Yan <yjmrobert@gmail.com>
2022-12-21 07:45:47 -0500

    :wrench: added project phase to flow

---
+++ Portal/src/Datahub.Core/Enums/ProjectPhase.cs
@@ -0,0 +1,9 @@
+namespace Datahub.Core.Enums
+{
+    public static class ProjectPhase
+    {
+        public static string PendingApproval => "Pending Approval";
+        public static string InProgress => "In Progress";
+        public static string Completed => "Completed";
+    }
+}
