bff3a3d4b855257454f13cfe20464bc99136d0ed
iroel <iroel.perez@gmail.com>
2023-01-03 14:15:00 -0500

    Adding the SyncDocs tool

---
+++ Portal/utils/SyncDocs/ConfigParams.cs
@@ -0,0 +1,11 @@
+﻿namespace SyncDocs;
+internal class ConfigParams
+{
+    public string Target { get; set; } = "";
+    public string Excluded { get; set; } = "";
+
+    public IEnumerable<string> GetExcludedFolders()
+    {
+        return (Excluded ?? "").Split(",", StringSplitOptions.RemoveEmptyEntries).Where(s => !string.IsNullOrEmpty(s));
+    }        
+}
