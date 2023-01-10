33f5f2affede3f5ba7127355e1ec96afc13c3628
Yan <yjmrobert@gmail.com>
2023-01-09 21:00:03 -0500

    :bug: added request for each dependency as well #206

--- Portal/src/Datahub.ProjectTools/Services/RequestManagementService.cs
+++ Portal/src/Datahub.ProjectTools/Services/RequestManagementService.cs
@@ -237,23 +237,27 @@ public class RequestManagementService : IRequestManagementService
             await ctx.Entry(project).Collection(p => p.Users).LoadAsync();
             var userId = await _userInformationService.GetUserIdString();
             var graphUser = await _userInformationService.GetCurrentGraphUserAsync();
-            var serviceRequest = new Datahub_ProjectServiceRequests()
-            {
-                ServiceType = GetTerraformServiceType(terraformTemplate),
-                ServiceRequests_Date_DT = DateTime.Now,
-                Is_Completed = null,
-                Project = project,
-                User_ID = userId,
-                User_Name = graphUser.UserPrincipalName
-            };
-
-            await RequestServiceWithDefaults(serviceRequest);
             var users = project.Users.Select(u => new TerraformUser() { Guid = u.User_ID, Email = u.User_Name })
                 .ToList();
 
             var workspace = project.ToResourceWorkspace(users);
             var templates = TerraformTemplate.LatestFromNameWithDependencies(terraformTemplate);
 
+            foreach (var template in templates)
+            {
+                var serviceRequest = new Datahub_ProjectServiceRequests()
+                {
+                    ServiceType = GetTerraformServiceType(template.Name),
+                    ServiceRequests_Date_DT = DateTime.Now,
+                    Is_Completed = null,
+                    Project = project,
+                    User_ID = userId,
+                    User_Name = graphUser.UserPrincipalName
+                };
+
+                await RequestServiceWithDefaults(serviceRequest);
+            }
+            
             var request = CreateResourceData.ResourceRunTemplate(workspace, templates, graphUser.Mail);
             await requestQueueService.AddProjectToStorageQueue(request);
             scope.Complete();
