4b42b15ca959e520034634d105ae8240629f9da9
Nick Salt <nsalt@apption.com>
2023-01-25 15:48:09 -0500

    💎 updated typography in DatahubTheme.cs and "mudified" homepage #301

--- Portal/src/Datahub.Core/Theme/DatahubTheme.cs
+++ Portal/src/Datahub.Core/Theme/DatahubTheme.cs
@@ -11,7 +11,45 @@ public abstract class DatahubTheme
             Default =
             {
                 FontFamily = new[] { "Open Sans", "sans-serif" },
-            }
+                FontSize = "0.9rem",        
+                LineHeight = 1.75,
+            }, 
+            H1 = new H1()
+            {
+                LineHeight = 1.25,
+                FontSize = "2.5rem",
+                FontWeight = 600,
+            },
+            H2 = new H2()
+            {
+                LineHeight = 1.35,
+                FontSize = "1.75rem",
+                FontWeight = 600,
+            },
+            H3 = new H3()
+            {
+                LineHeight = 1.3,
+                FontSize = "1.5rem",
+                FontWeight = 600,
+            },
+            H4 = new H4()
+            {
+                LineHeight = 1.2,
+                FontSize = "1.25rem",
+                FontWeight = 600,
+            },
+            H5 = new H5()
+            {
+                LineHeight = 1.2,
+                FontSize = "1.125rem",
+                FontWeight = 600,
+            },
+            H6 = new H6()
+            {
+                LineHeight = 1.2,
+                FontSize = "1rem",
+                FontWeight = 600,
+            },
         },
         Palette =
         {
