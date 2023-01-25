cebcd3a78b15bfcf40cf1b8a3a8bc46a00cfa1c9
Iroel Perez <iroel.perez@gmail.com>
2023-01-25 15:28:57 -0500

    Uploading and downloading the Datahub Metadata

---
+++ Portal/src/Datahub.Portal/Controllers/MetadataController.cs
@@ -0,0 +1,35 @@
+﻿using Datahub.Core.Services.Metadata;
+using Datahub.Metadata.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Datahub.Portal.Controllers
+{
+    [Route("[controller]")]
+    [AllowAnonymous]
+    public class MetadataController : Controller
+    {
+        private readonly ILogger<MetadataController> _logger;
+        private readonly IMetadataBrokerService _metadataBrokerService;
+
+        public MetadataController(ILogger<MetadataController> logger, IMetadataBrokerService metadataBrokerService)
+        {
+            _logger = logger;
+            _metadataBrokerService = metadataBrokerService;
+        }
+        
+        [HttpGet]
+        [Route("definitions/get")]
+        public async Task<IActionResult> GetMetadataDefinitions()
+        {
+            var profiles = await _metadataBrokerService.GetProfiles();
+            var fieldDefinitions = await _metadataBrokerService.GetFieldDefinitions();
+
+            var metadataDto = MetadataDTO.Create(profiles, fieldDefinitions.Fields);
+            JsonSerializerOptions options = new() { WriteIndented = true };
+
+            return new JsonResult(metadataDto, options);
+        }
+    }
+}
