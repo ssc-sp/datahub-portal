36eaeb40d0cd686307ec8b080f7abf781cb95ad3
Iroel Perez <iroel.perez@gmail.com>
2023-01-30 15:32:45 -0500

    Detecting deleted English pages in SyncDocs tool

--- Portal/utils/SyncDocs/FileMappingService.cs
+++ Portal/utils/SyncDocs/FileMappingService.cs
@@ -16,7 +16,26 @@ internal class FileMappingService
         try
         {
             var options = new JsonSerializerOptions() { WriteIndented = true };
+            var directory = Path.GetDirectoryName(path)!;
+
+            // delete unnecessary files
+            if (File.Exists(path))
+            {
+                var oldMappings = JsonSerializer.Deserialize<FilePair[]>(File.ReadAllText(path)) ?? Array.Empty<FilePair>();
+                foreach (var mapping in oldMappings)
+                {
+                    var enPath = NormalizePath($"{directory}{mapping.En}");
+                    if (!File.Exists(enPath))
+                    {
+                        var frPath = NormalizePath($"{directory}{mapping.Fr}");
+                        File.Delete(frPath);
+                    }
+                }
+            }
+
+            // save new mapping
             File.WriteAllText(path, JsonSerializer.Serialize(_pairs, options));
+
             return true;
         }
         catch (Exception)
@@ -24,6 +43,8 @@ internal class FileMappingService
             return false;
         }
     }
+
+    static string NormalizePath(string path) => path.Replace("\\", "/");
 }
 
 record FilePair(string Id, string En, string Fr);
