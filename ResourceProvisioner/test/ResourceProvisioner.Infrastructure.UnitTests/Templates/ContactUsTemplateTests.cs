a247f6c32878737f53bc28419bf462d24107fbb6
Yan <yjmrobert@gmail.com>
2023-02-08 06:45:19 -0500

    :wrench: updated contact us template #357

---
+++ ResourceProvisioner/test/ResourceProvisioner.Infrastructure.UnitTests/Templates/ContactUsTemplateTests.cs
@@ -0,0 +1,83 @@
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Datahub.Shared.Entities;
+using ResourceProvisioner.Infrastructure.Common;
+using ResourceProvisioner.Infrastructure.Services;
+
+namespace ResourceProvisioner.Infrastructure.UnitTests.Templates;
+
+using static Testing;
+
+public class ContactUsTemplateTests
+{
+    [SetUp]
+    public void RunBeforeEachTest()
+    {
+        var localModuleClonePath = DirectoryUtils.GetModuleRepositoryPath(_configuration);
+        var localInfrastructureClonePath = DirectoryUtils.GetInfrastructureRepositoryPath(_configuration);
+
+        VerifyDirectoryDoesNotExist(localModuleClonePath);
+        VerifyDirectoryDoesNotExist(localInfrastructureClonePath);
+    }
+
+    [Test]
+    public async Task ShouldNotCopyContactUsTemplate()
+    {
+        const string workspaceAcronym = "ShouldNotCopyContactUsTemplate";
+        var workspace = new TerraformWorkspace
+        {
+            Acronym = workspaceAcronym
+        };
+        await _repositoryService.FetchRepositoriesAndCheckoutProjectBranch(workspaceAcronym);
+
+        var module = new TerraformTemplate()
+        {
+            Name = TerraformTemplate.ContactUs,
+            Version = "latest"
+        };
+
+        await _terraformService.CopyTemplateAsync(module, workspace);
+
+        var moduleDestinationPath = DirectoryUtils.GetProjectPath(_configuration, workspaceAcronym);
+
+        // verify that the directory does not exist
+        Assert.That(Directory.Exists(moduleDestinationPath), Is.False);
+    }
+
+    [Test]
+    public async Task ShouldNotCopyContactUsTemplateInExistingProject()
+    {
+        const string workspaceAcronym = "ShouldNotCopyContactUsTemplate";
+        var workspace = new TerraformWorkspace
+        {
+            Acronym = workspaceAcronym
+        };
+        await _repositoryService.FetchRepositoriesAndCheckoutProjectBranch(workspaceAcronym);
+
+        var module = new TerraformTemplate()
+        {
+            Name = TerraformTemplate.NewProjectTemplate,
+            Version = "latest"
+        };
+
+        await _terraformService.CopyTemplateAsync(module, workspace);
+
+        var moduleDestinationPath = DirectoryUtils.GetProjectPath(_configuration, workspaceAcronym);
+
+        Assert.That(Directory.Exists(moduleDestinationPath), Is.True);
+        var fileCount = Directory.GetFiles(moduleDestinationPath, "*", SearchOption.AllDirectories).Length;
+        
+        module = new TerraformTemplate()
+        {
+            Name = TerraformTemplate.ContactUs,
+            Version = "latest"
+        };
+        
+        await _terraformService.CopyTemplateAsync(module, workspace);
+        
+        // assert that no new files were created
+        Assert.That(Directory.Exists(moduleDestinationPath), Is.True);
+        var newFileCount = Directory.GetFiles(moduleDestinationPath, "*", SearchOption.AllDirectories).Length;
+        Assert.That(newFileCount, Is.EqualTo(fileCount));
+    }
+}
