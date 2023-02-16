a244c1a2d2a9ecaabd780f519fb7422d26b4d3ef
Nick Salt <nsalt@apption.com>
2023-02-16 16:41:57 -0500

    🚧 fixed get whitelist method in ProjectResourcingWhitelistService.cs

--- Portal/src/Datahub.Infrastructure/Services/ProjectResourcingWhitelistService.cs
+++ Portal/src/Datahub.Infrastructure/Services/ProjectResourcingWhitelistService.cs
@@ -1,3 +1,4 @@
+using System.Transactions;
 using Datahub.Application.Services;
 using Datahub.Core.Model.Datahub;
 using Datahub.Core.Services;
@@ -20,12 +21,12 @@ namespace Datahub.Infrastructure.Services
 
         public async Task<IEnumerable<Datahub_Project_Resources_Whitelist>> GetAllProjectResourceWhitelistAsync()
         {
-            // await using var context = await _contextFactory.CreateDbContextAsync();
-                                         // return context.Projects.Select(async p => await GetProjectResourceWhitelistByProjectAsync(p.Project_ID))
-                                         //     .Select(t => t.Result)
-                                         //     .ToList();
-                                         
-            return new List<Datahub_Project_Resources_Whitelist>();
+            await using var context = await _contextFactory.CreateDbContextAsync();
+
+            var tasks = context.Projects.Select(GetProjectResourceWhitelistByProjectAsync);
+            var results = await Task.WhenAll(tasks);
+
+            return results.ToList();
         }
 
         public async Task<Datahub_Project_Resources_Whitelist> GetProjectResourceWhitelistByProjectAsync(int projectId)
@@ -37,8 +38,18 @@ namespace Datahub.Infrastructure.Services
             return whitelist ?? new Datahub_Project_Resources_Whitelist();
         }
 
+        private async Task<Datahub_Project_Resources_Whitelist> GetProjectResourceWhitelistByProjectAsync(
+            Datahub_Project project)
+        {
+            var whitelist = await GetProjectResourceWhitelistByProjectAsync(project.Project_ID);
+            whitelist.Project = project;
+            return whitelist;
+        }
+
         public async Task UpdateProjectResourceWhitelistAsync(Datahub_Project_Resources_Whitelist projectResourceWhitelist)
         {
+            using var scope = new TransactionScope(
+                TransactionScopeOption.Required, TransactionScopeAsyncFlowOption.Enabled);
             var currentUser = await _userInformationService.GetCurrentGraphUserAsync();
             projectResourceWhitelist.LastUpdated = DateTime.Now;
             projectResourceWhitelist.AdminLastUpdated_ID = currentUser.Id;
@@ -46,13 +57,14 @@ namespace Datahub.Infrastructure.Services
             await using var context = await _contextFactory.CreateDbContextAsync();
             if (projectResourceWhitelist.Id == 0)
             {
-                await context.Project_Resources_Whitelists.AddAsync(projectResourceWhitelist);
+                context.Project_Resources_Whitelists.Add(projectResourceWhitelist);
             }
             else
             {
                 context.Project_Resources_Whitelists.Update(projectResourceWhitelist);
             }
             await context.SaveChangesAsync();
+            scope.Complete();
         }
     }
 }
