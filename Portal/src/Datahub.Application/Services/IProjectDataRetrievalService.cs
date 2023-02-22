8041f16a0c8705ed9fc6152246a5553e1a8cf056
Yan <yjmrobert@gmail.com>
2023-02-22 09:36:06 -0500

    🔧 added dfs acl to get containers

--- Portal/src/Datahub.Application/Services/IProjectDataRetrievalService.cs
+++ Portal/src/Datahub.Application/Services/IProjectDataRetrievalService.cs
@@ -6,8 +6,12 @@ public interface IProjectDataRetrievalService
 {
     public Task<(List<string>, List<FileMetaData>, string)> GetStorageBlobPagesAsync(string projectAcronym,
         string containerName, Microsoft.Graph.User user, string prefix, string? continuationToken = default);
+    
+    public Task<(List<string>, List<FileMetaData>, string)> GetDfsPagesAsync(string projectAcronym,
+        string containerName, Microsoft.Graph.User user, string prefix, string? continuationToken = default);
 
     public Task<bool> StorageBlobExistsAsync(string filename, string projectAcronym, string containerName);
     public Task<Uri> DownloadFile(string container, FileMetaData file, string projectAcronym);
-    public Task<List<string>> GetProjectContainersAsync(string projectAcronym);
+    public Task<List<string>> GetProjectBlobContainersAsync(string projectAcronym);
+    public Task<List<string>> GetProjectDataLakeContainersAsync(string projectAcronym, Microsoft.Graph.User user);
 }
