5802b99e8e2260086700d20b3a5d2ef5d92ca454
Yan <yjmrobert@gmail.com>
2023-02-22 11:18:42 -0500

    🚧 wip on dfs

--- Portal/src/Datahub.Application/Services/IProjectDataRetrievalService.cs
+++ Portal/src/Datahub.Application/Services/IProjectDataRetrievalService.cs
@@ -5,10 +5,10 @@ namespace Datahub.Application.Services;
 public interface IProjectDataRetrievalService
 {
     public Task<(List<string>, List<FileMetaData>, string)> GetStorageBlobPagesAsync(string projectAcronym,
-        string containerName, Microsoft.Graph.User user, string prefix, string? continuationToken = default);
+        string containerName, Microsoft.Graph.User user, string currentWorkingDirectory, string? continuationToken = default);
     
     public Task<(List<string>, List<FileMetaData>, string)> GetDfsPagesAsync(string projectAcronym,
-        string containerName, Microsoft.Graph.User user, string prefix, string? continuationToken = default);
+        string containerName, Microsoft.Graph.User user, string currentWorkingDirectory, string? continuationToken = default);
 
     public Task<bool> StorageBlobExistsAsync(string filename, string projectAcronym, string containerName);
     public Task<Uri> DownloadFile(string container, FileMetaData file, string projectAcronym);
