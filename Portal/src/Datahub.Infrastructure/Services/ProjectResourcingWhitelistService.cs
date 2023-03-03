72b4e312c298687b7b21d2b02168efca509cbb59
Nick Salt <nsalt@apption.com>
2023-03-03 13:09:32 -0500

    ✨ added project whitelists

---
+++ Portal/src/Datahub.Infrastructure/Services/ProjectResourcingWhitelistService.cs
@@ -0,0 +1,67 @@
+using Datahub.Application.Services;
+using Datahub.Core.Model.Datahub;
+using Datahub.Core.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datahub.Infrastructure.Services
+{
+    public class ProjectResourcingWhitelistService : IProjectResourceWhitelistService
+    {
+
+        private readonly IDbContextFactory<DatahubProjectDBContext> _contextFactory;
+        private readonly IUserInformationService _userInformationService;
+
+        public ProjectResourcingWhitelistService(IDbContextFactory<DatahubProjectDBContext> contextFactory, 
+            IUserInformationService userInformationService)
+        {
+            _contextFactory = contextFactory;
+            _userInformationService = userInformationService;
+        }
+
+        public async Task<IEnumerable<Project_Whitelist>> GetAllProjectResourceWhitelistAsync()
+        {
+            await using var context = await _contextFactory.CreateDbContextAsync();
+
+            var tasks = context.Projects.Select(GetProjectResourceWhitelistByProjectAsync);
+            var results = await Task.WhenAll(tasks);
+
+            return results.ToList();
+        }
+
+        public async Task<Project_Whitelist> GetProjectResourceWhitelistByProjectAsync(int projectId)
+        {
+            await using var context = await _contextFactory.CreateDbContextAsync();
+            var whitelist = await context.Project_Whitelists
+                .Include(wl => wl.Project)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Project.Project_ID == projectId);
+            return whitelist ?? new Project_Whitelist(){ProjectId = projectId};
+        }
+
+        private async Task<Project_Whitelist> GetProjectResourceWhitelistByProjectAsync(
+            Datahub_Project project)
+        {
+            var whitelist = await GetProjectResourceWhitelistByProjectAsync(project.Project_ID);
+            whitelist.Project = project;
+            return whitelist;
+        }
+
+        public async Task UpdateProjectResourceWhitelistAsync(Project_Whitelist projectResourceWhitelist)
+        {
+            var currentUser = await _userInformationService.GetCurrentGraphUserAsync();
+            projectResourceWhitelist.LastUpdated = DateTime.Now;
+            projectResourceWhitelist.AdminLastUpdated_ID = currentUser.Id;
+            projectResourceWhitelist.AdminLastUpdated_UserName = currentUser.Mail;
+            await using var context = await _contextFactory.CreateDbContextAsync();
+            if (projectResourceWhitelist.Id == 0)
+            {
+                context.Project_Whitelists.Attach(projectResourceWhitelist);
+            }
+            else
+            {
+                context.Project_Whitelists.Update(projectResourceWhitelist);
+            }
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Portal/test/Datahub.Infrastructure.UnitTests/Services/ProjectResourcingWhitelistServiceTests.cs
@@ -0,0 +1,154 @@
+using Datahub.Core.Model.Datahub;
+using Datahub.Core.Services;
+using Datahub.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Graph;
+using Moq;
+namespace Datahub.Infrastructure.UnitTests.Services;
+using static Testing;
+public class ProjectResourcingWhitelistServiceTests
+{
+    private Mock<IDbContextFactory<DatahubProjectDBContext>> _mockFactory = null!;
+
+    private Mock<IUserInformationService> _mockUserInformationService = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+
+        var optionsBuilder =
+            new DbContextOptionsBuilder<DatahubProjectDBContext>()
+                .UseInMemoryDatabase((new Guid()).ToString());
+        // create a mock factory to return the db context when CreateDbContextAsync is called
+        var context = new DatahubProjectDBContext(optionsBuilder.Options);
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+        _mockFactory = new Mock<IDbContextFactory<DatahubProjectDBContext>>();
+        _mockFactory
+            .Setup(f => f.CreateDbContextAsync(CancellationToken.None))
+            .ReturnsAsync(() => new DatahubProjectDBContext(optionsBuilder.Options));
+        // create a mock user information service to return the current (admin) user when GetCurrentGraphUserAsync is called
+        _mockUserInformationService = new Mock<IUserInformationService>();
+        _mockUserInformationService
+            .Setup(f => f.GetCurrentGraphUserAsync())
+            .ReturnsAsync(new User(){Id = TestUserId, Mail = TestUserEmail});
+    }
+    /*
+     * Test Cases:
+     * - Get all whitelists, ensure there are only two
+     * - Get whitelist for project 1 and project 2, ensure settings are set 
+     */
+    [Test]
+    public async Task ShouldBeTwoWhitelists()
+    {
+        var whitelistService = GetProjectResourcingWhitelistService();
+        await SeedDatabase();
+        var projectWhitelists = await whitelistService.GetAllProjectResourceWhitelistAsync();
+        var datahubProjectResourcesWhitelists = projectWhitelists.ToList();
+        Assert.That(datahubProjectResourcesWhitelists.Where(wl => wl.Id > 0).ToList(), Has.Count.EqualTo(2));
+    }
+
+    [Test]
+    [TestCase("TEST1", true, false, false)]
+    [TestCase("TEST2", true, true, true)]
+    public async Task ShouldReturnProperWhitelist(string projectAcronym, bool allowStorage, bool allowVMs, bool allowDatabricks)
+    {
+        var whitelistService = GetProjectResourcingWhitelistService();
+        await SeedDatabase();
+        await using var context = await _mockFactory.Object.CreateDbContextAsync();
+        var project = await context.Projects.FirstOrDefaultAsync(p => p.Project_Acronym_CD == projectAcronym);
+        Assert.That(project, Is.Not.Null);
+        var whitelist = await whitelistService.GetProjectResourceWhitelistByProjectAsync(project!.Project_ID);
+        Assert.Multiple(() =>
+        {
+            Assert.That(project.Project_Acronym_CD, Is.EqualTo(whitelist.Project.Project_Acronym_CD));
+            Assert.That(allowStorage, Is.EqualTo(whitelist.AllowStorage));
+            Assert.That(allowVMs, Is.EqualTo(whitelist.AllowVMs));
+            Assert.That(allowDatabricks, Is.EqualTo(whitelist.AllowDatabricks));
+        });
+
+    }
+
+    [TestCase("TEST1")]
+    [TestCase("TEST2")]
+    [TestCase("TEST3")]
+    public async Task ShouldUpdateWhitelist(string projectAcronym)
+    {
+        var whitelistService = GetProjectResourcingWhitelistService();
+        await SeedDatabase();
+        await using var context = await _mockFactory.Object.CreateDbContextAsync();
+        var project = await context.Projects.FirstOrDefaultAsync(p => p.Project_Acronym_CD == projectAcronym);
+        Assert.That(project, Is.Not.Null);
+        var whitelist = await whitelistService.GetProjectResourceWhitelistByProjectAsync(project!.Project_ID);
+        // if whitelist id is greater than 0, assert that admin user is old user
+        if (whitelist.Id > 0)
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(whitelist.AdminLastUpdated_ID, Is.EqualTo(OldUserId));
+                Assert.That(whitelist.AdminLastUpdated_UserName, Is.EqualTo(OldUserEmail));
+            });
+        }
+        // allow all resources on whitelist
+        whitelist.AllowStorage = true;
+        whitelist.AllowDatabricks = true;
+        whitelist.AllowVMs = true;
+        await whitelistService.UpdateProjectResourceWhitelistAsync(whitelist);
+        //var whitelist = await whitelistService.GetProjectResourceWhitelistByProjectAsync(project!.Project_ID);
+        Assert.Multiple(() =>
+        {
+            Assert.That(whitelist.AdminLastUpdated_ID, Is.EqualTo(TestUserId));
+            Assert.That(whitelist.AdminLastUpdated_UserName, Is.EqualTo(TestUserEmail));
+        });
+
+    }
+
+    private async Task SeedDatabase()
+    {
+        await using var context = await _mockFactory.Object.CreateDbContextAsync();
+        var projects = PROJECT_ACRONYMS.Select((acronym, index) => new Datahub_Project()
+        {
+            Project_Acronym_CD = acronym,
+            Project_Name = PROJECT_NAMES[index],
+            Project_Status_Desc = "Active",
+            Sector_Name = "Test Sector",
+        }).ToArray();
+        await context.Projects.AddRangeAsync(projects);
+        await context.SaveChangesAsync();
+        var whitelists = new List<Project_Whitelist>
+        {
+            new()
+            {
+                ProjectId = projects[0].Project_ID,
+                AllowStorage = true,
+                AdminLastUpdated_ID = OldUserId,
+                AdminLastUpdated_UserName = OldUserEmail,
+                LastUpdated = DateTime.Now.Subtract(TimeSpan.FromDays(3)),
+            },
+            new()
+            {
+                ProjectId = projects[1].Project_ID,
+                AllowStorage = true,
+                AllowVMs = true,
+                AllowDatabricks = true,
+                AdminLastUpdated_ID = OldUserId,
+                AdminLastUpdated_UserName = OldUserEmail,
+                // set last updated to a few days before no
+                LastUpdated = DateTime.Now.Subtract(TimeSpan.FromDays(3)),
+            }
+        };
+        // purposely not creating a whitelist for the third project
+
+        await context.Project_Whitelists.AddRangeAsync(whitelists);
+        await context.SaveChangesAsync();
+    }
+
+    private ProjectResourcingWhitelistService GetProjectResourcingWhitelistService()
+    {
+
+        var projectUserManagementService = new ProjectResourcingWhitelistService(
+            _mockFactory.Object,
+            _mockUserInformationService.Object);
+        return projectUserManagementService;
+    }
+}
