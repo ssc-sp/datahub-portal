dc5298081ba4fa411eb8d4646cc04bfcdd8fbf0b
Yan <yjmrobert@gmail.com>
2023-03-17 10:40:18 -0400

    ✨ added three user lists for databricks module, refactored strings

--- ResourceProvisioner/test/ResourceProvisioner.Infrastructure.UnitTests/Testing.cs
+++ ResourceProvisioner/test/ResourceProvisioner.Infrastructure.UnitTests/Testing.cs
@@ -1,4 +1,5 @@
 using Datahub.Shared.Entities;
+using Datahub.Shared.Enums;
 using ResourceProvisioner.Application.Services;
 using ResourceProvisioner.Infrastructure.Services;
 using Microsoft.Extensions.Configuration;
@@ -6,6 +7,7 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using ResourceProvisioner.Application.Config;
 using ResourceProvisioner.Infrastructure.Common;
+
 // ReSharper disable InconsistentNaming
 
 namespace ResourceProvisioner.Infrastructure.UnitTests;
@@ -16,65 +18,69 @@ public class Testing
     internal static IConfiguration _configuration = null!;
     internal static IRepositoryService _repositoryService = null!;
     internal static ITerraformService _terraformService = null!;
-    
+
     internal static ResourceProvisionerConfiguration _resourceProvisionerConfiguration = null!;
-    
-    
+
+
     internal const string ProjectAcronym = "TEST";
+
     internal static TerraformWorkspace TestingWorkspace => new()
     {
         Acronym = ProjectAcronym,
     };
+
     internal const string RequestingUser = "Unit Test User";
     internal const string RequestingAdminUser = "Unit Test Admin User";
+
     internal static readonly TerraformTemplate TestTemplate = new()
     {
         Name = "TestModule",
         Version = "1.0.0",
     };
-    
+
     [OneTimeSetUp]
     public void RunBeforeAnyTests()
     {
         _configuration = new ConfigurationBuilder()
             .AddJsonFile("appsettings.Test.json")
             .Build();
-        
+
         _resourceProvisionerConfiguration = new ResourceProvisionerConfiguration();
         _configuration.Bind(_resourceProvisionerConfiguration);
-        
-        _terraformService = new TerraformService(Mock.Of<ILogger<TerraformService>>(), _resourceProvisionerConfiguration, _configuration);
-        
+
+        _terraformService = new TerraformService(Mock.Of<ILogger<TerraformService>>(),
+            _resourceProvisionerConfiguration, _configuration);
+
         var httpClientFactory = new Mock<IHttpClientFactory>();
         httpClientFactory.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(Mock.Of<HttpClient>());
-        _repositoryService = new RepositoryService(httpClientFactory.Object, Mock.Of<ILogger<RepositoryService>>(), _resourceProvisionerConfiguration, _terraformService);
+        _repositoryService = new RepositoryService(httpClientFactory.Object, Mock.Of<ILogger<RepositoryService>>(),
+            _resourceProvisionerConfiguration, _terraformService);
     }
 
     [OneTimeTearDown]
     public void RunAfterAnyTests()
     {
     }
-    
+
     internal static void VerifyDirectoryDoesNotExist(string path)
     {
         if (!Directory.Exists(path))
         {
             return;
         }
-        
+
         var dir = new DirectoryInfo(path);
         SetAttributesNormal(dir);
         try
         {
             dir.Delete(true);
-            
         }
         catch (Exception)
         {
             // ignored
         }
     }
-    
+
     internal static void SetAttributesNormal(DirectoryInfo dir)
     {
         foreach (var subDir in dir.GetDirectories())
@@ -84,14 +90,22 @@ public class Testing
             file.Attributes = FileAttributes.Normal;
         }
     }
-    
+
     internal static async Task<int> SetupNewProjectTemplate(string workspaceAcronym)
     {
         var workspace = new TerraformWorkspace
         {
             Acronym = workspaceAcronym
         };
-        await _repositoryService.FetchRepositoriesAndCheckoutProjectBranch(workspaceAcronym);
+        try
+        {
+            await _repositoryService.FetchRepositoriesAndCheckoutProjectBranch(workspaceAcronym);
+        }
+        catch (IOException ex)
+        {
+            await Task.Delay(1000);
+            await _repositoryService.FetchRepositoriesAndCheckoutProjectBranch(workspaceAcronym);
+        }
 
         var module = new TerraformTemplate
         {
@@ -107,12 +121,12 @@ public class Testing
         return Directory
             .GetFiles(moduleDestinationPath, "*.*", SearchOption.TopDirectoryOnly).Length;
     }
-    
+
     internal static string GenerateWorkspaceAcronym()
     {
         return $"{Guid.NewGuid().ToString().Replace("-", "")[..8]}";
     }
-    
+
     internal static TerraformWorkspace GenerateTestTerraformWorkspace(string workspaceAcronym, bool withUsers = true)
     {
         if (!withUsers)
@@ -123,30 +137,43 @@ public class Testing
             };
         }
 
+        var users = new List<TerraformUser>();
+        const int numberOfOwners = 2;
+        const int numberOfAdmins = 3;
+        const int numberOfUsers = 10;
+
+        users.AddRange(Enumerable.Range(0, numberOfOwners)
+            .Select(i => new TerraformUser
+            {
+                Email = $"owner{i}@email.com",
+                ObjectId = Guid.NewGuid().ToString(),
+                Role = Role.Owner
+            }));
+        
+        users.AddRange(Enumerable.Range(0, numberOfAdmins)
+            .Select(i => new TerraformUser
+            {
+                Email = $"admin{i}@email.com",
+                ObjectId = Guid.NewGuid().ToString(),
+                Role = Role.Admin
+            }));
+        
+        users.AddRange(Enumerable.Range(0, numberOfUsers)
+            .Select(i => new TerraformUser
+            {
+                Email = $"user{i}@email.com",
+                ObjectId = Guid.NewGuid().ToString(),
+                Role = Role.User
+            }));
+
+
         return new TerraformWorkspace
         {
             Acronym = workspaceAcronym,
-            Users = new List<TerraformUser>
-            {
-                new()
-                {
-                    Email = "1@email.com",
-                    ObjectId = "00000000-0000-0000-0000-000000000001"
-                },
-                new()
-                {
-                    Email = "2@email.com",
-                    ObjectId = "00000000-0000-0000-0000-000000000002"
-                },
-                new()
-                {
-                    Email = "3@email.com",
-                    ObjectId = "00000000-0000-0000-0000-000000000003"
-                }
-            }
+            Users = users
         };
     }
-    
+
     internal static TerraformTemplate GenerateTerraformTemplate(string template)
     {
         return new TerraformTemplate
