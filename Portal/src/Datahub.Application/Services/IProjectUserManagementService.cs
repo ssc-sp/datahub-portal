056f336103a61c1cd58297ec9f570d83ac730d26
Nick Salt <nsalt@apption.com>
2023-03-24 10:20:53 -0400

    ✨ added batch method to ProjectUserManagementService.cs

--- Portal/src/Datahub.Application/Services/IProjectUserManagementService.cs
+++ Portal/src/Datahub.Application/Services/IProjectUserManagementService.cs
@@ -5,6 +5,8 @@ namespace Datahub.Application.Services;
 
 public interface IProjectUserManagementService
 {
+    public Task BatchUpdateUsersInProject(string projectAcronym,
+        IEnumerable<(string userGraphId, ProjectMemberRole role)> projectMembers);
     Task AddUserToProject(string projectAcronym, string userGraphIds);
     Task AddUsersToProject(string projectAcronym, IEnumerable<string> userGraphId);
     Task RemoveUserFromProject(string projectAcronym, string userGraphId);
