07d767d83d7dfe63da139d69d7a151dfe167b2db
Iroel Perez <iroel.perez@gmail.com>
2023-04-25 16:53:39 -0400

    Updating achivement rules code

--- Portal/src/Datahub.Core/Model/Achievements/Configuration/AchivementConfiguration.cs
+++ Portal/src/Datahub.Core/Model/Achievements/Configuration/AchivementConfiguration.cs
@@ -38,31 +38,31 @@ public class AchivementConfiguration : IEntityTypeConfiguration<Achievement>
             (
                 "DHA-001", "Collaboration Connoisseur", "Logged in to DataHub", 1, 
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserLogin}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserLogin}", currentMetric)"""
             ),
             new Achievement
             (
                 "DHA-002", "Collaboration Commander", "Invite a user to your workspace", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserSentInvite}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserSentInvite}", currentMetric)"""
             ),
             new Achievement
             (
                 "DHA-003",  "Workspace Warrior", "Join a workspace", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserJoinedProject}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserJoinedProject}", currentMetric)"""
             ),
             new Achievement
             (
                 "DHA-004", "Workspace Wanderlust", "Leave a workspace", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserLeftProject}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserLeftProject}", currentMetric)"""
             ),
             new Achievement
             (
                 "DHA-005", "Consistent Contributor", "Login on multiple days", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserLoginMultipleDays}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserLoginMultipleDays}", currentMetric)"""
             ),
             #endregion
             
@@ -71,48 +71,48 @@ public class AchivementConfiguration : IEntityTypeConfiguration<Achievement>
             (
                 "STR-000", "Storage Savant", "Unlock all the 2.0 Storage Explorer achievements", 1,
                 // rules
-                """Utils.OwnsAchievement("STR-001", state)""",
-                """Utils.OwnsAchievement("STR-002", state)""",
-                """Utils.OwnsAchievement("STR-003", state)""",
-                """Utils.OwnsAchievement("STR-004", state)""",
-                """Utils.OwnsAchievement("STR-005", state)""",
-                """Utils.OwnsAchievement("STR-006", state)"""
+                """Utils.OwnsAchievement("STR-001", achivements)""",
+                """Utils.OwnsAchievement("STR-002", achivements)""",
+                """Utils.OwnsAchievement("STR-003", achivements)""",
+                """Utils.OwnsAchievement("STR-004", achivements)""",
+                """Utils.OwnsAchievement("STR-005", achivements)""",
+                """Utils.OwnsAchievement("STR-006", achivements)"""
             ),
             new Achievement
             (
                 "STR-001", "Unstoppable Uploader", "Upload a file using the workspace Storage Explorer", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserUploadFile}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserUploadFile}", currentMetric)"""
             ),
             new Achievement
             (
                 "STR-002", "Storage Socialite", "Share a file using the workspace Storage Explorer", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserShareFile}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserShareFile}", currentMetric)"""
             ),
             new Achievement
             (
                 "STR-003", "File Fetcher", "Download a file using the workspace Storage Explorer", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserShareFile}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserDownloadFile}", currentMetric)"""
             ),
             new Achievement
             (
                 "STR-004", "Daredevil Deleter", "Delete a file from the workspace with the Storage Explorer", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserDeleteFile}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserDeleteFile}", currentMetric)"""
             ),
             new Achievement
             (
                 "STR-005", "Folder Fashionista", "Create a folder in the workspace's Storage Explorer", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserCreateFolder}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserCreateFolder}", currentMetric)"""
             ),
             new Achievement
             (
                 "STR-006", "Folder Farewell", "Delete a folder in the workspace's Storage Explorer", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserDeletedFolder}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserDeletedFolder}", currentMetric)"""
             ),
             #endregion
 
@@ -121,69 +121,69 @@ public class AchivementConfiguration : IEntityTypeConfiguration<Achievement>
             (
                 "EXP-000", "Explorer Extraordinaire", "Unlock all the 2.0 Exploration achievements", 1,
                 // rules
-                """Utils.OwnsAchievement("EXP-001", state)""",
-                """Utils.OwnsAchievement("EXP-002", state)""",
-                """Utils.OwnsAchievement("EXP-003", state)""",
-                """Utils.OwnsAchievement("EXP-004", state)""",
-                """Utils.OwnsAchievement("EXP-005", state)""",
-                """Utils.OwnsAchievement("EXP-006", state)""",
-                """Utils.OwnsAchievement("EXP-007", state)""",
-                """Utils.OwnsAchievement("EXP-008", state)""",
-                """Utils.OwnsAchievement("EXP-009", state)"""
+                """Utils.OwnsAchievement("EXP-001", achivements)""",
+                """Utils.OwnsAchievement("EXP-002", achivements)""",
+                """Utils.OwnsAchievement("EXP-003", achivements)""",
+                """Utils.OwnsAchievement("EXP-004", achivements)""",
+                """Utils.OwnsAchievement("EXP-005", achivements)""",
+                """Utils.OwnsAchievement("EXP-006", achivements)""",
+                """Utils.OwnsAchievement("EXP-007", achivements)""",
+                """Utils.OwnsAchievement("EXP-008", achivements)""",
+                """Utils.OwnsAchievement("EXP-009", achivements)"""
             ),
             new Achievement
             (
                 "EXP-001", "Storage Safari", "Navigate to the Storage Explorer page of a workspace", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserViewFileExplorer}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserViewFileExplorer}", currentMetric)"""
             ),
             new Achievement
             (
                 "EXP-002", "Databricks Discovery", "Navigate to the Databricks page of a workspace", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserOpenDatabricks}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserOpenDatabricks}", currentMetric)"""
             ),
             new Achievement
             (
                 "EXP-003", "Resource Ranger", "View the resources section of DataHub", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserVisitResources}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserVisitResources}", currentMetric)"""
             ),
             new Achievement
             (
                 "EXP-004", "Workspace Wanderer", "View a workspace you are not a member of", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserViewProjectNotMemberOf}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserViewProjectNotMemberOf}", currentMetric)"""
             ),
             new Achievement
             (
                 "EXP-005", "Workspace Wayfarer", "Visit one of your own workspaces", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserViewProject}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserViewProject}", currentMetric)"""
             ),
             new Achievement
             (
                 "EXP-006", "Link Legend", "Use a recent link to get to the same page again", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserRecentLink}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserRecentLink}", currentMetric)"""
             ),
             new Achievement
             (
                 "EXP-007", "Prolific Polyglot", "Switch languages in the portal", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserToggleCulture}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserToggleCulture}", currentMetric)"""
             ),
             new Achievement
             (
                 "EXP-008", "Profile Peruser", "View your own profile page", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserViewProfile}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserViewProfile}", currentMetric)"""
             ),
             new Achievement
             (
                 "EXP-009", "Profile Prowler", "View another person's profile", 1,
                 // rules
-                $"""Utils.MatchMetric("{TelemetryEvents.UserViewOtherProfile}", state)"""
+                $"""Utils.MatchMetric("{TelemetryEvents.UserViewOtherProfile}", currentMetric)"""
             )
             #endregion
         };
