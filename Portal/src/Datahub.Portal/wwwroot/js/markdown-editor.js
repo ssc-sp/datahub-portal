ba6120915305c4769c10e948c48faee5e57f7e2e
Iroel Perez <iroel.perez@gmail.com>
2023-05-09 09:41:37 -0400

    Handing over changes

---
+++ Portal/src/Datahub.Portal/wwwroot/js/markdown-editor.js
@@ -0,0 +1,29 @@
+﻿const _instances = [];
+
+function initializeMarkdown(elementId, dotNetObjectRef) {
+
+    const easyMDE = new EasyMDE({
+        element: document.getElementById(elementId),
+        hideIcons: ["side-by-side", "fullscreen"]
+    });
+
+    easyMDE.codemirror.on("change", function () {
+        dotNetObjectRef.invokeMethodAsync("UpdateInternalValue", easyMDE.value());
+    });
+
+    _instances[elementId] = {
+        dotNetObjectRef: dotNetObjectRef,
+        elementId: elementId,
+        editor: easyMDE,
+    };
+
+    return {
+        initialize: true,
+        dispose: () => destroy_editor(elementId)
+    };
+};
+
+function destroyMarkdown(elementId) {
+    const instances = _instances || {};
+    delete instances[elementId];
+}
