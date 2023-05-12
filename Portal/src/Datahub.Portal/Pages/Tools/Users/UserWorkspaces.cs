4b43e264b9dfecc11643038128fe503f1b77a4b1
Nick Salt <nsalt@apption.com>
2023-05-12 12:48:30 -0400

    ✨ added user table to admin view #722

---
+++ Portal/src/Datahub.Portal/Pages/Tools/Users/UserWorkspaces.cs
@@ -0,0 +1,11 @@
+using Datahub.Core.Model.Achievements;
+using Datahub.Core.Model.Datahub;
+
+namespace Datahub.Portal.Pages.Tools.Users
+{
+    public record UserWorkspaces
+    {
+        public PortalUser User { get; init; } = null!;
+        public List<Datahub_Project                                                                                                                                                          > Workspaces { get; init; } = null!;
+    }
+}
