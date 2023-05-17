3631373a00ce6f1cee441c2cde0e3dfd0d7845f5
Erik <erik@ens-music.com>
2023-05-17 18:04:26 -0400

    working demo for maui uploader

---
+++ datahub-uploader/UploadPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using Azure.Storage.Blobs;
+using Datahub.Desktop.Uploader.Platforms;
+using Microsoft.Maui.Platform;
+
+namespace Datahub.Desktop.Uploader
+{
+    public partial class UploadPage : ContentPage
+    {
+
+        public UploadPage()
+        {
+            InitializeComponent();
+        }
+
+        private Dictionary<string,string> fileList = new();
+        private bool uploadInProgress;
+
+        private void ContentPage_Loaded(object? sender, EventArgs e)
+        {
+            if (Handler?.MauiContext != null)
+            {
+                var uiElement = this.ToPlatform(Handler.MauiContext);
+                DragDropHelper.RegisterDragDrop(uiElement, async fname =>
+                {
+                    if (!fileList.ContainsKey(fname))
+                    {
+                        fileList.Add(fname, "Ready...");
+                    }
+                    await UpdateFileLayout();
+                    //await mainPageViewModel.OpenFile(stream, CancellationToken.None);
+                });
+            }
+        }
+
+        private async Task UpdateFileLayout()
+        {
+            if (fileList.Count > 0)
+            {
+                
+                FileListSection.Clear();
+                foreach (var item in fileList)
+                {
+                    FileListSection.Add(new TextCell() { Text = item.Key, Detail = item.Value });
+                }
+                LbUpload1.IsVisible = false;
+                FileListTbView.IsVisible = true;
+                UploadBtn.IsEnabled = true && !uploadInProgress;
+            } else
+            {
+                LbUpload1.IsVisible = true;
+                FileListTbView.IsVisible = false;
+                UploadBtn.IsEnabled = false;
+            }
+        }
+
+        private async void AddfileBtn_Clicked(object sender, EventArgs e)
+        {
+            PickOptions options = new()
+            {
+                PickerTitle = "Please select a file to upload",
+                
+            };
+            var result = await FilePicker.Default.PickMultipleAsync(options);
+            if (result != null)
+            {
+                foreach (var file in result)
+                {
+                    fileList.Add(file.FullPath,"Ready...");
+                }
+                await UpdateFileLayout();
+            }
+
+        }
+
+        private async void OpenWSBtn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Uri uri = new Uri("https://federal-science-datahub.canada.ca/w/DW1/filelist");
+                await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+            }
+            catch (Exception ex)
+            {
+                // An unexpected error occurred. No browser may be installed on the device.
+            }
+
+        }
+
+        public BlobContainerClient GetBlobServiceClient()
+        {
+            BlobContainerClient client = new(
+                new Uri($"https://fsdhprojdw1poc.blob.core.windows.net/datahub?sp=racwd&st=2023-05-17T21:49:28Z&se=2023-06-18T05:49:28Z&spr=https&sv=2022-11-02&sr=c&sig=IE6faaF8a%2F6VJmxxaHOur4EqqlVaI8CYDX8Mteon5aM%3D"),
+                null);
+
+            return client;
+        }
+
+        private async void UploadBtn_Clicked(object sender, EventArgs e)
+        {
+            var client = GetBlobServiceClient();
+            uploadInProgress = true;
+            UploadBtn.IsEnabled = false;
+            foreach (var item in fileList)
+            {
+                fileList[item.Key] = "In Progress";
+                await UpdateFileLayout();
+                await Uploader.UploadBlocksAsync(client, item.Key, 4096);
+                fileList[item.Key] = "Completed";
+                await UpdateFileLayout();
+            }
+            UploadBtn.IsEnabled = true;
+            uploadInProgress = true;
+
+        }
+    }
+}
---
+++ datahub-uploader/Uploader.cs
@@ -0,0 +1,58 @@
+﻿using Azure.Storage.Blobs.Specialized;
+using Azure.Storage.Blobs;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.Desktop.Uploader
+{
+    internal class Uploader
+    {
+        public static async Task UploadBlocksAsync(
+            BlobContainerClient blobContainerClient,
+            string localFilePath,
+            int blockSize)
+        {
+            string fileName = Path.GetFileName(localFilePath);
+            BlockBlobClient blobClient = blobContainerClient.GetBlockBlobClient(fileName);
+
+            FileStream fileStream = File.OpenRead(localFilePath);
+            ArrayList blockIDArrayList = new ArrayList();
+            byte[] buffer;
+
+            var bytesLeft = (fileStream.Length - fileStream.Position);
+
+            while (bytesLeft > 0)
+            {
+                if (bytesLeft >= blockSize)
+                {
+                    buffer = new byte[blockSize];
+                    await fileStream.ReadAsync(buffer, 0, blockSize);
+                }
+                else
+                {
+                    buffer = new byte[bytesLeft];
+                    await fileStream.ReadAsync(buffer, 0, Convert.ToInt32(bytesLeft));
+                    bytesLeft = (fileStream.Length - fileStream.Position);
+                }
+
+                using (var stream = new MemoryStream(buffer))
+                {
+                    string blockID = Convert.ToBase64String(
+                        Encoding.UTF8.GetBytes(Guid.NewGuid().ToString()));
+
+                    blockIDArrayList.Add(blockID);
+                    await blobClient.StageBlockAsync(blockID, stream);
+                }
+                bytesLeft = (fileStream.Length - fileStream.Position);
+            }
+
+            string[] blockIDArray = (string[])blockIDArrayList.ToArray(typeof(string));
+
+            await blobClient.CommitBlockListAsync(blockIDArray);
+        }
+    }
+}
