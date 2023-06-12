d149a7d040c269c67abe98a48375cb8f0d85a612
Yan <yjmrobert@gmail.com>
2023-06-12 11:06:06 -0400

    🔧 updated the role claims

--- Portal/src/Datahub.Core/Data/RoleConstants.cs
+++ Portal/src/Datahub.Core/Data/RoleConstants.cs
@@ -1,9 +1,27 @@
-﻿namespace Datahub.Core.Data;
+﻿using Datahub.Core.Model.Projects;
+
+namespace Datahub.Core.Data;
 
 public static class RoleConstants
 {
     public const string ADMIN_SUFFIX = "-admin";
+    public const string WORKSPACE_LEAD_SUFFIX = "-workspace-lead";
+    public const string COLLABORATOR_SUFFIX = "-collaborator";
+    public const string GUEST_SUFFIX = "-guest";
+    
     public const string DATAHUB_ADMIN_PROJECT = "DHPGLIST";
     public const string DATAHUB_ROLE_ADMIN = DATAHUB_ADMIN_PROJECT + ADMIN_SUFFIX;
     public const string DATAHUB_ROLE_ADMIN_AS_GUEST = DATAHUB_ADMIN_PROJECT + "-admin-as-guest";
+
+    public static string GetRoleConstants(Project_Role role)
+    {
+        return role.Id switch
+        {
+            2 => WORKSPACE_LEAD_SUFFIX,
+            3 => ADMIN_SUFFIX,
+            4 => COLLABORATOR_SUFFIX,
+            5 => GUEST_SUFFIX,
+            _ => "role not found"
+        };
+    }
 }
--- Portal/src/Datahub.Core/RoleManagement/RoleClaimTransformer.cs
+++ Portal/src/Datahub.Core/RoleManagement/RoleClaimTransformer.cs
@@ -32,38 +32,28 @@ public class RoleClaimTransformer : IClaimsTransformation
             }
             else
             {
-                var allProjects = serviceAuthManager.GetAllProjects();
-
                 var authorizedProjects = await serviceAuthManager.GetUserAuthorizations(userId);
-                ((ClaimsIdentity)principal.Identity).AddClaim(new Claim(ClaimTypes.Role, $"default"));
-                ((ClaimsIdentity)principal.Identity).AddClaim(new Claim(ClaimTypes.Role, userId));
-                foreach (var project in allProjects)
+                ((ClaimsIdentity)principal.Identity)?.AddClaim(new Claim(ClaimTypes.Role, "default"));
+                ((ClaimsIdentity)principal.Identity)?.AddClaim(new Claim(ClaimTypes.Role, userId));
+
+                foreach (var (role, project) in authorizedProjects)
                 {
-                    if (await serviceAuthManager.IsProjectAdmin(userId, project))
+                    if (project.Project_Acronym_CD == RoleConstants.DATAHUB_ADMIN_PROJECT && serviceAuthManager.GetViewingAsGuest(userId))
                     {
-                        if (project == RoleConstants.DATAHUB_ADMIN_PROJECT && serviceAuthManager.GetViewingAsGuest(userId))
-                        {
-                            ((ClaimsIdentity)principal.Identity).AddClaim(new Claim(ClaimTypes.Role, RoleConstants.DATAHUB_ROLE_ADMIN_AS_GUEST));
-                        } 
-                        else
-                        {
-                            ((ClaimsIdentity)principal.Identity).AddClaim(new Claim(ClaimTypes.Role, $"{project}{RoleConstants.ADMIN_SUFFIX}"));
-                        }
+                        ((ClaimsIdentity)principal.Identity)?.AddClaim(new Claim(ClaimTypes.Role, RoleConstants.DATAHUB_ROLE_ADMIN_AS_GUEST));
+                    }
+                    else
+                    {
+                        ((ClaimsIdentity)principal.Identity)?.AddClaim(new Claim(ClaimTypes.Role,
+                            $"{project.Project_Acronym_CD}{RoleConstants.GetRoleConstants(role)}"));
                     }
                 }
-                foreach (var project in authorizedProjects)
-                {
-                    ((ClaimsIdentity)principal.Identity).AddClaim(new Claim(ClaimTypes.Role, project.Project_Acronym_CD));
-                }
             }
-
         }
         catch (Exception ex)
         {
             logger.LogCritical(ex, "Cannot load project permissions");
         }
         return principal;
-
     }
-
 }
