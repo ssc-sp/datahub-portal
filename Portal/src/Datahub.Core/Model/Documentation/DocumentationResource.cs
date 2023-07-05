75bf1c66dff0cf2d11f5e2e157ab69dda3c9d39a
Iroel Perez <iroel.perez@gmail.com>
2023-07-05 16:03:54 -0400

    Saving changes

---
+++ Portal/src/Datahub.Core/Model/Documentation/DocumentationResource.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Datahub.Core.Model.Documentation;
+
+public class DocumentationResource
+{
+    public Guid Id { get; set; }
+    public DateTime LastUpdated { get; set; }
+    public int Hits { get; set;}
+}
