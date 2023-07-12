f1e70a6cccb750e15be9e086444e68f52c9c4265
Iroel Perez <iroel.perez@gmail.com>
2023-07-12 08:59:00 -0400

    Reverse proxy built from DB using Projects data

---
+++ Portal/src/Datahub.Infrastructure/Services/ReverseProxy/ProxyConfigProvider.cs
@@ -0,0 +1,33 @@
+﻿using Datahub.Application.Services.ReverseProxy;
+using Yarp.ReverseProxy.Configuration;
+
+namespace Datahub.Infrastructure.Services.ReverseProxy;
+
+internal class ProxyConfigProvider : IProxyConfigProvider
+{
+    private readonly IReverseProxyConfigService _reverseProxyConfigService;
+    private bool _firstCall = true;
+
+    public ProxyConfigProvider(IReverseProxyConfigService reverseProxyConfigService)
+    {
+        _reverseProxyConfigService = reverseProxyConfigService;
+    }
+
+    public IProxyConfig GetConfig()
+    {
+        if (_firstCall)
+        {
+            _firstCall = false;
+            var routes = new List<RouteConfig>();
+            var clusters = new List<ClusterConfig>();
+            return new ProxyConfig(routes, clusters);
+        }
+        else
+        {
+            var config = _reverseProxyConfigService.GetConfigurationFromProjects();
+            return new ProxyConfig(config.Routes, config.Clusters);
+        }
+    }
+}
+
+
