6b6671305bfba775d29b03241a8558c607d0f58d
Iroel Perez <iroel.perez@gmail.com>
2023-07-12 16:46:07 -0400

    Providing the UI to enable/disable the workspace web app

--- Portal/src/Datahub.Infrastructure/Services/ReverseProxy/ProxyConfigProvider.cs
+++ Portal/src/Datahub.Infrastructure/Services/ReverseProxy/ProxyConfigProvider.cs
@@ -6,27 +6,21 @@ namespace Datahub.Infrastructure.Services.ReverseProxy;
 internal class ProxyConfigProvider : IProxyConfigProvider
 {
     private readonly IReverseProxyConfigService _reverseProxyConfigService;
-    private bool _firstCall = true;
 
     public ProxyConfigProvider(IReverseProxyConfigService reverseProxyConfigService)
     {
         _reverseProxyConfigService = reverseProxyConfigService;
     }
 
+    /// <summary>
+    /// Note this method the first call could return an empty list of routes and clusters 
+    /// then it will be invoked 5 minutes later, and so on.
+    /// </summary>
+    /// <returns></returns>
     public IProxyConfig GetConfig()
     {
-        if (_firstCall)
-        {
-            _firstCall = false;
-            var routes = new List<RouteConfig>();
-            var clusters = new List<ClusterConfig>();
-            return new ProxyConfig(routes, clusters);
-        }
-        else
-        {
-            var config = _reverseProxyConfigService.GetConfigurationFromProjects();
-            return new ProxyConfig(config.Routes, config.Clusters);
-        }
+        var config = _reverseProxyConfigService.GetConfigurationFromProjects();
+        return new ProxyConfig(config.Routes, config.Clusters);
     }
 }
 
