3d6d3b421ee26622c7485b9312a4369006d536db
Yan <yjmrobert@gmail.com>
2023-07-26 11:51:11 -0400

    🔧 added self registration table

---
+++ Portal/src/Datahub.Core/Model/Onboarding/SelfRegistrationDetails.cs
@@ -0,0 +1,8 @@
+namespace Datahub.Core.Model.Onboarding;
+
+public class SelfRegistrationDetails
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Comment { get; set; }
+}
