3deda008ad9969b892e040f2c46c9f5ceedb825d
davidreneuw <davrene11@gmail.com>
2023-07-28 14:55:06 -0400

    ✔️ Refactored code to implement users status as a service. Added copy to clipboard button

---
+++ Portal/src/Datahub.Application/Services/IUsersStatusService.cs
@@ -0,0 +1,9 @@
+﻿using Datahub.Application.Configuration;
+
+namespace Datahub.Application.Services
+{
+    public interface IUsersStatusService
+    {
+        public Task<Dictionary<string, List<string>>> GetUsersStatus(IHttpClientFactory _httpClientFactory, DatahubPortalConfiguration _datahubPortalConfiguration);
+    }
+}
