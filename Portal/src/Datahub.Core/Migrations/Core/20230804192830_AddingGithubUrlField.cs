f1029aa53133b046ced3fabb031f2ad9841e5a1c
Iroel Perez <iroel.perez@gmail.com>
2023-08-04 16:12:40 -0400

    Workspace Github integration

---
+++ Portal/src/Datahub.Core/Migrations/Core/20230804192830_AddingGithubUrlField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datahub.Core.Migrations.Core
+{
+    /// <inheritdoc />
+    public partial class AddingGithubUrlField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GitRepo_URL",
+                table: "Projects",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GitRepo_URL",
+                table: "Projects");
+        }
+    }
+}
