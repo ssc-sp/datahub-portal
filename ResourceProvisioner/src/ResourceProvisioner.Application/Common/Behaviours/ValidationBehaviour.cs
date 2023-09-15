bab8f89a39c75ca8c32c6ba4f351a16466507b7b
Erik <erik@apption.com>
2023-09-15 12:36:30 -0400

    updated mediatr to 12.x in rp

--- ResourceProvisioner/src/ResourceProvisioner.Application/Common/Behaviours/ValidationBehaviour.cs
+++ ResourceProvisioner/src/ResourceProvisioner.Application/Common/Behaviours/ValidationBehaviour.cs
@@ -13,11 +13,11 @@ public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TReque
         _validators = validators;
     }
 
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         if (!_validators.Any())
             return await next();
-        
+
         var context = new ValidationContext<TRequest>(request);
 
         var validationResults = await Task.WhenAll(
