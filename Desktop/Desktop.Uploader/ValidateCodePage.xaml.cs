b8959e0e4aa985e9f3c099e3271dc128159a06b4
Erik <erik@apption.com>
2023-09-20 12:01:59 -0400

    - removed foundatio - fixed upload code parsing - minor renames

--- Desktop/Desktop.Uploader/ValidateCodePage.xaml.cs
+++ Desktop/Desktop.Uploader/ValidateCodePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Datahub.Maui.Uploader
+﻿using Datahub.Core.DataTransfers;
+
+namespace Datahub.Maui.Uploader
 {
     public partial class UploadCodePage : ContentPage
     {
@@ -8,9 +10,18 @@
         }
 
 
-        private void UploadCodeText_TextChanged(object sender, TextChangedEventArgs e)
+        private async void UploadCodeText_TextChanged(object sender, TextChangedEventArgs e)
         {
-            ValidateCodeButton.IsEnabled = true;
+            if (CredentialEncoder.IsValid(UploadCodeText.Text))
+            {
+                ValidateCodeButton.IsEnabled = true;
+                ValidateCodeButton.Text = "Continue";
+                await ContinueFlowWithUploadCode(UploadCodeText.Text);
+            } else
+            {
+                ValidateCodeButton.IsEnabled = false;
+                ValidateCodeButton.Text = "Please Enter Valid Code";
+            }
         }
 
         private async void ValidateCodeButton_Clicked(object sender, EventArgs e)
@@ -27,7 +38,7 @@
         {
             try
             {
-                Uri uri = new Uri("https://federal-science-datahub.canada.ca/w/DW1/filelist");
+                Uri uri = new Uri("https://federal-science-datahub.canada.ca/resources/126b3f7a-e320-dc44-3707-e748b998b094");
                 await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
             }
             catch (Exception ex)
@@ -43,7 +54,20 @@
 
         private async void Clipboard_ClipboardContentChanged(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync("//SpeedTest");
+            if (Clipboard.Default.HasText)
+            {
+                var clipboard = await Clipboard.Default.GetTextAsync();
+                if (CredentialEncoder.IsValid(clipboard))
+                {
+                    await ContinueFlowWithUploadCode(clipboard);
+                }
+            }
+        }
+
+        private static async Task ContinueFlowWithUploadCode(string uploadCode)
+        {
+            (Application.Current as App).Context.Credentials = CredentialEncoder.DecodeCredentials(uploadCode);
+            await Shell.Current.GoToAsync("//SpeedTest");            
         }
     }
 }
