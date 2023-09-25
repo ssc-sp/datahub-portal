f83e53dba7c6752e7119e62eb4ff0ceb80bb7432
Yan <yjmrobert@gmail.com>
2023-09-25 16:49:21 -0400

    🔧 finished the py user run function

--- ResourceProvisioner/src/ResourceProvisioner.Functions.Python/function_app.py
+++ ResourceProvisioner/src/ResourceProvisioner.Functions.Python/function_app.py
@@ -1,5 +1,8 @@
 import azure.functions as func
-from pprint import pprint
+import os
+import pprint as pprint
+
+from lib.databricks_utils import get_workspace_client, remove_deleted_users_in_workspace, synchronize_workspace_users
 
 app = func.FunctionApp()
 
@@ -9,6 +12,14 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
 
     workspace_definition = req.get_json()
 
-    
+    databricksHost = workspace_definition['Apps']['azure-databricks']['HostUrl']
+    databricksToken = os.environ['DATABRICKS_TOKEN']
 
-    return func.HttpResponse("HttpTrigger1 function processed a request!!!")
+    workspace_client = get_workspace_client(databricksHost, databricksToken)
+
+    # Cleanup users in workspace that aren't in AAD Graph
+    remove_deleted_users_in_workspace(workspace_client)
+    synchronize_workspace_users(workspace_definition, workspace_client)
+
+
+    return func.HttpResponse("Successfully synchronized workspace users.")
