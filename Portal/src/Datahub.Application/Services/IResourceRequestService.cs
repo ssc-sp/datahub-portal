c77def1aa01f0ded17559ebf9d142e7793d49107
Yan <yjmrobert@gmail.com>
2023-09-28 10:40:14 -0400

    🔧 renamed messaging service

--- Portal/src/Datahub.Application/Services/IResourceRequestService.cs
+++
@@ -1,8 +0,0 @@
-using Datahub.Core.Data.ResourceProvisioner;
-
-namespace Datahub.Application.Services;
-
-public interface IResourceRequestService
-{
-    public Task AddProjectToStorageQueue(CreateResourceData project);
-}
