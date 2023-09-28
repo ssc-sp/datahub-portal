b93fbad82eccc1a6e392548d7008b3ccc316eed8
Alex Khavich <alexk@apption.com>
2023-09-27 20:12:03 -0400

    Updated configuration dialogs

---
+++ Portal/src/Datahub.Portal/Pages/Project/FileExplorer/Storage/AWSConnectionData.cs
@@ -0,0 +1,12 @@
+﻿namespace Datahub.Portal.Pages.Project.FileExplorer.Storage
+{
+    public class AWSConnectionData
+    {
+        public string AccessKeyId { get; set; }
+        public string AccessKeySecret { get; set; }
+        public string Region { get; set; }
+        public string BucketName { get; set; }
+
+        public string ConnectionData => $"{AccessKeyId}|{AccessKeySecret}|{Region}|{BucketName}";
+    }
+}
