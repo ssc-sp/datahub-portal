77a9b4327b1c85fb4e443dfd757dae76b405788e
Yan <yjmrobert@gmail.com>
2023-09-29 12:25:00 -0400

    🔧 added workspace app data to RP

--- ResourceProvisioner/src/ResourceProvisioner.Application/ResourceRun/Commands/CreateResourceRun/CreateResourceRunCommand.cs
+++ ResourceProvisioner/src/ResourceProvisioner.Application/ResourceRun/Commands/CreateResourceRun/CreateResourceRunCommand.cs
@@ -10,6 +10,8 @@ public class CreateResourceRunCommand : IRequest<PullRequestUpdateMessage>
 {
     public List<TerraformTemplate> Templates { get; set; }
     public TerraformWorkspace Workspace { get; set; }
+    
+    public WorkspaceAppData AppData { get; set; }
 
     public string RequestingUserEmail { get; set; }
 }
