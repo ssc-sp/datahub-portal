9f1f90592de6bf3b69c54ee3db42eed31111289b
Alex Khavich <alexk@apption.com>
2023-10-04 12:06:52 -0400

    Add, edit, delete multiple cloud storage accounts

--- Portal/src/Datahub.Portal/Pages/Project/FileExplorer/Storage/CloudStorageContainer.cs
+++ Portal/src/Datahub.Portal/Pages/Project/FileExplorer/Storage/CloudStorageContainer.cs
@@ -4,14 +4,18 @@ namespace Datahub.Portal.Pages.Project.FileExplorer.Storage;
 
 public class CloudStorageContainer
 {
-    public CloudStorageContainer(string accountName, string containerName, CloudStorageProviderType provider, ICloudStorageManager storageManager)
+    public CloudStorageContainer(string accountName, string containerName, CloudStorageProviderType provider, ICloudStorageManager storageManager, int? id = null, bool enabled = true)
     {
         ContainerName = containerName;
         AccountName = accountName;
         CloudStorageProvider = provider;
         StorageManager = storageManager;
+        Id = id;
+        Enabled = enabled;
     }
 
+    public int? Id { get; set; }
+    public bool Enabled { get; set; }
     public string Name => ContainerName;
     public string ContainerName { get; }
     public string AccountName { get; }
