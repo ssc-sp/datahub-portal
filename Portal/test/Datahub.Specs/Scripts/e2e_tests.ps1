a018dac0d2ba07214ab53f00a5039d65490345d5
davidreneuw <davrene11@gmail.com>
2023-10-10 11:35:29 -0400

    Added script to run tests on given environment, made a few modifications for tests to properly run

---
+++ Portal/test/Datahub.Specs/Scripts/e2e_tests.ps1
@@ -0,0 +1,29 @@
+﻿param (
+    [string]$env,
+    [string]$headless='false',
+    [int]$slowmo=2000
+)
+
+# We remember the previous working directory and set the working directory to the root of the Datahub.Specs project
+$prevPwd = $PWD; Set-Location -ErrorAction Stop -LiteralPath $PSScriptRoot/..
+
+try {
+    
+    # We figure out the proper url to use for the tests based on the environment
+    $url = switch ( $env )
+    {
+        'dev' {"https://fsdh-portal-app-dev.azurewebsites.net"}
+        'poc' {"https://federal-science-datahub.canada.ca"}
+        'int' {"https://fsdh-portal-app-int.azurewebsites.net"}
+        'local' {"http://localhost:5001" }
+        default {"http://localhost:5001"}
+    }
+    
+    # We start the tests with the given parameters
+    echo "Running tests for $env environment at $url"
+    dotnet test --environment TEST_URL=$url --environment HEADLESS=$headless --environment SLOWMO=$slowmo --environment SCRIPT_RUN=$true
+}
+finally {
+    # We end the tests by returning to the previous working directory
+    $prevPwd | Set-Location
+}
