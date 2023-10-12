8fa1395472e6685627bd0bbb7ec3a27f0ba18b66
Erik <erik@apption.com>
2023-10-12 12:20:56 -0400

    add base url in token to support uploader links in dev

--- Portal/test/Datahub.Infrastructure.UnitTests/DataTransferTests.cs
+++ Portal/test/Datahub.Infrastructure.UnitTests/DataTransferTests.cs
@@ -13,19 +13,20 @@ namespace Datahub.Core.DataTransfers.UnitTests
         public void EncodeDecodeCredentials()
         {
             var exp = DateTimeOffset.UtcNow;
-            var creds = new UploadCredentials() {  SASToken = "tk1", SASTokenExpiry = exp , WorkspaceCode = "test1"};
+            var creds = new UploadCredentials() {  SASToken = "tk1", SASTokenExpiry = exp , WorkspaceCode = "test1", DataHubEnvironment = DataHubEnvironment.Local };
             var encoded = CredentialEncoder.EncodeCredentials(creds);
             var decoded = CredentialEncoder.DecodeCredentials(encoded);
             Assert.That(decoded.SASToken, Is.EqualTo(creds.SASToken));
             Assert.That(decoded.SASTokenExpiry, Is.EqualTo(creds.SASTokenExpiry));
             Assert.That(decoded.WorkspaceCode, Is.EqualTo(creds.WorkspaceCode));
+            Assert.That(decoded.DataHubEnvironment, Is.EqualTo(creds.DataHubEnvironment));
         }
 
         [Test]
         public void ValidateCredentials()
         {
             var exp = DateTimeOffset.UtcNow;
-            var creds = new UploadCredentials() { SASToken = "tk1", SASTokenExpiry = exp, WorkspaceCode = "test1" };
+            var creds = new UploadCredentials() { SASToken = "tk1", SASTokenExpiry = exp, WorkspaceCode = "test1", DataHubEnvironment = DataHubEnvironment.Local };
             var encoded = CredentialEncoder.EncodeCredentials(creds);
             var bad = encoded + "asdasdasd";
             Assert.That(CredentialEncoder.IsValid(bad), Is.False);
