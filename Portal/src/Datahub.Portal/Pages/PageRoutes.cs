2adae8fce8c51d2bc1bc3965c1c910e74b9337c2
Yan <yjmrobert@gmail.com>
2023-11-22 15:48:33 -0500

    🔧 updated more links

--- Portal/src/Datahub.Portal/Pages/PageRoutes.cs
+++ Portal/src/Datahub.Portal/Pages/PageRoutes.cs
@@ -2,13 +2,15 @@ namespace Datahub.Portal.Pages;
 
 public static class PageRoutes
 {
-    public const string Explore = "/w";
+    public const string Explore = "/explore";
     public const string Home = "/home";
+    public const string News = "/news";
+    
+    public const string Achievements = "/achievements";
+    
     public const string WorkspaceDefault = "/w/{WorkspaceAcronymParam}";
     public const string Workspace = "/w/{WorkspaceAcronymParam}/{Section}";
-    public const string Achievements = "/achievements";
     
     public const string SettingsDefault = "/settings/";
     public const string Settings = "/settings/{Section}";
-    
 }
