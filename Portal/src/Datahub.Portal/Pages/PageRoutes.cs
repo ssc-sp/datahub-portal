0733f2f43a7ce7065c9340cc895035574e935c67
Yan <yjmrobert@gmail.com>
2023-11-22 09:31:25 -0500

    🔧 fixed up folder organization and relative links

--- Portal/src/Datahub.Portal/Pages/PageRoutes.cs
+++ Portal/src/Datahub.Portal/Pages/PageRoutes.cs
@@ -3,7 +3,12 @@ namespace Datahub.Portal.Pages;
 public static class PageRoutes
 {
     public const string Explore = "/w";
+    public const string Home = "/home";
     public const string WorkspaceDefault = "/w/{WorkspaceAcronymParam}";
     public const string Workspace = "/w/{WorkspaceAcronymParam}/{Section}";
+    public const string Achievements = "/achievements";
+    
+    public const string SettingsDefault = "/settings/";
+    public const string Settings = "/settings/{Section}";
     
 }
