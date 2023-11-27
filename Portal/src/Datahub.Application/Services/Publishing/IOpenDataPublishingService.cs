66bf6721714987f0468d020befa0bd01d43bf73a
Alex Khavich <alexk@apption.com>
2023-11-26 21:04:28 -0500

    Open Data publishing workflow WIP

---
+++ Portal/src/Datahub.Application/Services/Publishing/IOpenDataPublishingService.cs
@@ -0,0 +1,15 @@
+﻿using Datahub.Core.Model.Datahub;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.Application.Services.Publishing
+{
+    public interface IOpenDataPublishingService
+    {
+        Task<List<OpenDataSubmission>> GetOpenDataSubmissionsAsync(int workspaceId);
+        Task<OpenDataSubmission> GetOpenDataSubmissionAsync(long submissionId);
+    }
+}
