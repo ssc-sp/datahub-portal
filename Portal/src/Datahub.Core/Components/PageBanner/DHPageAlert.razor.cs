07780f45c96757a9d4d3649fc6ea4b197675a51e
davidreneuw <davrene11@gmail.com>
2023-12-08 11:50:56 -0500

    Revamped the page alerts Fixed an issue with learn sidebar crashing when resources are loading Moved home alert to the main layout

---
+++ Portal/src/Datahub.Core/Components/PageBanner/DHPageAlert.razor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Nodes;
+
+namespace Datahub.Core.Components.PageBanner
+{
+    public partial class DHPageAlert
+    {
+        private const string Prefix = "tutorials";
+        
+        public const string Home = $"{Prefix}/home";
+        public static readonly PageAlertObject HomeAlert = new("/Banners/Landing", "/fr/Banners/Atterrissage");
+        
+        public const string Projects = $"{Prefix}/projects";
+        public static readonly PageAlertObject ProjectsAlert = new("/Banners/AWS-storage-Databricks", "/fr/Banners/Stockage-AWS-Databricks");
+        
+        public const string ProjectFeatures = $"{Prefix}/features";
+        public const string Storage = $"{Prefix}/storage";
+
+        public static readonly Dictionary<string, dynamic> Alerts = new()
+        {
+            {Home, new List<PageAlertObject>() {HomeAlert, ProjectsAlert}},
+        };
+    }
+    
+    public struct PageAlertObject
+    {
+        public PageAlertObject(string wikiLinkEN, string wikiLinkFR)
+        {
+            WikiLinkEN = wikiLinkEN;
+            WikiLinkFR = wikiLinkFR;
+        }
+
+        public string WikiLinkEN { get; set; }
+        public string WikiLinkFR { get; set; }
+    }
+}
--- Portal/src/Datahub.Core/Model/UserTracking/Configuration/UserSettingsConfiguration.cs
+++ Portal/src/Datahub.Core/Model/UserTracking/Configuration/UserSettingsConfiguration.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Datahub.Core.Model.UserTracking.Configuration
 {
@@ -8,6 +10,11 @@ namespace Datahub.Core.Model.UserTracking.Configuration
     {
         public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<UserSettings> builder)
         {
+            var valueComparer = new ValueComparer<List<string>>(
+                (c1, c2) => c1.SequenceEqual(c2),
+                c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                c => c.ToList());
+            
             builder.ToTable("UserSettings");
             builder.HasKey(e => e.PortalUserId);
             builder.Property(e => e.UserName)
@@ -15,13 +22,19 @@ namespace Datahub.Core.Model.UserTracking.Configuration
             builder.Property(e => e.AcceptedDate);
             builder.Property(e => e.Language)
                 .HasMaxLength(5);
-            builder.Property(e => e.NotificationsEnabled);
-            builder.Property(e => e.HideAchievements);
-            builder.Property(e => e.HideAlerts);
+            builder.Property(e => e.NotificationsEnabled)
+                .HasDefaultValue(true);
+            builder.Property(e => e.HideAchievements)
+                .HasDefaultValue(false);
+            builder.Property(e => e.HideAlerts)
+                .HasDefaultValue(false);
             builder.Property(e => e.HiddenAlerts)
                 .HasConversion(
                     v => string.Join(',', v),
                     v => string.IsNullOrEmpty(v) ? new List<string>() : v.Split(',', System.StringSplitOptions.RemoveEmptyEntries).ToList());
+            builder.Property(e => e.HiddenAlerts)
+                .Metadata
+                .SetValueComparer(valueComparer);
             
         }
     }
