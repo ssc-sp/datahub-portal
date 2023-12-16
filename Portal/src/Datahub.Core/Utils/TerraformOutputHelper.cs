877cb3ed8b5cac34ade12247d4286a8d66584d52
Yan <yjmrobert@gmail.com>
2023-12-15 19:41:14 -0500

    🔧 Add Azure Postgres support to Terraform output processing
    The new changes introduce the handling of Azure Postgres output variables inside the Terraform output processing logic. This includes both parsing the output variables and storing the relevant information about Azure Postgres in the project resources. Moreover, enhancements were made for better formatting the expected Terraform output for different Azure services and corresponding unit tests were added to ensure the correctness of this new feature.

--- Portal/src/Datahub.Core/Utils/TerraformOutputHelper.cs
+++ Portal/src/Datahub.Core/Utils/TerraformOutputHelper.cs
@@ -6,7 +6,7 @@ namespace Datahub.Core.Utils;
 
 public static class TerraformOutputHelper
 {
-    public static string GetExpectedTerraformOutput(Datahub_Project project, string workspaceId = null, string workspaceUrl = null)
+    public static string GetExpectedTerraformOutput(Datahub_Project project, string workspaceId = null, string workspaceUrl = null, string envAcronym = "dev")
     {
         var expectedTerraformOutput = GetExpectedTerraformOutputResourceGroupString();
         // var workspaceId = "";
@@ -31,8 +31,15 @@ public static class TerraformOutputHelper
             expectedTerraformOutput =
                 string.Join(",", expectedTerraformOutput, GetExpectedTerraformOutputAzureWebAppString());
         }
+        
+        if(project.Resources.Any(r => r.ResourceType.Equals(TerraformTemplate.GetTerraformServiceType(TerraformTemplate.AzurePostgres))))
+        {
+            expectedTerraformOutput =
+                string.Join(",", expectedTerraformOutput, GetExpectedTerraformOutputAzurePostgresString());
+        }
 
         var content = expectedTerraformOutput
+            .Replace("{{env_acronym}}", envAcronym)
             .Replace("{{project_cd}}", project.Project_Acronym_CD)
             .Replace("{{project_cd_lower}}", project.Project_Acronym_CD.ToLower())
             .Replace("{{workspace_version}}", project.Version)
@@ -42,17 +49,56 @@ public static class TerraformOutputHelper
         return $"{{\n{content}\n}}";
     }
 
+    private static string GetExpectedTerraformOutputAzurePostgresString()
+    {
+        return @"  ""azure_postgresql_db_name"": {
+        ""sensitive"": false,
+        ""type"": ""string"",
+        ""value"": ""fsdh""
+    },
+    ""azure_postgresql_dns"": {
+        ""sensitive"": false,
+        ""type"": ""string"",
+        ""value"": ""fsdh-{{project_cd_lower}}-psql-{{env_acronym}}.postgres.database.azure.com""
+    },
+    ""azure_postgresql_id"": {
+        ""sensitive"": false,
+        ""type"": ""string"",
+        ""value"": ""/subscriptions/bc4bcb08-d617-49f4-b6af-69d6f10c240b/resourceGroups/fsdh_proj_{{project_cd_lower}}_{{env_acronym}}_rg/providers/Microsoft.DBforPostgreSQL/flexibleServers/fsdh-{{project_cd_lower}}-psql-{{env_acronym}}""
+    },
+    ""azure_postgresql_secret_name_admin"": {
+        ""sensitive"": false,
+        ""type"": ""string"",
+        ""value"": ""datahub-psql-admin""
+    },
+    ""azure_postgresql_secret_name_password"": {
+        ""sensitive"": false,
+        ""type"": ""string"",
+        ""value"": ""datahub-psql-password""
+    },
+    ""azure_postgresql_server_name"": {
+        ""sensitive"": false,
+        ""type"": ""string"",
+        ""value"": ""fsdh-{{project_cd_lower}}-psql-{{env_acronym}}""
+    },
+    ""azure_psql_module_status"": {
+        ""sensitive"": false,
+        ""type"": ""string"",
+        ""value"": ""completed""
+    }";
+    }
+
     private static string GetExpectedTerraformOutputAzureWebAppString()
     {
         return @"  ""azure_app_service_hostname"": {
     ""sensitive"": false,
     ""type"": ""string"",
-    ""value"": ""fsdh-proj-{{project_cd_lower}}-webapp-poc.azurewebsites.net""
+    ""value"": ""fsdh-proj-{{project_cd_lower}}-webapp-{{env_acronym}}.azurewebsites.net""
   },
   ""azure_app_service_id"": {
     ""sensitive"": false,
     ""type"": ""string"",
-    ""value"": ""/subscriptions/bc4bcb08-d617-49f4-b6af-69d6f10c240b/resourceGroups/fsdh_proj_{{project_cd_lower}}_poc_rg/providers/Microsoft.Web/sites/fsdh-proj-{{project_cd_lower}}-webapp-poc""
+    ""value"": ""/subscriptions/bc4bcb08-d617-49f4-b6af-69d6f10c240b/resourceGroups/fsdh_proj_{{project_cd_lower}}_{{env_acronym}}_rg/providers/Microsoft.Web/sites/fsdh-proj-{{project_cd_lower}}-webapp-{{env_acronym}}""
   },
   ""azure_app_service_module_status"": {
     ""sensitive"": false,
@@ -76,7 +122,7 @@ public static class TerraformOutputHelper
   ""azure_databricks_workspace_name"": {
     ""sensitive"": false,
     ""type"": ""string"",
-    ""value"": ""fsdh-dbk-{{project_cd_lower}}-poc""
+    ""value"": ""fsdh-dbk-{{project_cd_lower}}-{{env_acronym}}""
   },
   ""azure_databricks_workspace_url"": {
     ""sensitive"": false,
@@ -90,7 +136,7 @@ public static class TerraformOutputHelper
         return @"  ""azure_storage_account_name"": {
     ""sensitive"": false,
     ""type"": ""string"",
-    ""value"": ""fsdhproj{{project_cd_lower}}poc""
+    ""value"": ""fsdhproj{{project_cd_lower}}{{env_acronym}}""
   },
   ""azure_storage_blob_status"": {
     ""sensitive"": false,
@@ -114,7 +160,7 @@ public static class TerraformOutputHelper
   ""azure_resource_group_name"": {
     ""sensitive"": false,
     ""type"": ""string"",
-    ""value"": ""fsdh_proj_{{project_cd_lower}}_poc_rg""
+    ""value"": ""fsdh_proj_{{project_cd_lower}}_{{env_acronym}}_rg""
   },
   ""project_cd"": {
     ""sensitive"": false,
