4d9511b690c2cc7149a01a32afbc166ca4eaa433
David René <davrene11@gmail.com>
2023-12-19 15:48:07 -0500

    Created a BugReport Queue trigger function that creates ADO issues and email Data Solutions email Created an EmailService that makes building emails easier for functions Changed all functions to use the EmailService Moved the email templates to a different folder Created BugReportMessage and Sender so that Mediator can handle bug reports Implemented BugReports in the HelpDialog and on System Error pages.

---
+++ Portal/src/Datahub.Infrastructure/Queues/MessageHandlers/BugReportMessageSender.cs
@@ -0,0 +1,14 @@
+﻿using Datahub.Infrastructure.Queues.Messages;
+using Microsoft.Extensions.Configuration;
+
+namespace Datahub.Infrastructure.Queues.MessageHandlers
+{
+    public class BugReportMessageSender : QueueMessageSender<BugReportMessage>
+    {
+        public BugReportMessageSender(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        protected override string ConfigPathOrQueueName => "bug-report";
+    }
+}
