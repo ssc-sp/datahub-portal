627561d50c5f88e9873fab757c70536885cd8144
Erik <erik@apption.com>
2023-12-20 09:18:43 -0500

    refactored connection data to keyvault

---
+++ Portal/src/Datahub.Infrastructure.Offline/Security/OfflineKeyVaultUserService.cs
@@ -0,0 +1,43 @@
+﻿using Datahub.Application.Services.Security;
+using Datahub.Core.Model.CloudStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.Infrastructure.Offline.Security
+{
+    public class OfflineKeyVaultUserService : IKeyVaultUserService
+    {
+        public Task<IDictionary<string, string>> GetAllSecrets(string acronym)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDictionary<string, string>> GetAllSecrets(ProjectCloudStorage projectCloudStorage, string acronym)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string?> GetSecret(string acronym, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool?> IsSecretExpired(string acronym, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StoreAllSecrets(ProjectCloudStorage projectCloudStorage, string acronym, IDictionary<string, string> connectionData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StoreSecret(string acronym, string name, string secretValue, int monthValidity = 12)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Portal/src/Datahub.Infrastructure/Services/Security/ConfigureSecurityServices.cs
@@ -0,0 +1,19 @@
+﻿using Datahub.Application.Services.Security;
+using Datahub.Core.Services.Achievements;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.Infrastructure.Services.Security;
+
+public static class ConfigureSecurityServices
+{
+    public static IServiceCollection AddSecurityServices(this IServiceCollection services)
+    {
+        services.AddScoped<IKeyVaultUserService, KeyVaultUserService>();
+        return services;
+    }
+}
