04d1afcdfb65499ca05b320465a4f898ba50fe88
davidreneuw <davrene11@gmail.com>
2024-02-13 08:36:08 -0500

    ✅ Use client to post work items with SP credentials

--- Portal/src/Datahub.Functions/Providers/AdoClientProvider.cs
+++ Portal/src/Datahub.Functions/Providers/AdoClientProvider.cs
@@ -1,11 +1,9 @@
-﻿using System.Net.Http.Headers;
-using System.Text;
-using Azure.Core;
+﻿using Azure.Core;
 using Azure.Identity;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
 using Microsoft.VisualStudio.Services.Client;
+using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.WebApi;
-using Newtonsoft.Json;
 
 namespace Datahub.Functions.Providers
 {
@@ -21,21 +19,30 @@ namespace Datahub.Functions.Providers
         public async Task<WorkItemTrackingHttpClient> GetWorkItemClient()
         {
             var connection = await GetVssConnection();
-            var client = connection.GetClient<WorkItemTrackingHttpClient>();
+            var client = await connection.GetClientAsync<WorkItemTrackingHttpClient>();
 
             return client;
         }
+
         private async Task<VssConnection> GetVssConnection()
         {
+            var aadCredentials = await GetCredentials();
+            VssConnection vssConnection = new VssConnection(new Uri(_config.AdoConfig.OrgUrl), aadCredentials);
+            vssConnection.ConnectAsync().SyncResult();
+            return vssConnection;
+        }
+
+        private async Task<VssCredentials> GetCredentials()
+        {
+            const string scope = "499b84ac-1321-427f-aa17-267ca6975798/.default";
             var credentials = new ClientSecretCredential(_config.TenantId, _config.AdoConfig.SpClientId,
                 _config.AdoConfig.SpClientSecret);
             var accessToken =
                 await credentials.GetTokenAsync(new TokenRequestContext(new[]
-                    { _config.ClientId + "/.default" }));
+                    { scope }));
             var aadToken = new VssAadToken("Bearer", accessToken.Token);
             var aadCredentials = new VssAadCredential(aadToken);
-            VssConnection vssConnection = new VssConnection(new Uri(_config.AdoConfig.OrgUrl), aadCredentials);
-            return vssConnection;
+            return aadCredentials;
         }
     }
 }
