f6f5710a0990947e3f8f469ef01fa44eeb6051b6
Alex Khavich <alex.khavich@ssc-spc.gc.ca>
2024-03-04 16:22:52 -0500

    Reorganization, CkanService updates

---
+++ Portal/src/Datahub.Application/Exceptions/OpenDataPublishingException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.Application.Exceptions
+{
+    public class OpenDataPublishingException : Exception
+    {
+        public OpenDataPublishingException() : base() { }
+        public OpenDataPublishingException(string? message) : base(message) { }
+        public OpenDataPublishingException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
