ae45316bb9cdeafb98fbd1ee523e9897f86e52ad
David René <davrene11@gmail.com>
2024-03-12 11:56:47 -0400

    Began implementing unit tests

---
+++ Portal/test/Datahub.Infrastructure.UnitTests/Services/WorkspaceBudgetManagementServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Datahub.Application.Services.Azure;
+using Datahub.Application.Services.Budget;
+using Datahub.Infrastructure.Services.Azure;
+using Microsoft.Extensions.Logging;
+using static Datahub.Infrastructure.UnitTests.Testing;
+using NSubstitute;
+
+namespace Datahub.Infrastructure.UnitTests.Services
+{
+    [TestFixture]
+    public class WorkspaceBudgetManagementServiceTests
+    {
+        private IAzureResourceManagerClientProvider _armClientProvider;
+        private IWorkspaceBudgetManagementService _sut;
+        private readonly ILoggerFactory _loggerFactory;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _armClientProvider = new AzureResourceManagerClientProvider(_loggerFactory.CreateLogger<AzureResourceManagerClientProvider>());
+            _armClientProvider.Initialize();
+        }
+    }
+}
