a4b678e84f7a6b5364574bb23a62ff66740206f7
Vladimir Bukharov <Vladimir.Bukharov@ssc-spc.gc.ca>
2024-03-14 17:24:33 -0700

    initial commit

---
+++ ResourceProvisioner/src/ResourceProvisioner.Infrastructure/Helpers/ForwardableAdapter.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Newtonsoft.Json;
+using ResourceProvisioner.Domain.Common;
+
+namespace ResourceProvisioner.Infrastructure.Helpers;
+
+public class ForwardableAdapter<TRequest> : IForwardableMessage where TRequest : IRequest
+{
+    public TRequest Message { get; private set; }
+    public string Content => JsonConvert.SerializeObject(Message);
+    public ForwardableAdapter(TRequest message)
+    {
+        Message = message;
+    }
+}
---
+++ ResourceProvisioner/src/ResourceProvisioner.Infrastructure/Services/ForwardingConsumer.cs
@@ -0,0 +1,18 @@
+﻿using MassTransit;
+using ResourceProvisioner.Domain.Common;
+
+namespace ResourceProvisioner.Infrastructure.Services
+{
+    public class ForwardingConsumer: IConsumer<IForwardableMessage>
+    {
+        private readonly AzureServiceBusForwarder _forwarder;
+        public ForwardingConsumer(AzureServiceBusForwarder forwarder)
+        {
+            _forwarder = forwarder;
+        }
+        public async Task Consume(ConsumeContext<IForwardableMessage> context)
+        {
+            await _forwarder.ForwardMessageAsync(context.Message);
+        }
+    }
+}
