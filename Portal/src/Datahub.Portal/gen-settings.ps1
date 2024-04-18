16e7c5cc68e3d70266598b979cdea07a2e73fb1b
Erik <erik@apption.com>
2024-04-18 17:04:43 -0400

    powershell module for generating settings

---
+++ Portal/src/Datahub.Portal/gen-settings.ps1
@@ -0,0 +1,6 @@
+﻿$CurrentPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+#get full path from $CurrentPath
+
+Import-Module $CurrentPath/../../../scripts/appsettings.psm1 -Force
+
+Export-AppSettings -SourceFile "template.appsettings.sand.json" -Target AppSettings -EnvironmentName test
