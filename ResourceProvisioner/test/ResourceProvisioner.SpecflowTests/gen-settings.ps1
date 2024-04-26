dad5eb060a053bb7a3821fd100718c15510ed880
Erik <erik@apption.com>
2024-04-26 09:16:17 -0400

    - cleanup of old files

--- ResourceProvisioner/test/ResourceProvisioner.SpecflowTests/gen-settings.ps1
+++
@@ -1,6 +0,0 @@
-﻿$CurrentPath = Split-Path -Parent $MyInvocation.MyCommand.Path
-#get full path from $CurrentPath
-
-Import-Module $CurrentPath/../../../scripts/appsettings.psm1 -Force
-
-Export-AppSettings -SourceFile "template.appsettings.test.json" -Target AppSettings -EnvironmentName test
