875fca1479a805aa3c5b3a3eca13722a93270189
Sean-Stilwell <sstil051@uottawa.ca>
2024-04-30 12:00:12 -0400

    Adding initial version of the admin support request page

--- Portal/src/Datahub.Portal/Layout/SidebarIcons.cs
+++ Portal/src/Datahub.Portal/Layout/SidebarIcons.cs
@@ -36,6 +36,7 @@ public static class SidebarIcons
     public const string HealthCheck = "fa-light fa-heartbeat";
     public const string Email = "fa-light fa-mailbox";
     public const string AzureSubscriptions = "fa-light fa-cloud";
+    public const string SupportRequests = "fa-light fa-headset";
     
     // Default Sidebar Icons
     public const string Default = "fa-light fa-table-layout";
