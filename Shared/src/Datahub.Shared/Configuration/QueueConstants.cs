eb012285cbb19410e8c41279df2f14db508c6e70
Yan <yjmrobert@gmail.com>
2024-05-08 13:09:43 -0400

    🔧 Updated project usage notifier to service bus

--- Shared/src/Datahub.Shared/Configuration/QueueConstants.cs
+++ Shared/src/Datahub.Shared/Configuration/QueueConstants.cs
@@ -4,4 +4,7 @@ public static class QueueConstants
 {
     public const string BugReportQueueName = "bug-report";
     public const string EmailNotificationQueueName = "email-notification";
+    public const string InfrastructureHealthCheckQueueName = "infrastructure-health-check";
+    public const string WorkspaceAppServiceConfigurationQueueName = "workspace-app-service-configuration";
+    public const string ProjectUsageNotificationQueueName = "project-usage-notification";
 }
