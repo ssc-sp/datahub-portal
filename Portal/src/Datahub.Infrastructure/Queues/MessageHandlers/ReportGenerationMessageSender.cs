e9d7d79116cf61f75d841d6429d3600350b1ba90
Sean-Stilwell <sstil051@uottawa.ca>
2024-05-31 09:23:46 -0400

    Updating to work with masstransit

--- Portal/src/Datahub.Infrastructure/Queues/MessageHandlers/ReportGenerationMessageSender.cs
+++ Portal/src/Datahub.Infrastructure/Queues/MessageHandlers/ReportGenerationMessageSender.cs
@@ -1,13 +1,13 @@
 ﻿using Datahub.Infrastructure.Queues.Messages;
+using Datahub.Shared.Configuration;
+using MassTransit;
 using Microsoft.Extensions.Configuration;
 
 namespace Datahub.Infrastructure.Queues.MessageHandlers
 {
-    public class ReportGenerationMessageSender : QueueMessageSender<ReportGenerationMessage>
+    public class ReportGenerationMessageSender(ISendEndpointProvider sendEndpointProvider)
+        : QueueMessageSender<ReportGenerationMessage>(sendEndpointProvider)
     {
-        public ReportGenerationMessageSender(IConfiguration configuration) : base(configuration)
-        {
-        }
 
         protected override string ConfigPathOrQueueName => "QueueReportGeneration";
     }
