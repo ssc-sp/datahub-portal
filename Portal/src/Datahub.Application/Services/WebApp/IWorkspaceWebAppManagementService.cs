078da40277c0c334a9798fe49427033ed08b3bc9
davidreneuw <davrene11@gmail.com>
2024-06-26 11:23:12 -0400

    Make env vars available to app service

--- Portal/src/Datahub.Application/Services/WebApp/IWorkspaceWebAppManagementService.cs
+++ Portal/src/Datahub.Application/Services/WebApp/IWorkspaceWebAppManagementService.cs
@@ -6,10 +6,15 @@ namespace Datahub.Application.Services.WebApp
 {
     public interface IWorkspaceWebAppManagementService
     {
+        public Task<bool> Start(string webAppId, string workspaceAcronym);
         public Task<bool> Start(string webAppId);
         public Task<bool> Stop(string webAppId);
+        public Task<bool> Restart(string webAppId, string workspaceAcronym);
         public Task<bool> Restart(string webAppId);
         public Task<bool> GetState(string webAppId);
+        public Task SetAppSettings(string workspaceAcronym, string webAppId);
+        public Task<Dictionary<string, string>> GetAppSettings(string webAppId);
+        public Task<Dictionary<string, string>> GetEnvironmentVariables(string webAppId);
         public Task SaveConfiguration(string workspaceAcronym, AppServiceConfiguration configuration);
         public Task FillSystemConfiguration(string workspaceAcronym, AppServiceConfiguration configuration);
         public Task Configure(string workspaceAcronym, AppServiceConfiguration configuration);
