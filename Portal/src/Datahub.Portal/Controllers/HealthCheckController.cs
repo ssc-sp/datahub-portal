72f34e5f6efd45a34dc9c7e9a8261345bcd7ae71
Vladimir Bukharov <Vladimir.Bukharov@ssc-spc.gc.ca>
2024-07-29 08:55:40 -0700

    wip

--- Portal/src/Datahub.Portal/Controllers/HealthCheckController.cs
+++ Portal/src/Datahub.Portal/Controllers/HealthCheckController.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
-using Datahub.Core.Model.Datahub;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Datahub.Core.Model.Health;
 using System.Collections.Concurrent;
+using Microsoft.Identity.Client;
+using Datahub.Application.Configuration;
 using Datahub.Core.Model.Context;
+using Datahub.Core.Model.Health;
+using Datahub.Shared.Clients;
+using Datahub.Shared.Configuration;
+using Azure.Core;
+using System.Net.Http.Headers;
+
 
 namespace Datahub.Portal.Controllers
 {
@@ -17,12 +21,20 @@ namespace Datahub.Portal.Controllers
     {
         private readonly DatahubProjectDBContext _dbProjectContext;
         private readonly IMemoryCache _cache;
+        private readonly HttpClient _httpClient;
+        private readonly DatahubPortalConfiguration _portalConfiguration;
         private static readonly ConcurrentDictionary<string, SemaphoreSlim> _locks = new ConcurrentDictionary<string, SemaphoreSlim>();
 
-        public HealthCheckController(DatahubProjectDBContext dbProjectContext, IMemoryCache cache)
+
+        public HealthCheckController(DatahubProjectDBContext dbProjectContext, 
+            IMemoryCache cache,
+            DatahubPortalConfiguration portalConfiguration,
+            HttpClient httpClient)
         {
             _dbProjectContext = dbProjectContext;
             _cache = cache;
+            _portalConfiguration = portalConfiguration;
+            _httpClient = httpClient;
         }
 
         [HttpGet]
@@ -48,6 +60,72 @@ namespace Datahub.Portal.Controllers
             }
         }
          
+        [HttpGet("logstream1")]
+        public async Task<IActionResult> GetKuduLogStream()
+        {
+            
+            var kuduUrl = $"https://fsdh-portal-app-dev.scm.azurewebsites.net/api/logstream";
+            var request = new HttpRequestMessage(HttpMethod.Get, kuduUrl);
+
+            var access_token = await GetUserAccessTokenAsync(); //GetAccessTokenAsync();
+            _httpClient.DefaultRequestHeaders.Add("Bearer", access_token);
+
+            var response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var stream = await response.Content.ReadAsStreamAsync();
+                return new FileStreamResult(stream, response.Content.Headers.ContentType.ToString());
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, await response.Content.ReadAsStringAsync());
+            }
+            
+        // var content = await response.Content.ReadAsStringAsync();
+        // return Content(content, "text/html");
+        }
+
+        [HttpGet("logstream")]
+        public async Task<IActionResult> GetLogStream()
+        {
+            var access_token = await GetAccessTokenAsync();
+            _httpClient.DefaultRequestHeaders.Add("Bearer", access_token);
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://fsdh-portal-app-dev.scm.azurewebsites.net/api/logstream");
+            var response = await _httpClient.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Content(content, "text/html");
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, response.ReasonPhrase);
+            }
+        }
+        [HttpGet("logstream2")]
+        public async Task<IActionResult> HealthCheck()
+        {
+            var kuduUrl = $"https://fsdh-portal-app-dev.scm.azurewebsites.net/api/logstream";
+            var token = await GetUserAccessTokenAsync();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                var response = await client.GetAsync(kuduUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return Ok(content);
+                }
+                else
+                {
+                    return StatusCode((int)response.StatusCode, response.ReasonPhrase);
+                }
+            }
+        }
+
         private async Task<List<InfrastructureHealthCheck>> LoadCheckData()
         {
             var projects = await _dbProjectContext.Projects.AsNoTracking().Include(p => p.Resources).Select(p => p.Project_Acronym_CD).ToListAsync();
@@ -60,6 +138,40 @@ namespace Datahub.Portal.Controllers
                 .ToListAsync();
 
             return infrastructureHealth;
+        }
+
+        public IConfidentialClientApplication GetClient(AzureDevOpsConfiguration config)
+        {
+            return ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithClientSecret(config.ClientSecret)
+                .WithAuthority(new Uri($"https://login.microsoftonline.com/{config.TenantId}"))
+                .Build();
+        }
+        public async Task<string> GetAccessTokenAsync()
+        {
+            var scopes = "https://management.azure.com/.default";
+            // Configure the Azure DevOps client
+            var config = new AzureDevOpsConfiguration
+            {
+                TenantId = _portalConfiguration.AzureAd.TenantId,
+                ClientId = _portalConfiguration.AzureAd.InfraClientId,
+                ClientSecret = _portalConfiguration.AzureAd.InfraClientSecret
+            };
+            var _app = GetClient(config);
+            var result = await _app.AcquireTokenForClient([scopes]).ExecuteAsync();
+
+            return result.AccessToken;
+        }
+        
+        public async Task<string> GetUserAccessTokenAsync()
+        {
+            var scopes = new[] { "https://management.azure.com/.default" };
+            var credential = new InteractiveBrowserCredential();
+
+            var tokenRequestContext = new TokenRequestContext(scopes);
+            var token = await credential.GetTokenAsync(tokenRequestContext);
+
+            return token.Token;
         }
     }
 }
