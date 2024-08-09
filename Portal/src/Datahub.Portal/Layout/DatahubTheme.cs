4494343fc3999f8ecb3306241f16b3a52fa7eef3
Yan <yjmrobert@gmail.com>
2024-08-09 14:54:50 -0400

    ✨ Added environment top bar to display current environment and classification

--- Portal/src/Datahub.Portal/Layout/DatahubTheme.cs
+++ Portal/src/Datahub.Portal/Layout/DatahubTheme.cs
@@ -1,75 +1,85 @@
+using Datahub.Application.Configuration;
 using MudBlazor;
 
 namespace Datahub.Portal.Layout;
 
 public abstract class DatahubTheme
 {
-    public static readonly MudTheme DefaultTheme = new()
+    public static MudTheme DefaultTheme(DatahubPortalConfiguration? portalConfiguration = null)
     {
-        Typography =
+        return new MudTheme()
         {
-            Default =
+            LayoutProperties =
             {
-                FontFamily = new[] { "Open Sans", "sans-serif" },
-                FontSize = "0.9rem",        
-                LineHeight = 1.75,
-            }, 
-            H1 = new H1()
-            {
-                LineHeight = 1.25,
-                FontSize = "2.5rem",
-                FontWeight = 600,
+                AppbarHeight = portalConfiguration?.Hosting.EnvironmentTopBar == Hosting.TopBarEnvironments.Invisible
+                    ? "64px"
+                    : "84px"
             },
-            H2 = new H2()
+            Typography =
             {
-                LineHeight = 1.35,
-                FontSize = "1.75rem",
-                FontWeight = 600,
+                Default =
+                {
+                    FontFamily = new[] { "Open Sans", "sans-serif" },
+                    FontSize = "0.9rem",
+                    LineHeight = 1.75,
+                },
+                H1 = new H1()
+                {
+                    LineHeight = 1.25,
+                    FontSize = "2.5rem",
+                    FontWeight = 600,
+                },
+                H2 = new H2()
+                {
+                    LineHeight = 1.35,
+                    FontSize = "1.75rem",
+                    FontWeight = 600,
+                },
+                H3 = new H3()
+                {
+                    LineHeight = 1.3,
+                    FontSize = "1.5rem",
+                    FontWeight = 600,
+                },
+                H4 = new H4()
+                {
+                    LineHeight = 1.2,
+                    FontSize = "1.25rem",
+                    FontWeight = 600,
+                },
+                H5 = new H5()
+                {
+                    LineHeight = 1.2,
+                    FontSize = "1.125rem",
+                    FontWeight = 600,
+                },
+                H6 = new H6()
+                {
+                    LineHeight = 1.2,
+                    FontSize = "1rem",
+                    FontWeight = 600,
+                },
+                Body1 =
+                {
+                    FontSize = "0.875rem",
+                    LineHeight = 1.43,
+                    LetterSpacing = ".01071em",
+                },
+                Body2 =
+                {
+                    FontSize = "0.775rem",
+                    LineHeight = 1.36,
+                }
             },
-            H3 = new H3()
+            PaletteLight =
             {
-                LineHeight = 1.3,
-                FontSize = "1.5rem",
-                FontWeight = 600,
+                AppbarBackground = Colors.Shades.White,
+                Background = Colors.Gray.Lighten5
             },
-            H4 = new H4()
+            PaletteDark =
             {
-                LineHeight = 1.2,
-                FontSize = "1.25rem",
-                FontWeight = 600,
+                AppbarBackground = Colors.Gray.Darken3
             },
-            H5 = new H5()
-            {
-                LineHeight = 1.2,
-                FontSize = "1.125rem",
-                FontWeight = 600,
-            },
-            H6 = new H6()
-            {
-                LineHeight = 1.2,
-                FontSize = "1rem",
-                FontWeight = 600,
-            },
-            Body1 =
-            {
-                FontSize = "0.875rem",
-                LineHeight = 1.43,
-                LetterSpacing = ".01071em",
-            },
-            Body2 =
-            {
-                FontSize = "0.775rem",
-                LineHeight = 1.36,
-            }
-        },
-        PaletteLight =
-        {
-            AppbarBackground = Colors.Shades.White,
-            Background = Colors.Gray.Lighten5
-        },
-        PaletteDark =
-        {
-            AppbarBackground = Colors.Gray.Darken3
-        },
-    };
+        };
+    }
 }
