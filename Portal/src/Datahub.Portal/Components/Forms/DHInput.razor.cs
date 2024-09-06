33b7b20984ce9b990e29ea314c7c5da1dd830172
davidreneuw <davrene11@gmail.com>
2024-09-06 13:41:30 -0400

    🔧 WIP - Accessible forms

---
+++ Portal/src/Datahub.Portal/Components/Forms/DHInput.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Datahub.Portal.Components.Forms
+{
+    public partial class DHInput : ComponentBase
+    {
+        [Parameter]
+        public string Label { get; set; }
+        [Parameter]
+        public string Description { get; set; }
+        [Parameter]
+        public string Hint { get; set; }
+        [Parameter]
+        public string Type { get; set; }
+        [Parameter]
+        public string Placeholder { get; set; }
+        [Parameter]
+        public string Value { get; set; }
+        [Parameter]
+        public bool Required { get; set; }
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        [Parameter]
+        public string EndButton { get; set; }
+        
+        private string _id => $"{Label}-id-{Guid.NewGuid()}";
+        private string _helpId => $"{Label}-help-{Guid.NewGuid()}";
+        private string _hintId => $"{Label}-hint-{Guid.NewGuid()}";
+        
+        
+    }
+}
---
+++ Portal/src/Datahub.Portal/Components/Forms/DHInput.razor.js
@@ -0,0 +1,5 @@
+﻿export class DHInput {
+  
+}
+
+window.DHInput = DHInput;
