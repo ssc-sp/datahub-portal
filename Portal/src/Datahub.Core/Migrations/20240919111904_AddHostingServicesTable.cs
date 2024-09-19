276e92523724899c14e655e2a97edbd14bc5a61f
Sean-Stilwell <sstil051@uottawa.ca>
2024-09-19 07:20:19 -0400

    Adding GC Hosting migration

---
+++ Portal/src/Datahub.Core/Migrations/20240919111904_AddHostingServicesTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datahub.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHostingServicesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
