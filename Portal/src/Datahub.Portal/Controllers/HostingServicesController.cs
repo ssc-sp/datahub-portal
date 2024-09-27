d9c066b5a8cf6884baf189040dcb601e442fbb3e
Sean-Stilwell <sstil051@uottawa.ca>
2024-09-27 15:20:10 -0400

    Adding hosting services API to controllers

---
+++ Portal/src/Datahub.Portal/Controllers/HostingServicesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Datahub.Portal.Controllers;
+
+[ApiController]
+public class HostingServicesController : ControllerBase
+{
+    /// <summary>
+    /// Handles the HTTP POST request to the "api/gc-hosting" endpoint.
+    /// </summary>
+    /// <returns>The IActionResult representing the response.</returns>
+    [HttpPost("api/auth-echo")]
+    [Authorize]
+    public async Task<IActionResult> PostAuth()
+    {
+        try
+        {
+            var body = await new StreamReader(Request.Body).ReadToEndAsync();
+            return Ok(body);
+        }
+        catch (Exception ex)
+        {
+            return Ok(ex.Message);
+        }
+    }
+
+    [HttpPost("api/anon-echo")]
+    [AllowAnonymous]
+    public async Task<IActionResult> PostAnon()
+    {
+        try
+        {
+            var body = await new StreamReader(Request.Body).ReadToEndAsync();
+            return Ok(body);
+        }
+        catch (Exception ex)
+        {
+            return Ok(ex.Message);
+        }
+    }
+}
