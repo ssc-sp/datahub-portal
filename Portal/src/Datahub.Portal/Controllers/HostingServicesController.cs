e83f64a4c0fde64385e1588b37993f60fac3e514
Sean-Stilwell <sstil051@uottawa.ca>
2024-10-01 09:29:59 -0400

    Cleaning up the code, processing requests in a separate function

--- Portal/src/Datahub.Portal/Controllers/HostingServicesController.cs
+++ Portal/src/Datahub.Portal/Controllers/HostingServicesController.cs
@@ -8,30 +8,36 @@ namespace Datahub.Portal.Controllers;
 public class HostingServicesController : ControllerBase
 {
     /// <summary>
-    /// Handles the HTTP POST request to the "api/gc-hosting" endpoint.
+    /// Handles the authenticated HTTP POST request to the "api/auth-echo" endpoint.
     /// </summary>
     /// <returns>The IActionResult representing the response.</returns>
     [HttpPost("api/auth-echo")]
     public async Task<IActionResult> PostAuth()
     {
-        try
-        {
-            var body = await new StreamReader(Request.Body).ReadToEndAsync();
-            return Ok(body);
-        }
-        catch (Exception ex)
-        {
-            return Ok(ex.Message);
-        }
+        return await ProcessRequest(Request);
     }
 
+    /// <summary>
+    /// Handles the anonymous HTTP POST request to the "api/anon-echo" endpoint.
+    /// </summary>
+    /// <returns></returns>
     [HttpPost("api/anon-echo")]
     [AllowAnonymous]
     public async Task<IActionResult> PostAnon()
     {
+        return await ProcessRequest(Request);
+    }
+
+    /// <summary>
+    /// Logic to process the request.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    private async Task<IActionResult> ProcessRequest(HttpRequest request)
+    {
         try
         {
-            var body = await new StreamReader(Request.Body).ReadToEndAsync();
+            var body = await new StreamReader(request.Body).ReadToEndAsync();
             return Ok(body);
         }
         catch (Exception ex)
