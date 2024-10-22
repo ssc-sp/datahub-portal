d0c55f3caf50f05b525d8ea673d97300718fccef
Yan <yjmrobert@gmail.com>
2024-10-22 11:53:02 -0400

    🔧 Handle delete request in ProcessRequest method
    Incorporated logic to handle deletion of project resources in the ProcessRequest method. Added checks for delete request status and modified existing resource handling to update the status appropriately rather than creating a new resource.

--- Portal/src/Datahub.Infrastructure/Services/RequestManagementService.cs
+++ Portal/src/Datahub.Infrastructure/Services/RequestManagementService.cs
@@ -10,6 +10,7 @@ using Datahub.Core.Model.Projects;
 using Datahub.Shared.Entities;
 using Datahub.Shared.Enums;
 using Datahub.Core.Model.Context;
+using Datahub.Shared;
 
 namespace Datahub.Infrastructure.Services;
 
@@ -41,12 +42,12 @@ public class RequestManagementService : IRequestManagementService
     }
 
     /// <summary>
-    /// Processes a request to create a project resource.
+    /// Processes the given request for a specific project asynchronously.
     /// </summary>
-    /// <param name="project">The project.</param>
-    /// <param name="requestingUser">The requesting user.</param>
-    /// <param name="requestedTemplate">The requested template.</param>
-    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    /// <param name="project">The project for which the request is being processed.</param>
+    /// <param name="requestingUser">The user making the request.</param>
+    /// <param name="requestedTemplate">The template requested for the project.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     private async Task ProcessRequest(Datahub_Project project, PortalUser requestingUser,
         TerraformTemplate requestedTemplate)
     {
@@ -60,26 +61,23 @@ public class RequestManagementService : IRequestManagementService
         var resource = project.Resources
             .FirstOrDefault(r => r.ResourceType == TerraformTemplate.GetTerraformServiceType(requestedTemplate.Name));
 
-        // TODO: Add delete logic in here
-        
-        
         if (resource is not null)
         {
-            _logger.LogInformation(
-                "Project resource already exists for project {Acronym} and resource type {ServiceType}",
-                project.Project_Acronym_CD, requestedTemplate.Name);
-            return;
+            resource.Status = requestedTemplate.Status;
+        }
+        else
+        {
+            resource = new Project_Resources2
+            {
+                ProjectId = project.Project_ID,
+                RequestedById = requestingUser.Id,
+                ResourceType = TerraformTemplate.GetTerraformServiceType(requestedTemplate.Name),
+                Status = requestedTemplate.Status,
+            };
+
+            await ctx.Project_Resources2.AddAsync(resource);
         }
 
-        resource = new Project_Resources2
-        {
-            ProjectId = project.Project_ID,
-            RequestedById = requestingUser.Id,
-            ResourceType = TerraformTemplate.GetTerraformServiceType(requestedTemplate.Name),
-            Status = requestedTemplate.Status,
-        };
-        
-        await ctx.Project_Resources2.AddAsync(resource);
         await ctx.TrackSaveChangesAsync(_datahubAuditingService);
     }
 
@@ -90,7 +88,8 @@ public class RequestManagementService : IRequestManagementService
     /// <param name="terraformTemplate">The Terraform template.</param>
     /// <param name="requestingUser">The user making the request.</param>
     /// <returns>True if the Terraform request was handled successfully; otherwise, false.</returns>
-    public async Task<bool> HandleTerraformRequestServiceAsync(Datahub_Project datahubProject, TerraformTemplate terraformTemplate,
+    public async Task<bool> HandleTerraformRequestServiceAsync(Datahub_Project datahubProject,
+        TerraformTemplate terraformTemplate,
         PortalUser requestingUser)
     {
         try
@@ -106,15 +105,21 @@ public class RequestManagementService : IRequestManagementService
             {
                 return false;
             }
-            
-            
-            var dependencyTemplates = TerraformTemplate.GetDependenciesToCreate(terraformTemplate.Name);
-            if (terraformTemplate.Name != TerraformTemplate.VariableUpdate)
+
+            if (terraformTemplate.Status == TerraformStatus.DeleteRequested)
             {
                 await ProcessRequest(project, requestingUser, terraformTemplate);
-                foreach (var template in dependencyTemplates)
+            }
+            else
+            {
+                var dependencyTemplates = TerraformTemplate.GetDependenciesToCreate(terraformTemplate.Name);
+                if (terraformTemplate.Name != TerraformTemplate.VariableUpdate)
                 {
-                    await ProcessRequest(project, requestingUser, template);
+                    await ProcessRequest(project, requestingUser, terraformTemplate);
+                    foreach (var template in dependencyTemplates)
+                    {
+                        await ProcessRequest(project, requestingUser, template);
+                    }
                 }
             }
 
@@ -145,134 +150,4 @@ public class RequestManagementService : IRequestManagementService
             _ => Role.Guest
         };
     }
-    
-    
-
-    // public static FieldValueContainer BuildFieldValues(FieldDefinitions fieldDefinitions,
-    //     Dictionary<string, string> existingValues, string objectId = null)
-    // {
-    //     if (string.IsNullOrEmpty(objectId))
-    //     {
-    //         objectId = Guid.NewGuid().ToString();
-    //     }
-    //
-    //     var vals = fieldDefinitions.Fields.Select(f =>
-    //     {
-    //         var val = new ObjectFieldValue
-    //         {
-    //             FieldDefinition = f,
-    //             FieldDefinitionId = f.FieldDefinitionId
-    //         };
-    //
-    //         if (existingValues.ContainsKey(f.Field_Name_TXT))
-    //         {
-    //             val.Value_TXT = existingValues[f.Field_Name_TXT];
-    //         }
-    //
-    //         return val;
-    //     }).ToList();
-    //
-    //     return new(objectId, fieldDefinitions, vals);
-    // }
-    //
-    // private static FieldDefinitions BuildFieldDefsFromDynamic(dynamic fields)
-    // {
-    //     var defs = new FieldDefinitions();
-    //     var nextId = 1;
-    //
-    //     foreach (dynamic field in fields)
-    //     {
-    //         var def = new FieldDefinition
-    //         {
-    //             Field_Name_TXT = field.field_name,
-    //             Name_English_TXT = field.name_en,
-    //             Name_French_TXT = field.name_fr,
-    //             Required_FLAG = field.required ?? false,
-    //             Default_Value_TXT = field.default_value,
-    //             FieldDefinitionId = nextId++
-    //         };
-    //
-    //         if (field.choices != null)
-    //         {
-    //             foreach (dynamic choice in field.choices)
-    //             {
-    //                 var fc = new FieldChoice
-    //                 {
-    //                     FieldChoiceId = nextId++,
-    //                     Value_TXT = choice.value,
-    //                     Label_English_TXT = choice.label_en,
-    //                     Label_French_TXT = choice.label_fr,
-    //                     FieldDefinition = def,
-    //                     FieldDefinitionId = def.FieldDefinitionId
-    //                 };
-    //
-    //                 def.Choices.Add(fc);
-    //             }
-    //         }
-    //
-    //         defs.Add(def);
-    //     }
-    //
-    //     return defs;
-    // }
-    //
-    // private static MetadataProfile BuildProfileFromDefsAndDynamic(FieldDefinitions defs, dynamic profileDyn)
-    // {
-    //     var profileSections = new List<MetadataSection>();
-    //     var nextId = 1;
-    //
-    //     var profile = new MetadataProfile
-    //     {
-    //         Name = profileDyn.name,
-    //         Sections = profileSections,
-    //         ProfileId = nextId++
-    //     };
-    //
-    //     foreach (var secDyn in profileDyn.sections)
-    //     {
-    //         var section = new MetadataSection
-    //         {
-    //             SectionId = nextId++,
-    //             Name_English_TXT = secDyn.label_en,
-    //             Name_French_TXT = secDyn.label_fr,
-    //             Fields = new List<SectionField>(),
-    //             Profile = profile,
-    //             ProfileId = profile.ProfileId
-    //         };
-    //
-    //         foreach (string fieldName in secDyn.fields)
-    //         {
-    //             var fd = defs.Get(fieldName);
-    //             var sf = new SectionField
-    //             {
-    //                 FieldDefinition = fd,
-    //                 FieldDefinitionId = fd.FieldDefinitionId,
-    //                 Section = section,
-    //                 SectionId = section.SectionId
-    //             };
-    //             section.Fields.Add(sf);
-    //         }
-    //
-    //         profileSections.Add(section);
-    //     }
-    //
-    //
-    //     return profile;
-    // }
-    //
-    //
-    // private static ProjectResourceFormParams BuildFormParamsFromJson(string inputJson)
-    // {
-    //     if (string.IsNullOrEmpty(inputJson))
-    //     {
-    //         return default;
-    //     }
-    //
-    //     var anonObject = JsonConvert.DeserializeObject<dynamic>(inputJson);
-    //
-    //     var defs = BuildFieldDefsFromDynamic(anonObject.fields);
-    //     var profile = BuildProfileFromDefsAndDynamic(defs, anonObject.profile);
-    //
-    //     return new(defs, profile);
-    // }
 }
