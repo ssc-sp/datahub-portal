b67582e6953835df27aa33c607a3598493408a00
Erik <erik@apption.com>
2024-11-18 17:57:39 -0500

    minor merge fix

--- ResourceProvisioner/src/ResourceProvisioner.Infrastructure/Services/RepositoryService.cs
+++ ResourceProvisioner/src/ResourceProvisioner.Infrastructure/Services/RepositoryService.cs
@@ -91,6 +91,8 @@ public partial class RepositoryService(
         }
         finally
         {
+            logger.LogInformation("Deleting temporary directory {Directory} for resource run", DirectoryUtils.tempDirectory);
+            CleanUpEnvironment();
             _semaphore.Release();
         }
     }
