d059c7008e6b8d1180eb0e19fadde11479159263
akhavich <61466698+akhavich@users.noreply.github.com>
2024-12-10 15:00:26 -0500

    Health check updates: Storage and webapp checks; monitoring page (#1502)
    * Web app health check WIP
    
    * Updated static class and cleaned up duplicate code
    
    * Updated storage and web app health checks
    
    * Infrastructure health page code improvements

---
+++ Portal/src/Datahub.Portal/Services/HealthCheckPageUtil.cs
@@ -0,0 +1,40 @@
+﻿using Datahub.Core.Model.Health;
+
+namespace Datahub.Portal.Services
+{
+    public static class HealthCheckPageUtil
+    {
+        private const int STALE_AGE_HOURS = 24;
+        private const int EXPIRED_AGE_HOURS = 72;
+
+        private static double GetAgeInHours(InfrastructureHealthCheck infrastructureHealthCheck) => (DateTime.UtcNow - infrastructureHealthCheck.HealthCheckTimeUtc).TotalHours;
+
+        public static InfrastructureHealthStatus GetDisplayStatus(InfrastructureHealthCheck infrastructureHealthCheck) => infrastructureHealthCheck.Status switch
+        {
+            InfrastructureHealthStatus.Healthy => GetAgeInHours(infrastructureHealthCheck) < STALE_AGE_HOURS ? InfrastructureHealthStatus.Healthy : InfrastructureHealthStatus.NeedHealthCheckRun,
+            _ => infrastructureHealthCheck.Status
+        };
+
+        public static string GetDisplayStatusText(InfrastructureHealthCheck infrastructureHealthCheck) => GetDisplayStatus(infrastructureHealthCheck).ToString();
+
+        public static MudBlazor.Color GetDisplayColor(InfrastructureHealthCheck infrastructureHealthCheck) => infrastructureHealthCheck.Status switch
+        {
+            InfrastructureHealthStatus.Healthy => GetAgeBasedColor(GetAgeInHours(infrastructureHealthCheck)),
+            InfrastructureHealthStatus.Degraded => MudBlazor.Color.Warning,
+            InfrastructureHealthStatus.Unhealthy => MudBlazor.Color.Error,
+            _ => MudBlazor.Color.Default
+        };
+
+        private static MudBlazor.Color GetAgeBasedColor(double age) => age < STALE_AGE_HOURS ? MudBlazor.Color.Success :
+            age < EXPIRED_AGE_HOURS ? MudBlazor.Color.Warning :
+            MudBlazor.Color.Error;
+
+        public static string GetStatusDetails(InfrastructureHealthCheck health)
+        {
+            var timeStamp = health.HealthCheckTimeUtc.ToLocalTime().ToString();
+            if (health.Status == InfrastructureHealthStatus.Healthy) return timeStamp;
+            return $"{health.Details} (as of {timeStamp})";
+        }
+
+    }
+}
