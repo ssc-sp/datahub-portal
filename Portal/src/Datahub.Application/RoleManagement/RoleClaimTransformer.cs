363f7cabaa9aa5e8aba5c8fc369e34c7846a130b
Sean-Stilwell <sstil051@uottawa.ca>
2024-12-11 13:09:50 -0500

    Starting work on restricting adding users to both admin and approver projects

--- Portal/src/Datahub.Application/RoleManagement/RoleClaimTransformer.cs
+++ Portal/src/Datahub.Application/RoleManagement/RoleClaimTransformer.cs
@@ -29,13 +29,16 @@ public class RoleClaimTransformer(IServiceAuthManager serviceAuthManager, ILogge
                 claims.AddClaim(new Claim(ClaimTypes.Role, "default"));
                 claims.AddClaim(new Claim(ClaimTypes.Role, userId));
 
+                // Ensure that the user can't be both approver and admin
+                bool alreadyAdded = claims.HasClaim(ClaimTypes.Role, RoleConstants.DATAHUB_ROLE_ADMIN_AS_GUEST) || claims.HasClaim(ClaimTypes.Role, RoleConstants.DATAHUB_APPROVER_ROLE);
+
                 foreach (var (role, project) in authorizedProjects)
                 {
-                    if (project.Project_Acronym_CD == RoleConstants.DATAHUB_ADMIN_PROJECT && serviceAuthManager.GetViewingAsGuest(userId))
+                    if (!alreadyAdded && project.Project_Acronym_CD == RoleConstants.DATAHUB_ADMIN_PROJECT && serviceAuthManager.GetViewingAsGuest(userId))
                     {
                         claims.AddClaim(new Claim(ClaimTypes.Role, RoleConstants.DATAHUB_ROLE_ADMIN_AS_GUEST));
                     }
-                    else if (project.Project_Acronym_CD == RoleConstants.DATAHUB_APPROVER_PROJECT) // Move this to be an `else if` with the above condition to prevent both support & approver role from being added
+                    else if (!alreadyAdded && project.Project_Acronym_CD == RoleConstants.DATAHUB_APPROVER_PROJECT)
                     {
                         claims.AddClaim(new Claim(ClaimTypes.Role, RoleConstants.DATAHUB_APPROVER_ROLE));
                     }
