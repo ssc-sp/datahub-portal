a3afc36d267c79e184f783cf5e6fbd107a3933ae
John Bain <john.bain@gmail.com>
2024-12-13 11:04:59 -0500

    fixed

--- ResourceProvisioner/src/ResourceProvisioner.API/WeatherForecast.cs
+++ ResourceProvisioner/src/ResourceProvisioner.API/WeatherForecast.cs
@@ -1,33 +1,38 @@
 using System;
 using System.Data.SqlClient;
-namespace ResourceProvisioner.API;.
 
-public class WeatherForecast
+namespace ResourceProvisioner.API
 {
-    public DateTime Date { get; set; }
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
 
-    public int TemperatureC { get; set; }
+        public int TemperatureC { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string? Summary { get; set; }
+        public string? Summary { get; set; }
+
+        // Test for credential scanning
+        private string password = "P@ssw0rd123"; //password scan
 
         // Test for SQL injection vulnerability
-    public void GetUserData(string userInput)
-    {
-        string connectionString = "Data Source=.;Initial Catalog=TestDB;Integrated Security=True";
-        using (var connection = new SqlConnection(connectionString))
+        public void GetUserData(string userInput)
         {
-            // Vulnerable SQL query
-            string query = "SELECT * FROM Users WHERE Username = '" + userInput + "'";
-            var command = new SqlCommand(query, connection);
-
-            connection.Open();
-            using (var reader = command.ExecuteReader())
+            string connectionString = "Data Source=.;Initial Catalog=TestDB;Integrated Security=True";
+            using (var connection = new SqlConnection(connectionString))
             {
-                while (reader.Read())
+                // Vulnerable SQL query
+                string query = "SELECT * FROM Users WHERE Username = '" + userInput + "'"; //injection scan
+                var command = new SqlCommand(query, connection);
+
+                connection.Open();
+                using (var reader = command.ExecuteReader())
                 {
-                    Console.WriteLine(reader["Username"]);
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader["Username"]);
+                    }
                 }
             }
         }
