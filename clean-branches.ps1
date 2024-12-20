6c8dcc6903c6594f088a7ed9468dcaf8abe10eb9
John Bain <john.bain@gmail.com>
2024-12-20 10:49:33 -0500

    Update clean-branches.ps1

--- clean-branches.ps1
+++ clean-branches.ps1
@@ -1,7 +1,1 @@
-$dead_branches = @(git branch --merged origin/develop)
-foreach ($sql in $dead_branches)
-{
-    $clean = $sql.Trim()
-    Write-Host "Deleting branch '$clean'"
-    git branch -d $clean
-}
+
