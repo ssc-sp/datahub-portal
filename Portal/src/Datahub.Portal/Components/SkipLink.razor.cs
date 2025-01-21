b2101e912abf1215400b54c5dce773b43f5152a3
shudsky <67435074+shudsky@users.noreply.github.com>
2025-01-21 14:05:49 -0500

    Sg/accessibility skip link (#1518)
    * UI updates
    
    * Accessibility updates
    
    * Make it a component and add some javascript
    
    ---------
    
    Co-authored-by: datahub-portal-pr-rw[bot] <192352896+datahub-portal-pr-rw[bot]@users.noreply.github.com>
    Co-authored-by: Erik <erik.putrycz@ssc-spc.gc.ca>
    Co-authored-by: John Bain <john.bain@gmail.com>
    Co-authored-by: davidreneuw <davrene11@gmail.com>

---
+++ Portal/src/Datahub.Portal/Components/SkipLink.razor.cs
@@ -0,0 +1,49 @@
+﻿namespace Datahub.Portal.Components
+{
+    public enum SkipLinkType
+    {
+        MainContent,
+        Navigation
+    }
+
+    public partial class SkipLink
+    {
+        public const string MainContentId = "maincontent";
+        public const string NavigationId = "nav";
+        
+        public static string GetSkipLinkId(SkipLinkType type)
+        {
+            return type switch
+            {
+                SkipLinkType.MainContent => MainContentId,
+                SkipLinkType.Navigation => NavigationId,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        public string GetSkipLinkText(SkipLinkType type)
+        {
+            return type switch
+            {
+                SkipLinkType.MainContent => Localizer["Skip to main content"],
+                SkipLinkType.Navigation => Localizer["Skip to navigation"],
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        public static SkipLinkType GetSkipLinkType(string id)
+        {
+            return id switch
+            {
+                MainContentId => SkipLinkType.MainContent,
+                NavigationId => SkipLinkType.Navigation,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+        
+        public static List<string> GetAvailableSkipLinks()
+        {
+            return Enum.GetValues<SkipLinkType>().Select(GetSkipLinkId).ToList();
+        }
+    }
+}
---
+++ Portal/src/Datahub.Portal/Components/SkipLink.razor.js
@@ -0,0 +1,6 @@
+﻿export function focusElement(elementId) {
+    var element = document.getElementById(elementId);
+    if (element) {
+        element.focus();
+    }
+}
