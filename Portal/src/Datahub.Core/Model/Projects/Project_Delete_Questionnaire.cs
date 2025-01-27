a2d59a879d9d2ae833bf0c337d1d520b09cc063d
gcnabeel <nb.capitaltrivia@gmail.com>
2025-01-27 14:33:54 -0500

    workspace deletion questionnaire model

---
+++ Portal/src/Datahub.Core/Model/Projects/Project_Delete_Questionnaire.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Datahub.Core.Model.Achievements;
+
+namespace Datahub.Core.Model.Projects
+{
+    public class Project_Delete_Questionnaire
+    {
+        [Key]
+        public int Id { get; set; }
+        public bool IsWorkspaceNotRequired { get; set; }
+        public bool IsDataMigrated { get; set; }
+        public bool IsDataNotSubjectToLitigation { get; set; }
+        public bool DoesDataNotHaveArchivalValue { get; set; }
+        public bool IsDeletionConfirmed { get; set; }
+
+        public DateTime? DeletedDate { get; set; }
+        public Datahub_Project Project { get; set; }
+        public PortalUser DeletedBy { get; set; }
+    }
+}
