bff31150c222e6c5f0ec4a421c2774a7af771503
Vladimir Bukharov <Vladimir.Bukharov@ssc-spc.gc.ca>
2025-01-30 13:03:50 -0800

    cleanup
    Signed-off-by: Vladimir Bukharov <Vladimir.Bukharov@ssc-spc.gc.ca>

---
+++ Portal/src/Datahub.Portal/Model/AzureFlexServer.cs
@@ -0,0 +1,13 @@
+﻿namespace Datahub.Portal.Model
+{
+    public class AzureFlexServer
+    {
+        public string SKU { get; set; }
+        public int vCores { get; set; }
+        public string MemorySize { get; set; }
+        public string MaxIOPS { get; set; }
+        public string MaxBandwidth { get; set; }
+        public string Type { get; set; }
+        public int StorageSize { get; set; }
+    }
+}
