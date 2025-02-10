977b4d134d33c9fe0b6489861e97b6efa25d9cb6
gcnabeel <83655937+gcnabeel@users.noreply.github.com>
2025-02-10 16:23:30 -0500

    Added Postgres Tiers (#1544)
    * added postgres tiers
    
    * added projectitems file
    
    * added new variable
    
    * repository service refactor
    
    * repositoryservice tests updated for refactor
    
    * refactoring of terraformservice
    
    * remove migration bug
    
    * fixed tests causing issues
    
    ---------
    
    Co-authored-by: Erik <erik.putrycz@ssc-spc.gc.ca>
    Co-authored-by: datahub-portal-pr-rw[bot] <192352896+datahub-portal-pr-rw[bot]@users.noreply.github.com>

---
+++ Shared/src/Datahub.Shared/Entities/PostgresTier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datahub.Shared.Entities
+{
+    public class PostgresTier
+    {
+        public string SKUName { get; set; }
+        public int Cores { get; set; }
+        public string MemorySize { get; set; }
+        public string PSQL_SKU { get; set; }
+
+        public string Cost { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public static List<PostgresTier> GetPostgresTiers()
+        {
+            var postgresTiers = new List<PostgresTier>();
+            postgresTiers.Add(new PostgresTier { SKUName = "B1ms (default)", Cores = 1, MemorySize = "2GiB", PSQL_SKU = "B_Standard_B1ms", Cost = "$12.41/month", IsAvailable = true });
+            postgresTiers.Add(new PostgresTier { SKUName = "B2s", Cores = 2, MemorySize = "4GiB", PSQL_SKU = "B_Standard_B2s", Cost = "$49.64/month", IsAvailable = true });
+            postgresTiers.Add(new PostgresTier { SKUName = "B4ms", Cores = 4, MemorySize = "16GiB", PSQL_SKU = "B_Standard_B4ms", Cost = "$198.56/month", IsAvailable = true });
+            postgresTiers.Add(new PostgresTier { SKUName = "B8ms", Cores = 8, MemorySize = "32GiB", PSQL_SKU = "B_Standard_B8ms", Cost = "$397.12/month", IsAvailable = true });
+
+            return postgresTiers;
+        }
+    }
+}
