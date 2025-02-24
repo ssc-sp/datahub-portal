45cb5075576c840e21e839e3234734dca09f270a
Erik <erik.putrycz@ssc-spc.gc.ca>
2025-02-24 08:53:10 -0500

    GC Hosting + UI changes (#1574)
    * Verify that user in tenant (#1563)
    
    Co-authored-by: davidreneuw <davrene11@gmail.com>
    
    * ✨ New home page (#1556)
    
    * Mock home page and drop down in sidebar
    
    * Home page mocks
    
    * New type of card
    
    * New home page
    
    * Improve workspace sidebar dropdown
    
    * Adding localization and sorting
    
    * Only show non-deleted workspaces in dropdown
    
    * Make cost bar horizontal, add label on top
    
    * Remove debug leftover
    
    ---------
    
    Co-authored-by: datahub-portal-pr-rw[bot] <192352896+datahub-portal-pr-rw[bot]@users.noreply.github.com>
    Co-authored-by: Erik <erik.putrycz@ssc-spc.gc.ca>
    
    * Handle missing credits better (#1570)
    
    * 💰 Updating the GC Hosting endpoint fields (#1568)
    
    * Adding migration to change what GC Hosting gives us
    
    * Updating controller to handle new fields
    
    * Updating tool page to handle new fields
    
    * Updating tool page to add the workspace budget (missed before
    
    ---------
    
    Co-authored-by: datahub-portal-pr-rw[bot] <192352896+datahub-portal-pr-rw[bot]@users.noreply.github.com>
    
    * Postgres Tier code refactor (#1564)
    
    * Postgres tiers merging
    
    * code cleanup
    
    * Restricting usage emails to admins and workspace leads (#1565)
    
    * Fixing styling issue with HomeNav (#1559)
    
    * 🐛 Parametrize budget rollover into a configuration (#1562)
    
    * Parametrize budget rollover into a configuration
    
    * Fix logic to give the force option the highest authority
    
    * Adjust for unit tests
    
    ---------
    
    Co-authored-by: datahub-portal-pr-rw[bot] <192352896+datahub-portal-pr-rw[bot]@users.noreply.github.com>
    
    * Adding migration for new fields (#1573)
    
    * Added unit tests for GC Hosting API (#1571)
    
    * - misc validation for hosting service controller
    - removed WorkspaceIdentifier since acronym is returned
    
    * added request id
    
    * added feature for gc hosting
    
    * Add files via upload
    
    * moved test files
    
    * fixed workspace storage tests
    
    * added sample requests and unit tests for hosting services api
    
    * added valid sample for latest format
    
    * fix for unit tests
    
    * fixed required values in json
    
    ---------
    
    Co-authored-by: datahub-portal-pr-rw[bot] <192352896+datahub-portal-pr-rw[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: davidreneuw <davrene11@gmail.com>
    Co-authored-by: David René <56747050+davidreneuw@users.noreply.github.com>
    Co-authored-by: datahub-portal-pr-rw[bot] <192352896+datahub-portal-pr-rw[bot]@users.noreply.github.com>
    Co-authored-by: Sean Stilwell <sstil051@uottawa.ca>
    Co-authored-by: gcnabeel <83655937+gcnabeel@users.noreply.github.com>

---
+++ Portal/src/Datahub.Core/Migrations/SqliteDatahub/20250220190839_AddingHostingServicesCommitmentFields.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datahub.Core.Migrations.SqliteDatahub
+{
+    /// <inheritdoc />
+    public partial class AddingHostingServicesCommitmentFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FinancialAuthorityCommitmentIsOrg",
+                table: "GCHostingWorkspaceDetails",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FinancialAuthorityCommitmentIsRef",
+                table: "GCHostingWorkspaceDetails",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FinancialAuthorityCommitmentIsOrg",
+                table: "GCHostingWorkspaceDetails");
+
+            migrationBuilder.DropColumn(
+                name: "FinancialAuthorityCommitmentIsRef",
+                table: "GCHostingWorkspaceDetails");
+        }
+    }
+}
