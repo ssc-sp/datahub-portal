99cc5b29528b53d7146dce3fd23a925b2bf4651f
Erik <erik@apption.com>
2025-03-05 13:36:40 -0500

    added try/catch

--- Portal/src/Datahub.Portal/Controllers/IFrameMiddleware.cs
+++ Portal/src/Datahub.Portal/Controllers/IFrameMiddleware.cs
@@ -8,31 +8,39 @@
     public class IFrameMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<IFrameMiddleware> logger;
 
-        public IFrameMiddleware(RequestDelegate next)
+        public IFrameMiddleware(RequestDelegate next, ILogger<IFrameMiddleware> logger)
         {
             _next = next;
+            this.logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
             var endpoint = context.GetEndpoint();
             bool headerConfigured = false;
-            if (endpoint != null)
+            try
             {
-                var controllerActionDescriptor = endpoint.Metadata.GetMetadata<Microsoft.AspNetCore.Mvc.Controllers.ControllerActionDescriptor>();
-                if (controllerActionDescriptor != null)
+                if (endpoint != null)
                 {
-                    var methodInfo = controllerActionDescriptor.MethodInfo;
-                    var allowIFrame = methodInfo.GetCustomAttributes(typeof(AllowIFrameAttribute), true).Any();
+                    var controllerActionDescriptor = endpoint.Metadata.GetMetadata<Microsoft.AspNetCore.Mvc.Controllers.ControllerActionDescriptor>();
+                    if (controllerActionDescriptor != null)
+                    {
+                        var methodInfo = controllerActionDescriptor.MethodInfo;
+                        var allowIFrame = methodInfo.GetCustomAttributes(typeof(AllowIFrameAttribute), true).Any();
 
-                    // Add the header only if the AllowIFrameAttribute is not present
-                    if (allowIFrame)
-                    { 
-                        context.Response.Headers.Append("X-Frame-Options", "SAMEORIGIN");
-                        headerConfigured = true;
+                        // Add the header only if the AllowIFrameAttribute is not present
+                        if (allowIFrame)
+                        {
+                            context.Response.Headers.Append("X-Frame-Options", "SAMEORIGIN");
+                            headerConfigured = true;
+                        }
                     }
                 }
+            } catch (Exception e)
+            {
+                logger.LogWarning(e, "Error while processing IFrameMiddleware");
             }
             if (!headerConfigured)
             {
