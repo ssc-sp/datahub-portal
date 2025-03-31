734e7f049a974ab496585a0d55ef3a7038f036e5
davidreneuw <davrene11@gmail.com>
2025-03-31 11:55:48 -0400

    Fix unit tests

--- ServerlessOperations/src/Datahub.Functions/ProjectUsageScheduler.cs
+++ ServerlessOperations/src/Datahub.Functions/ProjectUsageScheduler.cs
@@ -181,8 +181,9 @@ public class ProjectUsageScheduler(
             var projects = ctx.Projects
                 .Include(p => p.DatahubAzureSubscription)
                 .Include(p => p.Credits)
-                .Where(p => !p.IsDeleted)
                 .AsNoTracking()
+                .ToList()
+                .Where(p => !p.IsDeleted)
                 .ToList();
 
             if (acronyms.Any())
