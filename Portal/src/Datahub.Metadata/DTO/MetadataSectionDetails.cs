865a58aa6186f6409a187cd5a7b53f4cb546d148
Vladimir Bukharov <Vladimir.Bukharov@ssc-spc.gc.ca>
2025-04-07 16:48:32 -0700

    feat: replace Elemental with Mud components

--- Portal/src/Datahub.Metadata/DTO/MetadataSectionDetails.cs
+++ Portal/src/Datahub.Metadata/DTO/MetadataSectionDetails.cs
@@ -1,33 +1,31 @@
 ﻿using System.Collections.Generic;
 
-namespace Datahub.Metadata.DTO
+namespace Datahub.Metadata.DTO;
+public class MetadataSectionDetails
 {
-    public class MetadataSectionDetails
+    public string ListId { get; set; }
+    public int SectionId { get; set; }
+    public string English { get; set; }
+    public string French { get; set; }
+    public HashSet<int> Fields { get; set; }
+    public override string ToString()
     {
-        public string ListId { get; set; }
-        public int SectionId { get; set; }
-        public string English { get; set; }
-        public string French { get; set; }
-        public HashSet<int> Fields { get; set; }
-        public override string ToString()
-        {
-            return string.IsNullOrEmpty(English) ? "" : $"{English} / {French}";
-        }
+        return string.IsNullOrEmpty(English) ? "" : $"{English} / {French}";
     }
-    public class MetadataSectionDetailsComparer : IEqualityComparer<MetadataSectionDetails>
+}
+public class MetadataSectionDetailsComparer : IEqualityComparer<MetadataSectionDetails>
+{
+    public bool Equals(MetadataSectionDetails x, MetadataSectionDetails y)
     {
-        public bool Equals(MetadataSectionDetails x, MetadataSectionDetails y)
-        {
-            if (x == null && y == null)
-                return true;
-            if (x == null || y == null)
-                return false;
-            return x.ListId == y.ListId;
-        }
+        if (x == null && y == null)
+            return true;
+        if (x == null || y == null)
+            return false;
+        return x.ListId == y.ListId;
+    }
 
-        public int GetHashCode(MetadataSectionDetails obj)
-        {
-            return obj.ListId.GetHashCode();
-        }
+    public int GetHashCode(MetadataSectionDetails obj)
+    {
+        return obj.ListId.GetHashCode();
     }
 }
