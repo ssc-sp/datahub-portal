0529848ca7f39007d4ae4be709b2b1ea98643352
David René <56747050+davidreneuw@users.noreply.github.com>
2025-06-02 13:17:54 -0400

    fix: Fix notification records not being saved, and logging (#1654)
    Co-authored-by: datahub-portal-pr-rw[bot] <192352896+datahub-portal-pr-rw[bot]@users.noreply.github.com>

--- Portal/src/Datahub.Application/Services/Projects/IProjectInactivityNotificationService.cs
+++ Portal/src/Datahub.Application/Services/Projects/IProjectInactivityNotificationService.cs
@@ -1,10 +1,8 @@
-﻿using Datahub.Core.Model.Projects;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-
+﻿
 namespace Datahub.Application.Services.Projects
 {
     public interface IProjectInactivityNotificationService
     {
-        public Task<EntityEntry<ProjectInactivityNotifications>> AddInactivityNotification(int projectId, DateTime notificationDate, int daysBeforeDeletion, string sentTo, CancellationToken ct);
+        public Task<int> AddInactivityNotification(int projectId, DateTime notificationDate, int daysBeforeDeletion, string sentTo, CancellationToken ct);
     }
 }
