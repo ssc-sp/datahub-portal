a63a23aeab82dad08ca10865cdeb882628c18513
Erik <erik@apption.com>
2025-07-04 16:43:39 -0400

    added generic component for time zone conversions

---
+++ Portal/src/Datahub.Portal/Services/TimeZoneExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolbelt.Blazor.Globalization;
+
+namespace Datahub.Portal.Services
+{
+    public static class TimeZoneExtensions
+    {
+        public static async Task<string?> GetLocalTimeString(this ILocalTimeZone localTimeZoneService, DateTime utcTime)
+        {
+            var localTimeZone = await localTimeZoneService.GetLocalTimeZoneAsync(null);
+            if (localTimeZone is null)
+            {
+                return null;
+            }
+            var local = TimeZoneInfo.ConvertTimeFromUtc(utcTime, localTimeZone);
+            return local.ToString("g"); // or your preferred format
+        }
+    }
+}
