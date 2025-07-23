ae185f1ef37dbad6375869dade1c5b2112fa30eb
Sean-Stilwell <sstil051@uottawa.ca>
2025-07-23 08:31:41 -0400

    Fixing failing test

--- Portal/test/Datahub.Tests/LoginAndRegistrationTests.cs
+++ Portal/test/Datahub.Tests/LoginAndRegistrationTests.cs
@@ -264,7 +264,7 @@ namespace Datahub.Tests
             Assert.Equal(ssoRedirect, _navigationManagerMock.LastUri);
         }
 
-        [Fact(Skip = "The register page is deprecated, this test is no longer necessary.")]
+        [Fact]
         public async Task Test_DeletedUserRegistration()
         {
             // Arrange
@@ -299,7 +299,7 @@ namespace Datahub.Tests
             Assert.Equal("/login", _navigationManagerMock.LastUri);
         }
 
-        [Fact(Skip = "The register page is deprecated, this test is no longer necessary.")]
+        [Fact]
         public async Task Test_NewUserRegistration()
         {
             // Arrange
