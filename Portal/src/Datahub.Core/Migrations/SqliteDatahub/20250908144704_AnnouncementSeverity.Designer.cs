2c88615bab16929f252017259f531a64c2f91ed4
shudsky <67435074+shudsky@users.noreply.github.com>
2025-09-22 10:12:31 -0400

    feat: Adding severity levels to announcements (#1736)
    Co-authored-by: datahub-portal-pr-rw[bot] <192352896+datahub-portal-pr-rw[bot]@users.noreply.github.com>
    Co-authored-by: Sean-Stilwell <sstil051@uottawa.ca>

--- Portal/src/Datahub.Core/Migrations/SqliteDatahub/20250908144704_AnnouncementSeverity.Designer.cs
+++ Portal/src/Datahub.Core/Migrations/SqliteDatahub/20250908144704_AnnouncementSeverity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Datahub.Core.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Datahub.Core.Migrations.SqliteDatahub
 {
     [DbContext(typeof(SqliteDatahubContext))]
-    partial class SqliteDatahubContextModelSnapshot : ModelSnapshot
+    [Migration("20250908144704_AnnouncementSeverity")]
+    partial class AnnouncementSeverity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.8");
