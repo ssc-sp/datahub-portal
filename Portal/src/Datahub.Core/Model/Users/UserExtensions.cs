e385aca4b72ad0eaa85754e5f499d64e4ab38869
Erik Putrycz <erik.putrycz@ssc-spc.gc.ca>
2025-11-24 09:47:47 -0500

    continued refactoring

---
+++ Portal/src/Datahub.Core/Model/Users/UserExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahub.Core.Model.Users
+{
+    public static class UserExtensions
+    {
+        public static string? UserUID(this PortalUser user)
+        {
+            if (user.EntraUser?.GraphGuid != null)
+            {
+                return user.EntraUser.GraphGuid;
+            }
+            else if (user.ExternalUser?.OID != null)
+            {
+                return user.ExternalUser.OID;
+            }
+            return null;
+        }
+    }
+}
