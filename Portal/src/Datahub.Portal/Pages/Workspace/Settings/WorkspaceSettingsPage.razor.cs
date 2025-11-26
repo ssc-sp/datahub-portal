47fc4874f3f9d2d6177c9c3f2f89a13cd6f696f6
Alex Khavich <alexk@apption.com>
2025-11-26 11:36:55 -0500

    Configuration of maximum upload size for external users

--- Portal/src/Datahub.Portal/Pages/Workspace/Settings/WorkspaceSettingsPage.razor.cs
+++ Portal/src/Datahub.Portal/Pages/Workspace/Settings/WorkspaceSettingsPage.razor.cs
@@ -27,6 +27,15 @@ public partial class WorkspaceSettingsPage
     }
 
     /// <summary>
+    /// Handles the change in max upload size for external users.
+    /// </summary>
+    /// <param name="value">The new max upload size</param>
+    internal void HandleMaxUploadChanged(long value)
+    {
+        _maxUploadForExternal = value;
+    }
+
+    /// <summary>
     /// Saves the changes made to the workspace model, updates the database,
     /// and triggers a Terraform update if necessary.
     /// </summary>
@@ -81,6 +90,7 @@ public partial class WorkspaceSettingsPage
     {
         _workspace.PreventAutoDelete = _preventAutoDelete;
         _workspace.Project_Budget = _budget;
+        _workspace.MaxUploadMBForGccf = _maxUploadForExternal;
     }
 
     /// <summary>
@@ -90,5 +100,6 @@ public partial class WorkspaceSettingsPage
     {
         _preventAutoDelete = _workspace.PreventAutoDelete;
         _budget = _workspace.Project_Budget ?? 0;
+        _maxUploadForExternal = _workspace.MaxUploadMBForGccf;
     }
 }
