a93be8da25b990dba0cd4b297c85d583d07359a9
Erik <erik@apption.com>
2025-12-18 15:28:22 -0500

    added new migration for external user and entra user

--- Portal/src/Datahub.Core/Migrations/SqliteDatahub/20251218201819_ExternalUser.Designer.cs
+++ Portal/src/Datahub.Core/Migrations/SqliteDatahub/20251218201819_ExternalUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Datahub.Core.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Datahub.Core.Migrations.SqliteDatahub
 {
     [DbContext(typeof(SqliteDatahubContext))]
-    partial class SqliteDatahubContextModelSnapshot : ModelSnapshot
+    [Migration("20251218201819_ExternalUser")]
+    partial class ExternalUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.11");
