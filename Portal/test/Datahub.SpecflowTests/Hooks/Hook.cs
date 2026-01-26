d0482b347e0e58e4467b63d006dfd01b24a3fc5f
vb-ssc <161481747+vb-ssc@users.noreply.github.com>
2026-01-26 10:43:36 -0800

    feat: Admin: publishing blocklist (#1768)
    Co-authored-by: Erik <erik.putrycz@ssc-spc.gc.ca>

--- Portal/test/Datahub.SpecflowTests/Hooks/Hook.cs
+++ Portal/test/Datahub.SpecflowTests/Hooks/Hook.cs
@@ -70,7 +70,7 @@ public class Hooks
     }
 
 
-    [BeforeScenario("IWebHostEnvironment")]
+    [BeforeScenario("IWebHostEnvironment", Order = 1)]
     public async Task BeforeScenarioRequiringOffline(IObjectContainer objectContainer, ScenarioContext scenarioContext)
     {
         // Use a concrete test implementation of IWebHostEnvironment instead of a substitute
@@ -85,6 +85,17 @@ public class Hooks
         };
 
         objectContainer.RegisterInstanceAs<IWebHostEnvironment>(testEnvironment);
+        
+        // Register IDbContextFactory for scenarios that might need it during teardown
+        // Use scenario-specific database name to ensure isolation between scenarios
+        var databaseName = $"TestDb_{scenarioContext.ScenarioInfo.Title.Replace(" ", "_")}_{Guid.NewGuid()}";
+        scenarioContext["DatabaseName"] = databaseName;
+        
+        var options = new DbContextOptionsBuilder<DatahubProjectDBContext>()
+            .UseInMemoryDatabase(databaseName: databaseName)
+            .Options;
+        var dbContextFactory = new SpecFlowDbContextFactory(options);
+        objectContainer.RegisterInstanceAs<IDbContextFactory<DatahubProjectDBContext>>(dbContextFactory);
     }
 
     // Simple concrete IWebHostEnvironment implementation for tests
