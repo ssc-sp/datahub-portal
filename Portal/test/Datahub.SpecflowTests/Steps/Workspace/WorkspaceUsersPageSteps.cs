210ef25ea90460de684fc64f7627eb01f0c46d18
Vladimir Bukharov <Vladimir.Bukharov@ssc-spc.gc.ca>
2026-04-09 10:38:38 -0700

    packages updated

--- Portal/test/Datahub.SpecflowTests/Steps/Workspace/WorkspaceUsersPageSteps.cs
+++ Portal/test/Datahub.SpecflowTests/Steps/Workspace/WorkspaceUsersPageSteps.cs
@@ -127,14 +127,13 @@ namespace Datahub.SpecflowTests.Steps
             var mockProjectUserManagementService = Substitute.For<IProjectUserManagementService>();
             Services.AddSingleton(mockProjectUserManagementService);
 
-            var mockLockedUserManagementService = Substitute.For<ILockedUserManagementService>();
-            mockLockedUserManagementService
-                .GetLockedUsersInWorkspaceAsync(Arg.Any<int>())
-                .Returns(Task.FromResult(new List<UserLockStatus>()));
-            Services.AddSingleton(mockLockedUserManagementService);
-
             var mockUserSettingsService = Substitute.For<IUserSettingsService>();
             Services.AddSingleton(mockUserSettingsService);
+
+            var mockLockedUserManagementService = Substitute.For<ILockedUserManagementService>();
+                mockLockedUserManagementService.GetLockedUsersInWorkspaceAsync(Arg.Any<int>())
+                .Returns(Task.FromResult(new List<UserLockStatus>()));
+            Services.AddSingleton(mockLockedUserManagementService);
 
             var mockProjectUsers = SetupProjectUsers();
 
