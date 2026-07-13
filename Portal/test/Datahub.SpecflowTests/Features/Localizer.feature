2cc0624ed02bec315075d0ca2e0ff10f12a5580b
Sean-Stilwell <sean.stilwell@ssc-spc.gc.ca>
2026-07-13 10:52:37 -0400

    adding unit tests

---
+++ Portal/test/Datahub.SpecflowTests/Features/Localizer.feature
@@ -0,0 +1,26 @@
+@IWebHostEnvironment
+Feature: Localizer
+  For internationalization, we use a localizer to retrieve strings that are displayed to users in English or French
+
+  Scenario: Strings should be retrieved in the appropriate language
+  Given a text field with localized content for <key> from <file>
+  When a user views the text in <language>
+  Then the user should see <expectedoutput>
+  Examples:
+    | key | language | expectedoutput | file |
+    | MainTitle | en | Federal Science DataHub | localization.json |
+    | MainTitle | fr | Plateforme fédérale de données scientifiques | localization.fr.json |
+    | Usage | en | Usage | cbr.json |
+    | Usage | fr | Utilisation | cbr.fr.json |
+    | Enable ML | en | Enable ML | databricks.json |
+    | Enable ML | fr | Activer l'apprentissage machine | databricks.fr.json |
+    | Parole Board of Canada | en | Parole Board of Canada | depts.json |
+    | Parole Board of Canada | fr | Commission des libérations conditionnelles du Canada | depts.fr.json |
+    | Lead Email | en | Lead Email | gchosting.json |
+    | Lead Email | fr | Courriel du responsable | gchosting.fr.json |
+    | /locked-lt | en | /verrouille | url.json |
+    | /verrouille-lt | fr | /locked | url.fr.json |
+    | https://documentation.sds.canada.ca/ | en | https://documentation.sds.canada.ca/en/ | url.json |
+    | https://documentation.sds.canada.ca/ | fr | https://documentation.sds.canada.ca/fr/ | url.fr.json |
+    | Current FSDH Version | en | Current FSDH Version | workspace_versions.json |
+    | Current FSDH Version | fr | Version actuelle de la PFDS | workspace_versions.fr.json |
---
+++ Portal/test/Datahub.SpecflowTests/Steps/LocalizerSteps.cs
@@ -0,0 +1,88 @@
+using AspNetCore.Localizer.Json.Commons;
+using AspNetCore.Localizer.Json.Extensions;
+using AspNetCore.Localizer.Json.JsonOptions;
+using Bunit;
+using Bunit.TestDoubles;
+using Datahub.Portal;
+using Datahub.SpecflowTests.Utils;
+using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using MudBlazor;
+using Reqnroll;
+using System.Globalization;
+using System.Text;
+
+namespace Datahub.SpecflowTests.Steps;
+
+[Binding]
+internal class LocalizerSteps(
+    ScenarioContext scenarioContext,
+    IWebHostEnvironment hostingEnvironment
+)
+{
+
+    private static IStringLocalizer CreateLocalizer()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+
+        services.AddJsonLocalization(options =>
+        {
+            options.CacheDuration = TimeSpan.FromMinutes(15);
+            options.ResourcesPath = "i18n";
+            options.LocalizationMode = LocalizationMode.I18n;
+            options.UseEmbeddedResources = true;
+            options.MissingTranslationLogBehavior = MissingTranslationLogBehavior.Ignore;
+            options.FileEncoding = Encoding.GetEncoding("UTF-8");
+            options.SupportedCultureInfos = new HashSet<CultureInfo>
+            {
+                new("en-CA"),
+                new("fr-CA")
+            };
+            options.AssemblyHelper = new AssemblyHelper(typeof(Startup).Assembly);
+        });
+
+        using var serviceProvider = services.BuildServiceProvider();
+        var factory = serviceProvider.GetRequiredService<IStringLocalizerFactory>();
+        return factory.Create(typeof(LocalizerSteps));
+    }
+
+    [Given("a text field with localized content for (.*) from (.*)")]
+    public void GivenATextFieldWithLocalizedContentForFrom(string key, string file)
+    {
+        scenarioContext["Key"] = key;
+        scenarioContext["File"] = file;
+    }
+
+    [When("a user views the text in (.*)")]
+    public void WhenAUserViewsTheTextIn(string language)
+    {
+        scenarioContext["Language"] = language;
+
+        var cultureInfo = language.ToLowerInvariant() switch
+        {
+            "en" or "en-ca" => new CultureInfo("en-CA"),
+            "fr" or "fr-ca" => new CultureInfo("fr-CA"),
+            _ => throw new ArgumentException($"Unsupported language: {language}")
+        };
+        
+        scenarioContext["LanguageKey"] = cultureInfo.Name;
+        Thread.CurrentThread.CurrentUICulture = cultureInfo;
+        Thread.CurrentThread.CurrentCulture = cultureInfo;
+
+        var key = scenarioContext["Key"] as string;
+        var stringLocalizer = CreateLocalizer();
+        var localizedString = stringLocalizer[key!];
+        scenarioContext["Output"] = localizedString.Value;
+    }
+
+    [Then("the user should see (.*)")]
+    public void ThenTheUserShouldSee(string expectedoutput)
+    {
+        var actualOutput = scenarioContext["Output"] as string;
+        var file = scenarioContext["File"] ?? string.Empty;
+        actualOutput.Should().Be(expectedoutput, $"that is what is expected in {file}");
+    }
+}
