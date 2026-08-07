3c4f062859726912dc0afdcdf15dcbf1616ffb15
John Bain <John.Bain@gmail.com>
2026-08-07 15:38:47 +0000

    test

--- ResourceProvisioner/test/ResourceProvisioner.Infrastructure.UnitTests/Templates/AzureAppServiceTemplateTests.cs
+++ ResourceProvisioner/test/ResourceProvisioner.Infrastructure.UnitTests/Templates/AzureAppServiceTemplateTests.cs
@@ -19,8 +19,7 @@ public class AzureAppServiceTemplateTests : TemplateTestCollection
     public void RunBeforeEachTest()
     {
         var localModuleClonePath = DirectoryUtils.GetModuleRepositoryPath(_resourceProvisionerConfiguration);
-        var localInfrastructureClonePath =
-            DirectoryUtils.GetInfrastructureRepositoryPath(_resourceProvisionerConfiguration);
+        var localInfrastructureClonePath = DirectoryUtils.GetInfrastructureRepositoryPath(_resourceProvisionerConfiguration);
 
         VerifyDirectoryDoesNotExist(localModuleClonePath);
         VerifyDirectoryDoesNotExist(localInfrastructureClonePath);
@@ -30,17 +29,12 @@ public class AzureAppServiceTemplateTests : TemplateTestCollection
     public async Task ShouldThrowExceptionIfProjectNotInitialized()
     {
         var workspaceAcronym = GenerateWorkspaceAcronym();
-        var workspace = GenerateTestTerraformWorkspace(workspaceAcronym, false);
-
-        await _repositoryService.FetchRepositoriesAndCheckoutProjectBranch(workspace);
-
+        
         var command = GenerateTestWorkspaceDefinition(
-         workspaceAcronym, new List<string>()
-         {
-                        TerraformTemplate.NewProjectTemplate,
-                        TerraformTemplate.NewProjectTemplate,
-                        TerraformTemplate.NewProjectTemplate
-         });
+            workspaceAcronym, 
+            new List<string> { TerraformTemplate.NewProjectTemplate }); // Fixed duplicate templates
+
+        await _repositoryService.FetchRepositoriesAndCheckoutProjectBranch(command.Workspace);
 
         Assert.ThrowsAsync<ProjectNotInitializedException>(async () =>
         {
@@ -54,22 +48,17 @@ public class AzureAppServiceTemplateTests : TemplateTestCollection
         var workspaceAcronym = GenerateWorkspaceAcronym();
         var newProjectTemplateExpectedFileCount = await SetupNewProjectTemplate(workspaceAcronym);
         var module = GenerateTerraformTemplate(TerraformTemplate.AzureAppService);
+        
         var command = GenerateTestWorkspaceDefinition(
-              workspaceAcronym, new List<string>()
-              {
-                        TerraformTemplate.NewProjectTemplate,
-                        TerraformTemplate.NewProjectTemplate,
-                        TerraformTemplate.NewProjectTemplate
-              });
-        await _terraformService.CopyTemplateAsync(module.Name, command);
+            workspaceAcronym, 
+            new List<string> { TerraformTemplate.NewProjectTemplate });
 
+        await _terraformService.CopyTemplateAsync(module.Name, command);
         await _repositoryService.FetchModuleRepository(command.Workspace.Version);
 
-        var moduleSourcePath =
-            DirectoryUtils.GetTemplatePath(_resourceProvisionerConfiguration, TerraformTemplate.AzureAppService);
+        var moduleSourcePath = DirectoryUtils.GetTemplatePath(_resourceProvisionerConfiguration, TerraformTemplate.AzureAppService);
         var moduleDestinationPath = DirectoryUtils.GetProjectPath(_resourceProvisionerConfiguration, workspaceAcronym);
 
-        // verify all the files are copied except for the datahub readme
         var expectedFiles = Directory.GetFiles(moduleSourcePath, "*.*", SearchOption.TopDirectoryOnly)
             .Where(filename => !TerraformService.EXCLUDED_FILE_EXTENSIONS.Contains(Path.GetExtension(filename)))
             .ToList();
@@ -81,13 +70,11 @@ public class AzureAppServiceTemplateTests : TemplateTestCollection
                 Has.Length.EqualTo(expectedFiles.Count + newProjectTemplateExpectedFileCount));
         });
 
-        // go through each file and assert that the content is the same
         foreach (var file in expectedFiles)
         {
             var sourceFileContent = await File.ReadAllTextAsync(file);
             var expectedContent = sourceFileContent.Replace(TerraformService.TerraformTagToken, $"?ref={_resourceProvisionerConfiguration.ModuleRepository.Branch}-{command.Workspace.Version}");
-            var destinationFileContent =
-                await File.ReadAllTextAsync(Path.Join(moduleDestinationPath, Path.GetFileName(file)));
+            var destinationFileContent = await File.ReadAllTextAsync(Path.Join(moduleDestinationPath, Path.GetFileName(file)));
             Assert.That(destinationFileContent, Is.EqualTo(expectedContent));
         }
     }
@@ -98,32 +85,24 @@ public class AzureAppServiceTemplateTests : TemplateTestCollection
         var workspaceAcronym = GenerateWorkspaceAcronym();
         await SetupNewProjectTemplate(workspaceAcronym);
 
-        var workspace = GenerateTestTerraformWorkspace(workspaceAcronym);
         var module = GenerateTerraformTemplate(TerraformTemplate.AzureAppService);
-        var expectedVariables = GenerateExpectedVariables(workspace);
+        var expectedVariables = GenerateExpectedVariables();
 
         var command = GenerateTestWorkspaceDefinition(
-            workspaceAcronym, new List<string>()
-            {
-                TerraformTemplate.NewProjectTemplate,
-                TerraformTemplate.NewProjectTemplate,
-                TerraformTemplate.NewProjectTemplate
-            });
-        
+            workspaceAcronym, 
+            new List<string> { TerraformTemplate.NewProjectTemplate });
         
         await _terraformService.CopyTemplateAsync(module.Name, command);
         await _terraformService.ExtractVariables(module.Name, command);
-
         await _repositoryService.FetchModuleRepository(command.Workspace.Version);
 
         var expectedVariablesFilename = Path.Join(
             DirectoryUtils.GetProjectPath(_resourceProvisionerConfiguration, workspaceAcronym),
             $"{module.Name}.auto.tfvars.json");
+            
         Assert.That(File.Exists(expectedVariablesFilename), Is.True);
 
-        var actualVariables =
-            JsonSerializer.Deserialize<JsonObject>(
-                await File.ReadAllTextAsync(expectedVariablesFilename));
+        var actualVariables = JsonSerializer.Deserialize<JsonObject>(await File.ReadAllTextAsync(expectedVariablesFilename));
 
         foreach (var (key, value) in actualVariables!)
         {
@@ -141,17 +120,14 @@ public class AzureAppServiceTemplateTests : TemplateTestCollection
         var workspaceAcronym = GenerateWorkspaceAcronym();
         await SetupNewProjectTemplate(workspaceAcronym);
 
-        var workspace = GenerateTestTerraformWorkspace(workspaceAcronym);
-        var expectedVariables = GenerateExpectedVariables(workspace);
+        var expectedVariables = GenerateExpectedVariables();
         var module = GenerateTerraformTemplate(TerraformTemplate.AzureAppService);
 
         var command = GenerateTestWorkspaceDefinition(
-         workspaceAcronym, new List<string>()
-         {
-                TerraformTemplate.NewProjectTemplate,
-                TerraformTemplate.NewProjectTemplate,
-                TerraformTemplate.NewProjectTemplate
-         });
+            workspaceAcronym, 
+            new List<string> { TerraformTemplate.NewProjectTemplate });
+            
+
         await _terraformService.CopyTemplateAsync(module.Name, command);
 
         await _terraformService.ExtractVariables(module.Name, command);
@@ -159,14 +135,14 @@ public class AzureAppServiceTemplateTests : TemplateTestCollection
         await _terraformService.ExtractVariables(module.Name, command);
         await _repositoryService.FetchModuleRepository(command.Workspace.Version);
 
+
         var expectedVariablesFilename = Path.Join(
             DirectoryUtils.GetProjectPath(_resourceProvisionerConfiguration, workspaceAcronym),
             $"{module.Name}.auto.tfvars.json");
+            
         Assert.That(File.Exists(expectedVariablesFilename), Is.True);
 
-        var actualVariables =
-            JsonSerializer.Deserialize<JsonObject>(
-                await File.ReadAllTextAsync(expectedVariablesFilename));
+        var actualVariables = JsonSerializer.Deserialize<JsonObject>(await File.ReadAllTextAsync(expectedVariablesFilename));
 
         foreach (var (key, value) in actualVariables!)
         {
@@ -178,7 +154,7 @@ public class AzureAppServiceTemplateTests : TemplateTestCollection
         }
     }
 
-    private static JsonObject GenerateExpectedVariables(TerraformWorkspace workspace)
+    private static JsonObject GenerateExpectedVariables()
     {
         return new JsonObject
         {
